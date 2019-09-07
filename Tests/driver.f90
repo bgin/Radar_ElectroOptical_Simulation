fdbb06989dd173d8dfdcaf921925afd9ac05ab9d
Bernard Gingold <beniekg@gmail.com>
2019-09-07 18:48:24 +0200

    TMatrix_MPS_Test driver (main)

--- Tests/driver.f90
+++ Tests/driver.f90
@@ -1,102 +1,95 @@
-program Tmatrix_Test
-  use mod_tmatrix  
-  implicit none
-
-    
- 
 
 
- 
- 
- 
- 
- ! Arguments to TMatrix_driver subroutine
- real(R64P) :: RAT,AXMAX,B,GAM,EPS,LAM,MRR,MRI,DDELT
- integer(I32P) :: NDISTR,NPNAX,NKMAX,NP,NPNA,NDGS,i,j
- 
- ! Output arguments
- real(R64P) :: CEXT,CSCA,W,COSPH,REFF,VEFF
- real(R64P), dimension(NPL) :: ALPHA1,ALPHA2,ALPHA3,ALPHA4,BETA1,BETA2
- !DIR$ ATTRIBUTES ALIGN : 64 :: ALPHA1
- !DIR$ ATTRIBUTES ALIGN : 64 :: ALPHA2
- !DIR$ ATTRIBUTES ALIGN : 64 :: ALPHA3
- !DIR$ ATTRIBUTES ALIGN : 64 :: ALPHA4
- !DIR$ ATTRIBUTES ALIGN : 64 :: BETA1
- !DIR$ ATTRIBUTES ALIGN : 64 :: BETA2
- real(R64P), dimension(6,19) :: SCATMAT
- !DIR$ ATTRIBUTES ALIGN : 64 :: SCATMAT
- 
- ! Initialziation
- RAT = 1._R64P
- NDISTR = 3_I32P
- AXMAX  = 0.3_R64P
- NPNAX  = 1_I32P
- B = 0.1_R64P
- GAM = 0.5_R64P
- NKMAX = 51_I32P
- EPS = 2._R64P
- NP = -1_I32P
- LAM = 0.5_R64P
- MRR = 1.53_R64P
- MRI = 0.008_R64P
- DDELT = 0.001_R64P
- NPNA = 19_I32P
- NDGS = 2_I32P
- 
- ! Output variables
- ! Arrays
- ALPHA1 = 0.0_R64P
- ALPHA2 = 0.0_R64P
- ALPHA3 = 0.0_R64P
- ALPHA4 = 0.0_R64P
- BETA1 = 0.0_R64P
- BETA2 = 0.0_R64P
- SCATMAT = 0.0_R64P
- ! Scakalrs
- CEXT = 0._R64P
- CSCA = 0._R64P
- W = 0._R64P
- COSPH = 0._R64P
- REFF = 0._R64P
- VEFF = 0._R64P
- 
-    call TMATRIX_Driver(RAT,NDISTR,AXMAX,NPNAX,B,GAM,NKMAX,EPS,  &
-                        NP,LAM,MRR,MRI,DDELT,NPNA,NDGS,CEXT,CSCA, &
-                        W,COSPH,REFF,VEFF,ALPHA1,ALPHA2,ALPHA3,   &
-                        ALPHA4,BETA1,BETA2,SCATMAT           )
-    
-    
-    print*, " Dumping output values from the T-MATRIX model "
-    print*, CEXT,CSCA,W,COSPH,REFF,VEFF
-    print*, SCATMAT
-!    write(6,10)  CEXT
-!10  format("CEXT=",F10.15)
-!    write(6,20)  CSCA
-!20  format("CSCA=",F10.15)
-!    write(6,30)  W
-!30  format("W=",F10.15)
-!    write(6,40)  COSPH
-!40  format("COSPH=",F10.15)
-!    write(6,50)  REFF
-!50  format("REFF=",F10.15)
-!    write(6,60)  VEFF
-!60  format("VEFF=",F10.15)
-    
-   ! do i = 1, 6
-   !     do j = 1, 19
-   !         write(6,70) SCATMAT(i,j)
-!70          format("Complex Amplitude values: ",F10.15)
-!        end do
-!    end do
-    print*, " End of model dump "
- 
+program TMatrix_MPS_Test
+     use mod_kinds, only : int4, dp
+     use mod_tmatrix_mps, only : tmatrix_mps_driver,NANGMAX
+     use IFPORT, only : dclock
+     implicit none
 
+     ! Arguments
+     integer(kind=int4), parameter :: nLp = 15
+     integer(kind=int4), parameter :: nL  = 10
+     integer(kind=int4), parameter :: np  = 20
+     integer(kind=int4), parameter :: MXINT = 200
+     integer(kind=int4), parameter :: NADD  = 0
+     integer(kind=int4), parameter :: idscmt = 1
+     integer(kind=int4), parameter :: irat   = 1
+     integer(kind=int4), parameter :: Mie    = 0
+     real(kind=dp),      parameter :: small = 0.000001_dp
+     real(kind=dp),      parameter :: sang  = 1.0_dp
+     real(kind=dp),      parameter :: w     = 0.029979246_dp
+     !real(kind=dp), dimension(9)     :: tmp
+     real(kind=dp), dimension(3,nLp) :: shp
+     real(kind=dp), dimension(9,nLp) :: r0
+     integer(kind=int4), dimension(nLp) :: idshp 
+     ! results
+     real(kind=dp),   dimension(4,4,NANGMAX) :: mue
+     !DIR$ ATTRIBUTES ALIGN : 64 :: mue
+     real(kind=dp),   dimension(NANGMAX)     :: dang,inat,pol,i11,i21,i12,i22
+     !DIR$ ATTRIBUTES ALIGN : 64 :: dang
+     !DIR$ ATTRIBUTES ALIGN : 64 :: inat
+     !DIR$ ATTRIBUTES ALIGN : 64 :: pol
+     !DIR$ ATTRIBUTES ALIGN : 64 :: i11
+     !DIR$ ATTRIBUTES ALIGN : 64 :: i21
+     !DIR$ ATTRIBUTES ALIGN : 64 :: i12
+     !DIR$ ATTRIBUTES ALIGN : 64 :: i22
+     real(kind=dp),   dimension(nLp)         :: cexti,cabsi,cscai,assymi,cpri
+     real(kind=dp) :: cext,cabs,csca,assym,cabsv,   &
+                      cscav,cbakv,cprv,cexts,cabss, &
+                      cscas,cbaks,cprs
+     real(kind=dp) :: start,end,duration
+     ! Initialization
+     !
+     idshp   = 0
+     r0(1,:) = 0.5_dp
+     r0(2,:) = 0.0_dp
+     r0(3,:) = 0.0_dp
+     r0(4,:) = 0.0_dp
+     r0(5,:) = 0.0_dp
+     r0(6,:) = 0.0_dp
+     r0(7,:) = 3.0_dp
+     r0(8,:) = 1.6_dp
+     r0(9,:) = 0.02_dp
+     shp    = 0.0_dp
+     mue    = 0.000000001_dp
+     dang   = 0.000000001_dp
+     inat   = 0.000000001_dp
+     pol    = 0.000000001_dp
+     i11    = 0.000000001_dp
+     i21    = 0.000000001_dp
+     i12    = 0.000000001_dp
+     i22    = 0.000000001_dp
+     cexti  = 0.000000001_dp
+     cabsi  = 0.000000001_dp
+     cscai  = 0.000000001_dp
+     assymi = 0.000000001_dp
+     cpri   = 0.000000001_dp
+     cext   = 0.000000001_dp
+     cabs   = 0.000000001_dp
+     csca   = 0.000000001_dp
+     assym  = 0.000000001_dp
+     cabsv  = 0.000000001_dp
+     cscav  = 0.000000001_dp
+     cbakv  = 0.000000001_dp
+     cprv   = 0.000000001_dp
+     cexts  = 0.000000001_dp
+     cabss  = 0.000000001_dp
+     cscas  = 0.000000001_dp
+     cbaks  = 0.000000001_dp
+     cprs   = 0.000000001_dp
+     start  = 0.0_dp
+     end    = 0.0_dp
+     duration = 0.0_dp
+     ! Crude time measurement
+     start = dclock()
+     call tmatrix_mps_driver(nLp,np,Mie,small,MXINT,NADD,idscmt,sang,w,irat,  &
+                             nL,idshp,shp,r0,cext,cabs,csca,assym,cextv,cabsv, &
+                             cscav,cbakv,cprv,cexts,cabss,cscas,cbaks,cprs,    &
+                             dang,inat,pol,i11,i21,i12,i22,cexti,cabsi,cscai,  &
+                             assymi,cpri,mue)
+     end = dclock()
+     duration = end - start
+     print*, "tmatrix_mps_driver -- total execution time: ", duration, " microseconds."
 
-
-
-
-
-
-
-
-end program   Tmatrix_Test
+     
+end program TMatrix_MPS_Test
