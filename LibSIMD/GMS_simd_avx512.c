1c84a733c9b511798c211c4c403b6e88867a2196
Bernard Gingold <beniekg@gmail.com>
2019-11-17 18:08:34 +0100

    Fixing a few errors

--- LibSIMD/GMS_simd_avx512.c
+++ LibSIMD/GMS_simd_avx512.c
@@ -10,9 +10,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_add_ps(v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_add_ps(*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_sub_pd(v8f64 a, v8f64 b) {
         __m512d zmm = _mm512_sub_pd(*(__m512d*)&a,*(__m512d*)&b);
         return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_sub_ps(v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_sub_ps(*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mul_pd(v8f64 a, v8f64 b) {
@@ -20,9 +30,19 @@
         return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mul_ps(v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_mul_ps(*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_div_pd(v8f64 a, v8f64 b) {
         __m512d zmm = _mm512_div_pd(*(__m512d*)&a,*(__m512d*)&b);
         return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_div_ps(v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_div_ps(*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_ceil_pd(v8f64 src, __mmask8 k, v8f64 a) {
@@ -30,9 +50,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_ceil_ps(v16f32 src, __mmask16 k, v16f32 a) {
+        __m512 zmm = _mm512_mask_ceil_ps(*(__m512*)&src,k,*(__m512*)&a);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_ceil_pd(v8f64 a) {
         __m512d zmm = _mm512_ceil_pd(*(__m512d*)&a);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_ceil_ps(v16f32 a) {
+        __m512 zmm = _mm512_ceil_ps(*(__m512*)&a);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_abs_pd(v8f64 a) {
@@ -40,9 +70,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_abs_ps(v16f32 a) {
+        __m512 zmm = _mm512_abs_ps(*(__m512*)&a);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask_abs_pd(v8f64 a, __mmask8 k, v8f64 b) {
         __m512d zmm = _mm512_mask_abs_pd(*(__m512d*)&a,k,*(__m512d*)&b);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask_abs_ps(v16f32 a, __mmask16 k, v16f32 b) {
+        __m512 zmm = _mm512_mask_abs_ps(*(__m512*)&a,k,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_add_pd(v8f64 src, __mmask8 k, v8f64 a, v8f64 b) {
@@ -50,9 +90,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_add_ps(v16f32 src, __mmask16 k, v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_mask_add_ps(*(__m512*)&src,k,*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_maskz_add_pd(__mmask8 k, v8f64 a, v8f64 b) {
         __m512d zmm = _mm512_maskz_add_pd(k,*(__m512d*)&a,*(__m512d*)&b);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_maskz_add_ps(__mmask16 k, v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_maskz_add_ps(k,*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_add_round_pd(v8f64 a, v8f64 b, int rounding) {
@@ -60,9 +110,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_add_round_ps(v16f32 a, v16f32 b, int rounding) {
+        __m512 zmm = _mm512_add_round_ps(*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask_add_round_pd(v8f64 src, __mmask8 k, v8f64 a, v8f64 b, int rounding) {
         __m512d zmm = _mm512_mask_add_round_pd(*(__m512d*)&src,k,*(__m512d*)&a,*(__m512d*)&b,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask_add_round_ps(v16f32 src, __mmask16 k, v16f32 a, v16f32 b, int rounding) {
+        __m512 zmm = _mm512_mask_add_round_ps(*(__m512*)&src,k,*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_add_round_pd(__mmask8 k, v8f64 a, v8f64 b, int rounding) {
@@ -70,9 +130,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_add_round_ps(__mmask16 k, v16f32 a, v16f32 b, int rounding) {
+        __m512 zmm = _mm512_maskz_add_round_ps(k,*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask_div_pd(v8f64 src, __mmask8 k, v8f64 a, v8f64 b) {
         __m512d zmm = _mm512_mask_div_pd(*(__m512d*)&src,k,*(__m512d*)&a,*(__m512d*)&b);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask_div_ps(v16f32 src, __mmask16 k, v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_mask_div_ps(*(__m512*)&src,k,*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_div_pd(__mmask8 k, v8f64 a, v8f64 b) {
@@ -80,9 +150,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_div_ps(__mmask16 k, v16f32 a, v16f32 b) {
+        __m512 zmm = _mm512_maskz_div_ps(k,*(__m512*)&a,*(__m512*)&b);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_div_round_pd(v8f64 a, v8f64 b, int rounding) {
         __m512d zmm = _mm512_div_round_pd(*(__m512d*)&a,*(__m512d*)&b,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_div_round_ps(v16f32 a, v16f32 b, int rounding) {
+        __m512 zmm = _mm512_div_round_ps(*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_div_round_pd(v8f64 src, __mmask8 k, v8f64 a, v8f64 b, int rounding) {
@@ -90,9 +170,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_div_round_ps(v16f32 src, __mmask16 k, v16f32 a, v16f32 b, int rounding) {
+        __m512 zmm = _mm512_mask_div_round_ps(*(__m512*)&src,k,*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_maskz_div_round_pd(__mmask8 k, v8f64 a, v8f64 b, int rounding) {
         __m512d zmm = _mm512_maskz_div_round_pd(k,*(__m512d*)&,*(__m512d*)&b,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_maskz_div_round_ps(__mmask16 k, v16f32 a, v16f32 b, rounding) {
+        __m512 zmm = _mm512_maskz_div_round_ps(k,*(__m512*)&a,*(__m512*)&b,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_fmadd_pd(v8f64 a, v8f64 b, v8f64 c) {
@@ -100,9 +190,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_fmadd_ps(v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_fmadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask_fmadd_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_mask_fmadd_pd(*(__m512d*)&a,k,*(__m512d*)&b,*(__m512d*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask_fmadd_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_mask_fmadd_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask3_fmadd_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
@@ -110,9 +210,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask3_fmadd_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_mask3_fmadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_maskz_fmadd_pd(__mmask8 k, v8f64 a, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_maskz_fmadd_pd(k,*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_maskz_fmadd_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_maskz_fmadd_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_fmadd_round_pd(v8f64 a, v8f64 b, v8f64 c, int rounding) {
@@ -120,9 +230,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_fmadd_round_ps(v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_fmadd_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask_fmadd_round_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c, int rounding) {
         __m512d zmm = _mm512_mask_fmadd_round_pd(*(__m512d*)&a,k,*(__m512d*)&b,*(__m512d*)&c,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask_fmadd_round_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_mask_fmadd_round_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask3_fmadd_round_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k, int rounding) {
@@ -130,9 +250,20 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask3_fmadd_round_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k, int rounding){
+        __m512 zmm = _mm512_mask3_fmadd_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k,rounding);
+	return (*(v16f32*)&zmm);
+     }
+     
+
      v8f64 v8f64_maskz_fmadd_round_pd(_mmask8 k,v8f64 a, v8f64 b, v8f64 c, int rounding) {
         __m512d zmm = _mm512_maskz_fmadd_round_pd(k,*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_maskz_fmadd_round_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_maskz_fmadd_round_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_fmaddsub_pd(v8f64 a, v8f64 b, v8f64 c) {
@@ -145,9 +276,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fmaddsub_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_mask_fmaddsub_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask3_fmaddsub_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
         __m512d zmm = _mm512_mask3_fmaddsub_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask3_fmaddsub_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_mask3_fmaddsub_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_fmaddsub_pd(__mmask8 k, v8f64 a, v8f64 b, v8f64 c) {
@@ -155,9 +296,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_fmaddsub_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_maskz_fmaddsub_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_fmaddsub_round_pd(v8f64 a, v8f64 b, v8f64 c, int rounding) {
         __m512d zmm = _mm512_fmaddsub_round_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_fmaddsub_round_ps(v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_fmaddsub_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_fmaddsub_round_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c, int rounding) {
@@ -165,9 +316,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fmaddsub_round_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_mask_fmaddsub_round_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask3_fmaddsub_round_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k, int rounding) {
         __mm512d zmm = _mm512_mask3_fmaddsub_round_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask3_fmaddsub_round_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k, int rounding) {
+        __m512 zmm = _mm512_mask3_fmaddsub_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_fmaddsub_round_pd(__mmask8 k, v8f64 a, v8f64 b, v8f64 c, int rounding) {
@@ -175,9 +336,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_fmaddsub_round_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_maskz_fmaddsub_round_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_fmsub_pd(v8f64 a, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_fmsub_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_fmsub_ps(v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_fmsub_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_fmsub_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c) {
@@ -185,9 +356,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fmsub_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_mask_fmsub_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask3_fmsub_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
         __m512d zmm = _mm512_mask3_fmsub_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask3_fmsub_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_mask3_fmsub_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_fmsub_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
@@ -195,9 +376,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_fmsub_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_maskz_fmsub_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_fmsubadd_pd(v8f64 a, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_fmsubadd_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_fmsubadd_ps(v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_fmsubadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_fmsubadd_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c) {
@@ -205,9 +396,20 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fmsubadd_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_mask_fmsubadd_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
+
      v8f64 v8f64_mask3_fmsubadd_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
         __m512d zmm = _mm512_mask3_fmsubadd_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask3_fmsubadd_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_mask3_fmsubadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_fmsubadd_pd(__mmask8 k, v8f64 a, v8f64 b, v8f64 c) {
@@ -215,9 +417,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_fmsubadd_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_maskz_fmsubadd_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_fmsubadd_round_pd(v8f64 a, v8f64 b, v8f64 c, int rounding) {
         __m512d zmm = _mm512_fmsubadd_round_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_fmsubadd_round_ps(v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_fmsubadd_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_fmsubadd_round_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c, int rounding) {
@@ -225,9 +437,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fmsubadd_round_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_mask_fmsubadd_round_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask3_fmsubadd_round_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k, int rounding) {
         __m512d zmm = _mm512_mask3_fmsubadd_round_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k,rounding);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_mask3_fmsubadd_round_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k, int rounding) {
+        __m512 zmm = _mm512_mask3_fmsubadd_round_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_maskz_fmusbadd_round_pd(__mmask8 k, v8f64 a, v8f64 b, v8f64 c, int rounding) {
@@ -235,9 +457,19 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_maskz_fmsubadd_round_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c, int rounding) {
+        __m512 zmm = _mm512_maskz_fmsubadd_round_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,rounding);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_fnmadd_pd(v8f64 a, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_fnmadd_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_fnmadd_ps(v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_fnmadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_mask_fnmadd_pd(v8f64 a, __mmask8 k, v8f64 b, v8f64 c) {
@@ -245,14 +477,29 @@
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask_fnmadd_ps(v16f32 a, __mmask16 k, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_mask_fnmadd_ps(*(__m512*)&a,k,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_mask3_fnmadd_pd(v8f64 a, v8f64 b, v8f64 c, __mmask8 k) {
         __m512d zmm = _mm512_mask3_fnmadd_pd(*(__m512d*)&a,*(__m512d*)&b,*(__m512d*)&c,k);
 	return (*(v8f64*)&zmm);
      }
 
+     v16f32 v16f32_mask3_fnmadd_ps(v16f32 a, v16f32 b, v16f32 c, __mmask16 k) {
+        __m512 zmm = _mm512_mask3_fnmadd_ps(*(__m512*)&a,*(__m512*)&b,*(__m512*)&c,k);
+	return (*(v16f32*)&zmm);
+     }
+
      v8f64 v8f64_maskz_fnmadd_pd(__mmask8 k,v8f64 a, v8f64 b, v8f64 c) {
         __m512d zmm = _mm512_maskz_fnmadd_pd(k,*(__m512d*)&a,*(__m512d*)&b,*(__m512*)&c);
 	return (*(v8f64*)&zmm);
+     }
+
+     v16f32 v16f32_maskz_fnmadd_ps(__mmask16 k, v16f32 a, v16f32 b, v16f32 c) {
+        __m512 zmm = _mm512_maskz_fnmadd_ps(k,*(__m512*)&a,*(__m512*)&b,*(__m512*)&c);
+	return (*(v16f32*)&zmm);
      }
 
      v8f64 v8f64_fnmadd_round_pd(v8f64 a, v8f64 b, v8f64 c, int rounding) {
@@ -315,24 +562,24 @@
 	return (*(v8f64*)&zmm);
      }
 
-     v8f64 v8f64_mask_reduce_add_pd(__mmask8 k, v8f64 a) {
-        __m512d zmm = _mm512_mask_reduce_add_pd(k,*(__m512d*)&a);
-	return (*(v8f64*)&zmm);
+     double v8f64_mask_reduce_add_pd(__mmask8 k, v8f64 a) {
+      return(  _mm512_mask_reduce_add_pd(k,*(__m512d*)&a));
+
      }
 
-     v8f64 v8f64_reduce_add_pd(v8f64 a) {
-        __m512d zmm = _mm512_reduce_add_pd(*(__m512d*)&a);
-	return (*(v8f64*)&zmm);
+     double v8f64_reduce_add_pd(v8f64 a) {
+       return (  _mm512_reduce_add_pd(*(__m512d*)&a));
+
      }
 
-     v8f64 v8f64_mask_reduce_mul_pd(__mmask8 k, v8f64 a) {
-        __m512d zmm = _mm512_mask_reduce_mul_pd(k,*(__m512d*)&a);
-	return (*(v8f64*)&zmm);
+     double  v8f64_mask_reduce_mul_pd(__mmask8 k, v8f64 a) {
+       return ( _mm512_mask_reduce_mul_pd(k,*(__m512d*)&a));
+
      }
 
-     v8f64 v8f64_reduce_mul_pd(v8f64 a) {
-        __m512d zmm = _mm512_reduce_mul_pd(*(__m512d*)&a);
-	return (*(v8f64*)&zmm);
+     double  v8f64_reduce_mul_pd(v8f64 a) {
+       return ( _mm512_reduce_mul_pd(*(__m512d*)&a));
+
      }
 
      v8f64 v8f64_mask_sub_pd(v8f64 src, __mmask8 k, v8f64 a, v8f64 b) {
@@ -838,5 +1085,7 @@
         __m512d zmm = _mm512_permute_pd(*(__m512d*)&a,imm8);
 	return (*(v8f64*)&zmm);
      }
+
+     
 
      
