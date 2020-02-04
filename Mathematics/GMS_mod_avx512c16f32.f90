13662b4f333f73957d724be2a23058f89e76bed6
Bernard Gingold <beniekg@gmail.com>
2020-02-04 17:21:58 +0100

    Adding complex operators
    Work in progress....

--- Mathematics/GMS_mod_avx512c16f32.f90
+++ Mathematics/GMS_mod_avx512c16f32.f90
@@ -78,41 +78,41 @@ module  mod_avx512c16f32
 
      interface operator (+)
          module procedure c16_add_c16
-         module procedure c16_add_c2
-         module procedure c16_add_v8
+         module procedure c16_add_c1
+         module procedure c16_add_v16
          module procedure c16_add_s1
-         module procedure c2_add_c16
-         module procedure v8_add_c16
+         module procedure c1_add_c16
+         module procedure v16_add_c16
          module procedure s1_add_c16
      end interface operator (+)
 
      interface operator (-)
          module procedure c16_sub_c16
-         module procedure c16_sub_c2
-         module procedure c16_sub_v8
+         module procedure c16_sub_c1
+         module procedure c16_sub_v16
          module procedure c16_sub_s1
-         module procedure c2_sub_c16
-         module procedure v8_sub_c16
+         module procedure c1_sub_c16
+         module procedure v16_sub_c16
          module procedure s1_sub_c16
      end interface operator (-)
 
      interface operator (*)
          module procedure c16_mul_c16
-         module procedure c16_mul_c2
-         module procedure c16_mul_v8
+         module procedure c16_mul_c1
+         module procedure c16_mul_v16
          module procedure c16_mul_s1
-         module procedure c2_mul_c16
-         module procedure v8_mul_c16
+         module procedure c1_mul_c16
+         module procedure v16_mul_c16
          module procedure s1_mul_c16
      end interface operator (*)
 
      interface operator (/)
          module procedure c16_div_c16
-         module procedure c16_div_c2
-         module procedure c16_div_v8
+         module procedure c16_div_c1
+         module procedure c16_div_v16
          module procedure c16_div_s1
-         module procedure c2_div_c16
-         module procedure v8_div_c16
+         module procedure c1_div_c16
+         module procedure v16_div_c16
          module procedure s1_div_c16
      end interface operator (/)
 
@@ -120,15 +120,15 @@ module  mod_avx512c16f32
 
       interface operator (==)
          module procedure c16_eq_c16
-         module procedure c16_eq_c2
+         module procedure c16_eq_c1
        
-         module procedure c2_eq_c16
+         module procedure c1_eq_c16
         
       end interface operator (==)
 
       interface operator (/=)
          module procedure c16_neq_c16
-         module procedure c16_neq_c2
+         module procedure c16_neq_c1
        
          module procedure c2_neq_c16
       
@@ -136,49 +136,49 @@ module  mod_avx512c16f32
 
       interface operator (>)
          module procedure c16_gt_c16
-         module procedure c16_gt_c2
+         module procedure c16_gt_c1
         
-         module procedure c2_gt_c16
+         module procedure c1_gt_c16
      
       end interface operator (>)
 
       interface operator (<)
          module procedure c16_lt_c16
-         module procedure c16_lt_c2
+         module procedure c16_lt_c1
         
-         module procedure c2_lt_c16
+         module procedure c1_lt_c16
         
       end interface operator (<)
 
       interface operator (>=)
          module procedure c16_ge_c16
-         module procedure c16_ge_c2
+         module procedure c16_ge_c1
        
-         module procedure c2_ge_c16
+         module procedure c1_ge_c16
         
       end interface operator (>=)
 
       interface operator (<=)
          module procedure c16_le_c16
-         module procedure c16_le_c2
+         module procedure c16_le_c1
         
-         module procedure c2_le_c16
+         module procedure c1_le_c16
         
       end interface operator (<=)
 #else
      interface operator (==)
          module procedure c16_eq_c16
-         module procedure c16_eq_c2
+         module procedure c16_eq_c1
        
-         module procedure c2_eq_c16
+         module procedure c1_eq_c16
         
       end interface operator (==)
 
       interface operator (/=)
          module procedure c16_neq_c16
-         module procedure c16_neq_c2
+         module procedure c16_neq_c1
        
-         module procedure c2_neq_c16
+         module procedure c1_neq_c16
       
       end interface operator (/=)
 
@@ -192,25 +192,25 @@ module  mod_avx512c16f32
 
       interface operator (<)
          module procedure c16_lt_c16
-         module procedure c16_lt_c2
+         module procedure c16_lt_c1
         
-         module procedure c2_lt_c16
+         module procedure c1_lt_c16
         
       end interface operator (<)
 
       interface operator (>=)
          module procedure c16_ge_c16
-         module procedure c16_ge_c2
+         module procedure c16_ge_c1
        
-         module procedure c2_ge_c16
+         module procedure c1_ge_c16
         
       end interface operator (>=)
 
       interface operator (<=)
          module procedure c16_le_c16
-         module procedure c16_le_c2
+         module procedure c16_le_c1
         
-         module procedure c2_le_c16
+         module procedure c1_le_c16
         
       end interface operator (<=)
 
@@ -224,7 +224,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: default_init
       pure function default_init() result(iq)
         !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: default_init
-        !DIR$ ATTRIBUTES VECTOR :: default_init
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: default_init
       
 #endif
         use mod_vecconsts, only : v16_n0
@@ -244,7 +244,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: array_init
       pure function array_init(re,im) result(iq)
          !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: array_init
-        !DIR$ ATTRIBUTES VECTOR :: array_init
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: array_init
 #endif
         real(kind=sp), dimension(0:15), intent(in) :: re
         real(kind=sp), dimension(0:15), intent(in) :: im
@@ -264,7 +264,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: complex1_init
       pure function complex1_init(c) result(iq)
         !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: complex1_init
-        
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: complex_init
 #endif
         complex(kind=sp),     intent(in) :: c
 #if defined __INTEL_COMPILER 
@@ -283,7 +283,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: complex2x16_init
       pure function complex2x16_init(c) result(iq)
         !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: complex2x16_init
-        !DIR$ ATTRIBUTES VECTOR :: complex2x16_init
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: complex2x16_init
 #endif
         complex(kind=sp), dimension(0:15),  intent(in) :: c
 #if defined __INTEL_COMPILER 
@@ -302,7 +302,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: zmm16r42x_init
       pure function zmm16r42x_init(v1,v2) result(iq)
         !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: zmm16r42x_init
-        !DIR$ ATTRIBUTES VECTOR :: zmm16r42x_init
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: zmm16r42x_init
 #endif
         type(ZMM16r4_t),    intent(in) :: v1
         type(ZMM16r4_t),    intent(in) :: v2
@@ -322,7 +322,7 @@ module  mod_avx512c16f32
         !DIR$ ATTRIBUTES INLINE :: zmm16r41x_init
       pure function zmm16r41x_init(v) result(iq)
         !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: zmm16r41x_init
-        !DIR$ ATTRIBUTES VECTOR :: zmm16r41x_init
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: zmm16r41x_init
 #endif
         type(ZMM16r4_t),    intent(in) :: v
 #if defined __INTEL_COMPILER 
@@ -352,5 +352,192 @@ module  mod_avx512c16f32
         iq.re = s
         iq.im = v16_n0
       end function r41x_init
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function copy_init(rhs) result(iq) !GCC$ ATTRIBUTES cold :: copy_init !GCC$ ATTRIBUTES vectorcall :: copy_init !GCC$ ATTRIBUTES inline :: copy_init
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: copy_init
+      pure function copy_init(rhs) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: copy_init
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: copy_init
+#endif
+        type(AVX512c16f32_t),    intent(in) :: rhs
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+         iq = rhs
+      end function copy_init
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c16_add_c16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c16_add_c16 !GCC$ ATTRIBUTES vectorcall :: c16_add_c16 !GCC$ ATTRIBUTES inline :: c16_add_c16
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: c16_add_c16
+      pure function c16_add_c16(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c16_add_c16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c16_add_c16
+#endif
+        type(AVX512c16f32_t),    intent(in) :: x
+        type(AVX512c16f32_t),    intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.re+y.re
+        iq.im = x.im+y.im
+      end function c16_add_c16
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c16_add_c1(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c16_add_c1 !GCC$ ATTRIBUTES vectorcall :: c16_add_c1 !GCC$ ATTRIBUTES inline :: c16_add_c1
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: c16_add_c1
+      pure function c16_add_c1(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c16_add_c1
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c16_add_c1
+#endif
+        type(AVX512c16f32_t),    intent(in) :: x
+        complex(kind=sp),        intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.re+real(y,kind=sp)
+        iq.im = x.im+aimag(y,kind=sp)
+      end function c16_add_c1
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c16_add_v16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c16_add_v16 !GCC$ ATTRIBUTES vectorcall :: c16_add_v16 !GCC$ ATTRIBUTES inline :: c16_add_v16
+#elif defined __ICC || defined __INTEL_COMPILER
+         !DIR$ ATTRIBUTES INLINE :: c16_add_v16
+      pure function c16_add_v16(x,y) result(iq)
+         !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c16_add_v16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c16_add_v16
+#endif
+        use mod_vecconsts, only : v16_n0
+        type(AVX512c16f32_t),    intent(in) :: x
+        type(ZMM16r4_t),         intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.re+y.v
+        iq.im = v16_n0
+      end function c16_add_v16
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c16_add_s1(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c16_add_s1 !GCC$ ATTRIBUTES vectorcall :: c16_add_s1 !GCC$ ATTRIBUTES inline :: c16_add_s1
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: c16_add_s1
+      pure function c16_add_s1(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c16_add_s1
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c16_add_s1
+#endif
+        use mod_veccconts, only : v16_n0
+        type(AVX512c16f32_t),    intent(in) :: x
+        real(kind=sp),           intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.re+y
+        iq.im = v16_n0
+      end function c16_add_s1
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c1_add_c16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c1_add_c16 !GCC$ ATTRIBUTES vectorcall :: c1_add_c16 !GCC$ ATTRIBUTES inline :: c1_add_c16
+#elif defined __ICC || defined __INTEL_COMPILER
+      !DIR$ ATTRIBUTES INLINE :: c1_add_c16
+      pure function c1_add_c16(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c1_add_c16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c1_add_c16
+#endif
+        complex(kind=sp),      intent(in) :: x
+        type(AVX512c16f32_t),  intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = real(x,kind=sp)+y.re
+        iq.im = aimag(x,kind=sp)+y.im
+      end function c1_add_c16
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function v16_add_c16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: v16_add_c16 !GCC$ ATTRIBUTES vectorcall :: v16_add_c16 !GCC$ ATTRIBUTES inline :: v16_add_c16
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: v16_add_c16
+      pure function v16_add_c16(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: v16_add_c16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: v16_add_c16
+#endif
+        use mod_vecconsts, only : v16_n0
+        type(ZMM16r4_t),      intent(in) :: x
+        type(AVX512c16f32_t), intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.v+y.re
+        iq.im = v16_n0
+      end function v16_add_c16
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function s1_add_c16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: s1_add_c16 !GCC$ ATTRIBUTES vectorcall :: s1_add_c16 !GCC$ ATTRIBUTES inline :: s1_add_c16
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: s1_add_c16
+      pure function s1_add_c16(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: s1_add_c16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: s1_add_c16
+#endif
+         use mod_vecconsts, only : v16_n0
+        real(kind=sp),         intent(in) :: x
+        type(AVX512c16f32_t),  intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = s1
+        iq.im = v16_n0
+      end function s1_add_c16
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      pure function c16_sub_c16(x,y) result(iq) !GCC$ ATTRIBUTES hot :: c16_sub_c16 !GCC$ ATTRIBUTES vectorcall :: c16_sub_c16 !GCC$ ATTRIBUTES inline :: c16_sub_c16
+#elif defined __ICC || defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: c16_sub_c16
+      pure function c16_sub_c16(x,y) result(iq)
+        !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: c16_sub_c16
+        !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: c16_sub_c16
+#endif
+        type(AVX512c16f32_t),     intent(in) :: x
+        type(AVX512c16f32_t),     intent(in) :: y
+#if defined __INTEL_COMPILER 
+        !DIR$ ATTRIBUTES ALIGN : 64 :: iq
+        type(AVX512c16f32_t) :: iq
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+        type(AVX512c16f32_t) :: iq !GCC$ ATTRIBUTES aligned(64) :: iq
+#endif
+        iq.re = x.re-y.re
+        iq.im = x.im-y.im
+      end function c16_sub_c16
+        
+        
+        
+      
+      
       
 end module mod_avx512c16f32
