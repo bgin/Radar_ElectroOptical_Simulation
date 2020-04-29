486c0a603ff306387912aaab72af4cdf78fb95e9
Bernard Gingold <beniekg@gmail.com>
2020-04-29 18:13:43 +0200

    Adding linpack functions

--- Mathematics/GMS_matrix_computations.f90
+++ Mathematics/GMS_matrix_computations.f90
@@ -254,7 +254,7 @@ module mod_matrix_computations
           complex(kind=sp)   :: t
           complex(kind=sp)   :: wk
           complex(kind=sp)   :: wkm
-          real(kind=sp)      :: t0,t1,t2,t3
+          real(kind=sp)      :: t0,t1,t2,t3,t4,t5
           real(kind=sp)      :: anorm
           real(kind=sp)      :: s
           real(kind=sp)      :: sm
@@ -330,67 +330,309 @@ module mod_matrix_computations
 !
 !  Solve L * V = Y.
 !
-  do k = 1, n
-
-    l = ipvt(k)
-
-    t = z(l)
-    z(l) = z(k)
-    z(k) = t
-
-    if ( k < n ) then
-      z(k+1:n) = z(k+1:n) + t * a(k+1:n,k)
-    end if
-
-    if ( 1.0E+00 < cabs1 ( z(k) ) ) then
-      s = 1.0E+00 / cabs1 ( z(k) )
-      z(1:n) = z(1:n) * s
-      ynorm = s * ynorm
-    end if
-
-  end do
-
-  s = 1.0E+00 / scasum ( n, z, 1 )
-  z(1:n) = z(1:n) * s
-  ynorm = s * ynorm
+         do k = 1, n
+            l = ipvt(k)
+            t = z(l)
+            z(l) = z(k)
+            z(k) = t
+            if( k < n ) then
+                z(k+1:n) = z(k+1:n) + t * a(k+1:n,k)
+             end if
+            t3 = cabs1(z(k))
+            if(1.0E+00_sp < t3) then
+               s = 1.0E+00_sp / t3
+               z(1:n) = z(1:n) * s
+               ynorm = s * ynorm
+            end if
+         end do
+         s = 1.0E+00_sp / scasum(n,z,1)
+         z(1:n) = z(1:n) * s
+         ynorm = s * ynorm
 !
 !  Solve U * Z = V.
 !
-  do k = n, 1, -1
-
-    if ( cabs1 ( a(k,k) ) < cabs1 ( z(k) ) ) then
-      s = cabs1 ( a(k,k) ) / cabs1 ( z(k) )
-      z(1:n) = z(1:n) * s
-      ynorm = s * ynorm
-    end if
-
-    if ( cabs1 ( a(k,k) ) /= 0.0E+00 ) then
-      z(k) = z(k) / a(k,k)
-    else
-      z(k) = cmplx ( 1.0E+00, 0.0E+00 )
-    end if
-
-    t = -z(k)
-    z(1:k-1) = z(1:k-1) + t * a(1:k-1,k)
-
-  end do
+         do k = n, 1, -1
+            t4 = cabs1(a(k,k))
+            t5 = cabs1(z(k))
+            if(t4 < t5) then
+               s = t4 / t5
+               z(1:n) = z(1:n) * s
+               ynorm = s * ynorm
+            end if
+            if(t4 /= 0.0E+00_sp) then
+               z(k) = z(k) / a(k,k)
+            else
+               z(k) = cmplx(1.0E+00_sp, 0.0E+00_sp)
+            end if
+            t = -z(k)
+            z(1:k-1) = z(1:k-1) + t * a(1:k-1,k)
+          end do
 !
 !  Make ZNORM = 1.
 !
-  s = 1.0E+00 / scasum ( n, z, 1 )
-  z(1:n) = z(1:n) * s
-  ynorm = s * ynorm
+          s = 1.0E+00_sp / scasum(n, z, 1)
+          z(1:n) = z(1:n) * s
+          ynorm = s * ynorm
+          if(anorm /= 0.0E+00_sp) then
+             rcond = ynorm / anorm
+          else
+             rcond = 0.0E+00
+          end if
+     end subroutine cgeco
 
-  if ( anorm /= 0.0E+00 ) then
-    rcond = ynorm / anorm
-  else
-    rcond = 0.0E+00
-  end if
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    07 May 2006
+!
+!  Author:
+!
+!    FORTRAN90 version by John Burkardt
+!
+!  Reference:
+!
+!    Jack Dongarra, Jim Bunch, Cleve Moler, Pete Stewart,
+!    LINPACK User's Guide,
+!    SIAM, 1979,
+!    ISBN13: 978-0-898711-72-1,
+     !    LC: QA214.L56.
 
-      end subroutine cgeco
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     subroutine cgefa(a,lda,n,ipvt,info) !GCC$ ATTRIBUTES hot :: cgefa !GCC$ ATTRIBUTES aligned(16) :: cgefa !GCC$ ATTRIBUTES inline :: cgefa
+#elif defined __ICC || defined __INTEL_COMPILER
+       !DIR$ ATTRIBUTES INLINE :: cgefa
+     subroutine cgefa(a,lda,n,ipvt,info)
+       !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: cgefa
+#endif
+          complex(kind=sp),    dimension(lda,n),  intent(inout) :: a
+          integer(kind=int4),                     intent(in)    :: lda
+          integer(kind=int4),                     intent(in)    :: n
+          integer(kind=int4),  dimension(n),      intent(out)   :: ipvt
+          integer(kind=int4),                     intent(out)   :: info
+          ! Locals
+          complex(kind=sp) :: t
+          integer(kind=int4) :: j,k,l
+          real(kind=sp) :: t0
+!
+!  Gaussian elimination with partial pivoting.
+!
+          info = 0
+          t0   = 0.0_sp
+          do k = 1, n - 1
+!
+!  Find L = pivot index.
+!
+               l = icamax(n-k+1,a(k:n,k),1) + k - 1
+               ipvt(k) = l
+!
+!  Zero pivot implies this column already triangularized.
+            !
+               t0 = cabs1(a(l,k))
+               if(t0 == 0.0E+00_sp ) then
+                   info = k
+                   cycle
+               end if
+!
+!  Interchange if necessary.
+!
+               if(l /= k) then
+                  t      = t0
+                  a(l,k) = a(k,k)
+                  a(k,k) = t
+               end if
+!
+!  Compute multipliers
+!
+               t = -cmplx(1.0E+00_sp, 0.0E+00_sp) / a(k,k)
+               a(k+1:n,k) = a(k+1:n,k) * t
+!
+!  Row elimination with column indexing
+!
+               do j = k+1, n
+                   t = a(l,j)
+                   if(l /= k) then
+                      a(l,j) = a(k,j)
+                      a(k,j) = t
+                   end if
+                   a(k+1:n,j) = a(k+1:n,j) + t * a(k+1:n,k)
+               end do
+          end do
+          ipvt(n) = n
+          if(cabs1(a(n,n)) == 0.0E+00_sp ) then
+              info = n
+           end if
+     end subroutine cgefa
 
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    11 April 2006
+!
+!  Author:
+!
+!    FORTRAN90 version by John Burkardt
+!
+!  Reference:
+!
+!    Jack Dongarra, Jim Bunch, Cleve Moler, Pete Stewart,
+!    LINPACK User's Guide,
+!    SIAM, 1979,
+!    ISBN13: 978-0-898711-72-1,
+     !    LC: QA214.L56.
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     function icamax(n,x,incx) !GCC$ ATTRIBUTES hot :: icamax !GCC$ ATTRIBUTES aligned(64) :: icamax !GCC$ ATTRIBUTES inline :: icamax
+#elif defined __ICC || defined __INTEL_COMPILER
+       !DIR$ ATTRIBUTES INLINE :: icamax
+     function icamax(n,x,incx)
+         !DIR$ ATTRIBUTES CODE_ALIGN : 64 :: icamax
+         integer(kind=int4),              intent(in) :: n
+         complex(kind=sp),  dimension(*), intent(in) :: x
+         integer(kind=int4),              intent(in) :: incx
+         ! Locals
+         integer(kind=int4) :: icamax
+         real(kind=sp) :: smax
+         icamax = 0
+         if(n < 1 .or.incx <= 0) then
+              return
+         end if
+         if(n == 1) then
+             return
+         end if  
+         icamax = 1
+         if(incx /= 1) then
+            ix = 1
+            smax = cabs1(x(1))
+            ix = ix + incx
+            do i = 2, n
+               if(smax < cabs1(x(ix))) then
+                  icamax = i
+                  smax = cabs1(x(ix))
+               end if
+               ix = ix + incx
+            end do
+         else
+            smax = cabs1(x(1))
+            do i = 2, n
+               if(smax < cabs1(x(i))) then
+                  icamax = i
+                  smax = cabs1(x(i))
+               end if
+            end do
+         end if
+     end function icamax
+
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    14 May 2004
+!
+!  Author:
+!
+!    FORTRAN90 version by John Burkardt
+!
+!  Parameters:
+     !
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     function csign1(z1,z2) !GCC$ ATTRIBUTES hot :: csign1 !GCC$ ATTRIBUTES aligned(64) :: csign1 !GCC$ ATTRIBUTES inline :: csign1
+#elif defined __ICC || defined __INTEL_COMPILER
+         !DIR$ ATTRIBUTES INLINE :: csign1
+     function csign4(z1,z2)
+       !DIR$ CODE_ALIGN : 64 :: csign1
+#endif
+           complex(kind=sp),    intent(in) :: z1
+           complex(kind=sp),    intent(in) :: z2
+           ! LOcals
+           complex(kind=sp) :: csign1
+           if(cabs1(z2) == 0.0E+00_sp) then
+                csign1 = cmplx(0.0E+00_sp, 0.0E+00_sp)
+           else
+                csign1 = cabs1(z1) * (z2 / cabs1(z2))
+           end if
+     end function csign1
+ 
+     
         
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    22 May 2002
+!
+!  Author:
+!
+!    FORTRAN90 version by John Burkardt
+!
+!  Reference:
+!
+!    Jack Dongarra, Jim Bunch, Cleve Moler, Pete Stewart,
+!    LINPACK User's Guide,
+!    SIAM, 1979,
+!    ISBN13: 978-0-898711-72-1,
+!    LC: QA214.L56.
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     function cabs1(z) !GCC$ ATTRIBUTES hot :: cabs1 !GCC$ ATTRIBUTES aligned(64) :: cabs1 !GCC$ ATTRIBUTES inline :: cabs1
+#elif defined __ICC || defined __INTEL_COMPILER
+       !DIR$ ATTRIBUTES INLINE :: cabs1
+       function cabs1(z)
+       !DIR$ ATTRIBUTES CODE_ALIGN : 64 :: cabs1
+#endif
+         complex(kind=sp),     intent(in) :: z
+         real(kind=sp) :: cabs1
+         cabs1 = abs(real(z)) + abs(aimag(z))
+     end function cabs1
 
+!  Modified:
+!
+!    11 April 2006
+!
+!  Author:
+!
+!    FORTRAN90 version by John Burkardt
+!
+!  Reference:
+!
+!    Jack Dongarra, Jim Bunch, Cleve Moler, Pete Stewart,
+!    LINPACK User's Guide,
+!    SIAM, 1979,
+!    ISBN13: 978-0-898711-72-1,
+!    LC: QA214.L56.
+!
+!    Charles Lawson, Richard Hanson, David Kincaid, Fred Krogh,
+!    Basic Linear Algebra Subprograms for FORTRAN usage,
+!    ACM Transactions on Mathematical Software,
+!    Volume 5, Number 3, pages 308-323, 1979.
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     function scasum(n,x,incx) !GCC$ ATTRIBUTES hot :: scasum !GCC$ ATTRIBUTES aligned(64) :: scasum !GCC$ ATTRIBUTES inline :: scasum
+#elif defined __ICC || defined __INTEL_COMPILER
+       !DIR$ ATTRIBUTES INLINE :: scasum
+     function scasum(n,x,incx)
+         !DIR$ ATTRIBUTES CODE_ALIGN : 64 :: scasum
+#endif
+          integer(kind=int4),             intent(in) :: n
+          complex(kind=sp), dimension(*), intent(in) :: x
+          integer(kind=int4),             intent(in) :: incx
+          ! Locals
+          real(kind=sp) :: scasum
+          if(n <= 0 .or. incx <= 0) then
+             return
+          end if
+          scasum = 0.0_sp
+          if(incx == 1) then
+               scasum = sum(abs(real(x(1:n))) + abs(aimag(x(1:n))))
+          else
+               nincx = n * incx
+               scasum = sum(abs(real(x(1:nincx:incx))) &
+                      + abs(aimag(x(1:nincx:incx))))
+          end if
+     end function scasum
 
         
 
