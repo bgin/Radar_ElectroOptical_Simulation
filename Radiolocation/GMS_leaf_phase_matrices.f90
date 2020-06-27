eb092a97b71b901da4635041a9286d572523d972
Bernard Gingold <beniekg@gmail.com>
2020-06-27 17:23:57 +0200

    Add files via upload

--- Radiolocation/GMS_leaf_phase_matrices.f90
+++ Radiolocation/GMS_leaf_phase_matrices.f90
@@ -45,6 +45,9 @@ module  mod_leaf_phase_matrices
      character(*),         parameter :: MOD_LEAF_PHASE_MATRICES_ID = &
           "$Id: GMS_leaf_phase_matrices.f90 1000 +00200 2020-06-20 16:09 beniekg@gmail.com $"
 
+
+     
+
    contains
 
 #if defined __GFORTRAN__ && !defined __INTEL_COMPILER
@@ -52,8 +55,7 @@ module  mod_leaf_phase_matrices
                                             eig1x4lp,eig1x4ln,eig4x4mp,     &
                                             eig4x4mn,eig4x4mpi,eig4x4mni,   &
                                             expa4x4mp,expa4x4mn,stokes4x4m, &
-                                            scat2x2m,thinc,phinc,thsc,phsc, &
-                                            thdr,phdr,z,delta,lvar,    &
+                                            scat2x2m, ,z,delta,lvar,    &
                                             crown_height,trunk_height,dens, &
                                             ldiam,lthick,lmg,lrho,ldens,theta,&
                                             ctheta,stheta,leaves_epsr, &
@@ -63,8 +65,7 @@ module  mod_leaf_phase_matrices
                                             eig1x4lp,eig1x4ln,eig4x4mp,     &
                                             eig4x4mn,eig4x4mpi,eig4x4mni,   &
                                             expa4x4mp,expa4x4mn,stokes4x4m, &
-                                            scat2x2m,thinc,phinc,thsc,phsc, &
-                                            thdr,phdr,z,delta,lvar,lvarv, &
+                                            scat2x2m,z,delta,lvar,lvarv, &
                                             crown_height,trunk_height,dens,  &
                                             ldiam,lthick,lmg,lrho,ldens,theta,&
                                             ctheta,stheta,leaves_epsr, &
@@ -86,12 +87,6 @@ module  mod_leaf_phase_matrices
            real(kind=sp),     dimension(4,4),    intent(inout)  :: expa4x4mn
            real(kind=sp),     dimension(4,4),    intent(outout) :: stokes4x4m
            complex(kind=sp),  dimension(2,2),    intent(outout) :: scat2x2m
-           real(kind=sp),                        intent(in)  :: thinc
-           real(kind=sp),                        intent(in)  :: phinc
-           real(kind=sp),                        intent(in)  :: thsc
-           real(kind=sp),                        intent(in)  :: phsc
-           real(kind=sp),                        intent(in)  :: thdr
-           real(kind=sp),                        intent(in)  :: phdr
            real(kind=sp),                        intent(in)  :: z
            real(kind=sp),                        intent(in)  :: delta
            real(kind=sp),                        intent(in)  :: lvar  ! leaf variation of orientation function 
@@ -134,7 +129,7 @@ module  mod_leaf_phase_matrices
            complex(kind=sp), dimension(2,2,2) :: sm2x2avg_t2 !GCC$ ATTRIBUTES aligned(64) :: sm2x2avg_t2
            complex(kind=sp), dimension(2,2,2) :: sm2x2avg_t3 !GCC$ ATTRIBUTES aligned(64) :: sm2x2avg_t3
 #endif
-           complex(kind=sp), automatic :: j,cwork
+           complex(kind=sp), automatic :: j,cwork,orient_distr
            real(kind=sp),    automatic :: tr_start1,tr_stop1,dt_rad1
            real(kind=sp),    automatic :: tr_start2,tr_stop2,dt_rad2
            real(kind=sp),    automatic :: tr_start3,tr_stop3,dt_rad3
@@ -143,6 +138,12 @@ module  mod_leaf_phase_matrices
            real(kind=sp),    automatic :: pr_start3,pr_stop3,dp_rad3
            real(kind=sp),    automatic :: dp1t1,dp2t2,dp3t3
            real(kind=sp),    automatic :: t0,norm
+           real(kind=sp),    automatic :: thinc
+           real(kind=sp),    automatic :: phinc
+           real(kind=sp),    automatic :: thsc
+           real(kind=sp),    automatic :: phsc
+           real(kind=sp),    automatic :: thdr
+           real(kind=sp),    automatic :: phdr
            integer(kind=int4), automatic :: nth1,nth2,nth3
            integer(kind=int4), automatic :: nph1,nph2,nph3
            integer(kind=int4), automatic :: ii,j,l,k,jj
@@ -165,6 +166,19 @@ module  mod_leaf_phase_matrices
            dp1t1 = dp_rad1*dt_rad1
            dp2t2 = dp_rad2*dt_rad2
            dp3t3 = dp_rad3*dt_rad3
+           orient_distr = 0.0_sp
+           if((nth1/=0).and.(nph1/=0)) then
+              do jj=1, nth1
+                 thdr = tr_start1+dt_rad1*real(jj-1,kind=sp)
+                 orient_distr = compute_leaf_odf(thdr)
+                 do ii=1, nph1
+                    phdr = pr_start1+dp_rad1*real(ii-1,kind=sp)
+                    thinc = theta
+                    thsc  = 3.141592653589793_sp-theta
+                    phinc = 3.141592653589793_sp
+                    phsc  = 0.0_sp
+                    if(po) then
+                       
      end subroutine compute_leaf_phase_matrices
      
 #if defined __GFORTRAN__ && !defined __INTEL_COMPILER
@@ -249,18 +263,215 @@ module  mod_leaf_phase_matrices
               dp_rad3 = 0.0_sp
       end subroutine set_leaf_quadrature_bounds
       
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      real(kind=sp) function compute_leaf_odf(th) !GCC$ ATTRIBUTES hot :: compute_leaf_odf !GCC$ ATTRIBUTES aligned(16) :: compute_leaf_odf !GCC$ ATTRIBUTES inline :: compute_leaf_odf
+#elif defined __INTEL_COMPILER
+        !DIR$ ATTRIBUTES INLINE :: compute_leaf_odf
+      real(kind=sp) function compute_leaf_odf(th)
+          !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: compute_leaf_odf
+#endif
+            use mod_tree_orient
+            use, intrinsic :: IEEE_ARITHMETIC
+            real(kind=sp),      intent(in) :: th
+            ! Locals
+            real(kind=sp), automatic :: mu,nu,pi_tol
+            real(kind=sp), paramaeter :: tol = 8.727e-7_sp
+            ! Exec code ....
+            if(leaf_orient<1 .or. leaf_orient>7) then
+               compute_leaf_odf = IEEE_VALUE(IEEE_QUIET_NAN)
+               return
+            end if
+            pi_tol = 1.570796326794897_sp-tol
+            if(leaf_orient==1) then
+               if(th>1.570796326794897_sp) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th==1.570796326794897_sp) then
+                  compute_leaf_odf = 0.5_sp
+               else
+                  compute_leaf_odf = sin(th)
+               end if
+            else if(leaf_orient==2) then
+               if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 2.770_sp
+                  mu = 1.172_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            else if(leaf_orient==3) then
+               if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 1.1720_sp
+                  mu = 2.7700_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            else if(leaf_orient==4) then
+               if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 3.326_sp
+                  mu = 3.326_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            else if(leaf_orient==5) then
+               if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 0.433_sp
+                  mu = 0.433_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            else if (leaf_orient==6) then
+               if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 1.0_sp
+                  mu = 1.0_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            else if(leaf_orient==7) then
+                 if(th>=pi_tol) then
+                  compute_leaf_odf = 0.0_sp
+               else if(th<=tol) then
+                  compute_leaf_odf = 0.0_sp
+               else
+                  nu = 1.101_sp
+                  mu = 1.930_sp
+                  call leaf_angle_orient(mu,nu,th,compute_leaf_odf)
+               end if
+            end if
+          end function compute_leaf_odf
+      
 
-
-
-
-
-
-
-
-
-
-
-
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     subroutine  leaf_angle_orient(mu,nu,th,pdf) !GCC$ ATTRIBUTES hot :: leaf_angle_orient !GCC$ ATTRIBUTES aligned(64) :: leaf_angle_orient !GCC$ ATTRIBUTES inline :: leaf_angle_orient
+#elif defined __INTEL_COMPILER
+             !DIR$ ATTRIBUTES INLINE :: leaf_angle_orient
+     subroutine  leaf_angle_orient(mu,nu,th,pdf)
+             !DIR$ ATTRIBUTES CODE_ALIGN : 64 :: leaf_angle_orient
+#endif
+           real(kind=sp),      intent(in)    :: mu
+           real(kind=sp),      intent(in)    :: nu
+           real(kind=sp),      intent(in)    :: th
+           real(kind=sp),      intent(inout) :: pdf
+           ! Locals
+           real(kind=sp), automatic :: t0,t1,t2
+           real(kind=sp), parameter :: two_over_pi = 0.636619772367581_sp
+           real(kind=sp), parameter :: half_pi     = 1.570796326794897_sp
+           t0 = two_over_pi*exp(gamma(mu+nu)-gamma(mu)-gamma(nu))
+           t1 = 1.0_sp-th/half_pi**(nu-1.0_sp)
+           t2 = th/half_pi**(mu-1.0_sp)
+           pdf = t0*t1*t2
+      end subroutine  leaf_angle_orient
+        
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+      subroutine leaf_phys_optics_approx(thinc,phinc,thsc,phsc,     &
+                                         thdr,phdr,rad_freq,rad_k0, &
+                                         rad_wv,leaf_mg,leaf_rho,   &
+                                         leaf_dens,leaf_diam,leaf_tau, &
+                                         epsr,epsrc,scat_mat) !GCC$ ATTRIBUTES hot :: leaf_phys_optics_approx !GCC$ ATTRIBUTES aligned(16) :: leaf_phys_optics_approx
+#elif defined __INTEL_COMPILER
+       subroutine leaf_phys_optics_approx(thinc,phinc,thsc,phsc,     &
+                                         thdr,phdr,rad_freq,rad_k0, &
+                                         rad_wv,leaf_mg,leaf_rho,   &
+                                         leaf_dens,leaf_diam,leaf_tau, &
+                                         epsr,epsrc,scat_mat)
+          !DIR$ ATTRIBUTES CODE_ALIGN : 16 :: leaf_phys_optics_approx
+#endif
+           real(kind=sp),                     intent(in) :: thinc
+           real(kind=sp),                     intent(in) :: phinc
+           real(kind=sp),                     intent(in) :: thsc
+           real(kind=sp),                     intent(in) :: phsc
+           real(kind=sp),                     intent(in) :: thdr
+           real(kind=sp),                     intent(in) :: phdr
+           real(kind=sp),                     intent(in) :: rad_freq
+           real(kind=sp),                     intent(in) :: rad_k0
+           real(kind=sp),                     intent(in) :: rad_wv
+           real(kind=sp),                     intent(in) :: leaf_mg
+           real(kind=sp),                     intent(in) :: leaf_rho
+           real(kind=sp),                     intent(in) :: leaf_dens
+           real(kind=sp),                     intent(in) :: leaf_diam
+           real(kind=sp),                     intent(in) :: leaf_tau
+           complex(kind=sp),  dimension(12),  intent(in) :: epsr
+           complex(kind=sp),  dimension(12),  intent(in) :: epsrc
+           complex(kind=sp),  dimension(2,2), intent(inout) :: scat_mat
+           ! LOcals
+           complex(kind=sp), automatic :: j,eps,res,const
+           complex(kind=sp), automatic :: gamh,game,gamhe_c1,gamhe_c2
+           real(kind=sp), automatic :: tau,q,p,a,b,tol
+           real(kind=sp), automatic :: cosphi1
+           real(kind=sp), automatic :: thj,phij
+           real(kind=sp), automatic :: sin_thi,cos_thi,sin_thj,cos_thj
+           real(kind=sp), automatic :: sin_ths,cos_ths,cos_phj,sin_phij,sin_phji
+           real(kind=sp), automatic :: cos_phij,cos_phji,sin_phsj,sin_phjs,cos_phjs
+           real(kind=sp), automatic :: cos_beta,sin_beta,sin_phi,cos_phi,cos_beta_phi
+           real(kind=sp), automatic :: sin_phpr,cos_phpr,sin_betapr
+           real(kind=sp), automatic :: u,v,sinu,sinv,sinu_u,sinv_v
+           real(kind=sp), automatic :: cosb_p,sintj_ti,sints_tj
+           real(kind=sp), automatic :: costj_sinpjs,costs_sinpsj
+           real(kind=sp), automatic :: sinpij_cospsj,cpij_cti_ctj,cpsj_cts_ctj
+           real(kind=sp), automatic :: s1,s2,s3,s4,s5,w1,w2,t0,t1,t2,t3,t4
+           ! Exec code ....
+           tol = 0.0001_sp
+           t0  = sin(thdr)
+           j = cmplx(0.0_sp,1.0_sp)
+           t1 = sin(thinc)
+           tau = leaf_tau/100.0_sp
+           t2 = cos(phdr-phinc)
+           eps = epsr(4)
+           t3 = cos(thinc)
+           a = 0.5_sp*1.772453850905516_sp*leaf_diam/100.0_sp
+           t4 = cos(thdr)
+           b = a
+           cosphi1 = -(t0*t1*t2*t3*t4)
+           if(cosphi1<0.0_sp) then
+              thj = 3.141592653589793_sp-thinc
+              phij = 3.141592653589793_sp+phinc
+              cosphi1 = -cosphi1
+           else
+              thj = thinc
+              phij = phinc
+           end if
+           sin_thi = t1
+           cos_thi = t3
+           sin_thj = sin(thj)
+           cos_thj = cos(thj)
+           sin_ths = sin(thsc)
+           cos_ths = cos(thsc)
+           cos_phij = cos(phij)
+           sin_phij = sin(phinc-phij)
+           sin_phji = -sin_phij
+           w1 = sin_thi*sin_phji
+           cos_phij = cos(phinc-phij)
+           q = 1.0_sp/sqrt(1.0_sp-w1*w1)
+           cos_phji = cos_phij
+           cos_beta = q*cosphi1
+           sin_phsj = sin(phsc-phij)
+           sin_beta = q*(-cos_thj*sin_thi*cos_phji+&
+                         cos_thi*sin_thj) 
+           sin_phjs = -sin_phsj
+           sin_phi  = sin_thi*sin_phij
+           cos_phsj = cos(phsc-phij)
+           cos_phi  = sqrt(1.0_sp-sin_phi*sin_phi)
+           cos_beta_phi = cos_beta*cos_phi
+           sin_phpr = sin_ths*sin_phsj
+           cos_phpr = sqrt(1.0_sp-sin_phpr*sin_phpr)
+           sin_betapr = (cos_ths*sin_thi- &
+                cos_thj*sin_ths*cos_phsj)/cos_phpr
+           p = 1.0_sp/sqrt(1.0_sp-cos_beta_phi*cos_beta_phi)
+       end subroutine leaf_phys_optics_approx
+       
 
 
 end module mod_leaf_phase_matrices
