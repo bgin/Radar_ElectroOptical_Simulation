9882a1ad06951137843a77c2d47b4cece7eac3b9
Bernard Gingold <beniekg@gmail.com>
2021-01-17 19:45:47 +0100

    Vectorized random functions
    Work in progress.

--- Statistics/GMS_vec_rand_distr.f90
+++ Statistics/GMS_vec_rand_distr.f90
@@ -789,7 +789,460 @@ function random_beta_zmm16r4(aa,bb,first) result(vrand)
   end do
   if(swap) vrand.v = v16r4_n1.v-vrand.v
 end function random_beta_zmm16r4
-  
 
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+function random_t_ymm8r4(m) result(vrand) !GCC$ ATTRIBUTES hot :: random_t_ymm8r4 !GCC$ ATTRIBUTES aligned(32) :: random_t_ymm8r4 !GCC$ ATTRIBUTES inline :: random_t_ymm8r4
+#elif defined __ICC || defined __INTEL_COMPILER
+function random_t_ymm8r4(m) result(vrand)
+  !DIR$ ATTRIBUTES INLINE :: random_t_ymm8r4
+  !DIR$ ATTRIBUTES ALIGN : 32 :: random_t_ymm8r4
+  !DIR$ ATTRIBUTES VECTOR :: random_t_ymm8r4
+#endif
+  ! Adapted from Fortran 77 code from the book:
+!     Dagpunar, J. 'Principles of random variate generation'
+!     Clarendon Press, Oxford, 1988.   ISBN 0-19-852202-9
+
+! FUNCTION GENERATES A RANDOM VARIATE FROM A
+! T DISTRIBUTION USING KINDERMAN AND MONAHAN'S RATIO METHOD.
+
+!     M = DEGREES OF FREEDOM OF DISTRIBUTION
+!           (1 <= 1NTEGER)
+  integer(kind=i4),    intent(in) :: m
+  type(YMM8r4_t) :: vrand
+  ! Locals
+  type(YMM8r4_t), save :: s,c,a,f,g
+  type(YMM8r4_t), automatic :: r,x,v,t0
+  integer(kind=i4), automatic :: mm = 0
+  ! Exec code ....
+  if(m < 1) then
+     vrand.v = v8r4_huge
+     return
+  end if
+  r.v = v8r4_n0.v
+  x.v = v8r4_n0.v
+  v.v = v8r4_n0.v
+  if(m /= mm) then
+     s.v = real(m,kind=sp)
+     c.v = -v8r4_quart.v*(s.v+v8r4_n1.v)
+     a.v = v8r4_n4.v/(v8r4_n1.v+v8r4_n1.v/s.v)**c.v
+     f.v = v8r4_n16.v/a.v
+     if(m>1) then
+        g.v  = s.v-v8r4_n1.v
+        t0.v = v8r4_n1.v/g.v
+        g.v  = ((s.v+v8r4_n1.v)*t0.v)**c*sqrt((s.v+s.v)*t0.v
+     else
+        g.v = v8r4_n1.v
+     end if
+     mm = n
+  end if
+
+  do
+     call random_number(r.v)
+     if(all(r.v<=v8r4_n0.v)) cycle
+     call random_number(v.v)
+     x.v = (v8r4_n2.v*v.v-v8r4_n1.v)*g.v/r.v
+     v.v = x.v*x.v
+     if(all(v.v>v8r4_n5.v-a.v*r.v)) then
+        if(m>=1 .and. all(r.v*(v.v+v8r4_n3.v)>f.v)) cycle
+        if(all(r.v>(v8r4_n1.v+v.vs.v)**c.v)) cycle
+     end if
+     exit
+  end do
+  vrand.v = x.v
+end function random_t_ymm8r4
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+function random_t_zmm16r4(m) result(vrand) !GCC$ ATTRIBUTES hot :: random_t_zmm16r4 !GCC$ ATTRIBUTES aligned(32) :: random_t_zmm16r4 !GCC$ ATTRIBUTES inline :: random_t_zmm16r4
+#elif defined __ICC || defined __INTEL_COMPILER
+function random_t_zmm16r4(m) result(vrand)
+  !DIR$ ATTRIBUTES INLINE :: random_t_zmm16r4
+  !DIR$ ATTRIBUTES ALIGN : 32 :: random_t_zmm16r4
+  !DIR$ ATTRIBUTES VECTOR :: random_t_zmm16r4
+#endif
+  ! Adapted from Fortran 77 code from the book:
+!     Dagpunar, J. 'Principles of random variate generation'
+!     Clarendon Press, Oxford, 1988.   ISBN 0-19-852202-9
+
+! FUNCTION GENERATES A RANDOM VARIATE FROM A
+! T DISTRIBUTION USING KINDERMAN AND MONAHAN'S RATIO METHOD.
+
+!     M = DEGREES OF FREEDOM OF DISTRIBUTION
+!           (1 <= 1NTEGER)
+  integer(kind=i4),    intent(in) :: m
+  type(ZMM16r4_t) :: vrand
+  ! Locals
+  type(ZMM16r4_t), save :: s,c,a,f,g
+  type(ZMM16r4_t), automatic :: r,x,v,t0
+  integer(kind=i4), automatic :: mm = 0
+  ! Exec code ....
+  if(m < 1) then
+     vrand.v = v16r4_huge
+     return
+  end if
+  r.v = v16r4_n0.v
+  x.v = v16r4_n0.v
+  v.v = v16r4_n0.v
+  if(m /= mm) then
+     s.v = real(m,kind=sp)
+     c.v = -v16r4_quart.v*(s.v+v16r4_n1.v)
+     a.v = v16r4_n4.v/(v16r4_n1.v+v16r4_n1.v/s.v)**c.v
+     f.v = v16r4_n16.v/a.v
+     if(m>1) then
+        g.v  = s.v-v16r4_n1.v
+        t0.v = v16r4_n1.v/g.v
+        g.v  = ((s.v+v16r4_n1.v)*t0.v)**c*sqrt((s.v+s.v)*t0.v
+     else
+        g.v = v16r4_n1.v
+     end if
+     mm = n
+  end if
+
+  do
+     call random_number(r.v)
+     if(all(r.v<=v16r4_n0.v)) cycle
+     call random_number(v.v)
+     x.v = (v16r4_n2.v*v.v-v16r4_n1.v)*g.v/r.v
+     v.v = x.v*x.v
+     if(all(v.v>v16r4_n5.v-a.v*r.v)) then
+        if(m>=1 .and. all(r.v*(v.v+v16r4_n3.v)>f.v)) cycle
+        if(all(r.v>(v16r4_n1.v+v.vs.v)**c.v)) cycle
+     end if
+     exit
+  end do
+  vrand.v = x.v
+end function random_t_zmm16r4
+
+#if defined __GFORTAN__ && !defined __INTEL_COMPILER
+subroutine random_mvnorm_ymm8r4(n,h,d,f,first,x,ier) !GCC$ ATTRIBUTES aligned(32) :: random_mvnorm_ymm8r4 !GCC$ ATTRIBUTES hot :: random_mvnorm_ymm8r4 !GCC$ ATTRIBUTES inline :: random_mvnorm_ymm8r4
+#elif defined __ICC || defined __INTEL_COMPILER
+subroutine random_mvnorm_ymm8r4(n,h,d,f,first,x,ier)
+  !DIR$ ATTRIBUTES INLINE :: random_mvnorm_ymm8r4
+  !DIR$ ATTRIBUTES ALIGN : 32 :: random_mvnorm_ymm8r4
+  !DIR$ ATTRIBUTES VECTOR :: random_mvnorm_ymm8r4
+#endif
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+  use omp_lib
+#endif
+! Adapted from Fortran 77 code from the book:
+!     Dagpunar, J. 'Principles of random variate generation'
+!     Clarendon Press, Oxford, 1988.   ISBN 0-19-852202-9
+
+! N.B. An extra argument, ier, has been added to Dagpunar's routine
+
+!     SUBROUTINE GENERATES AN N VARIATE RANDOM NORMAL
+!     VECTOR USING A CHOLESKY DECOMPOSITION.
+
+! ARGUMENTS:
+!        N = NUMBER OF VARIATES IN VECTOR
+!           (INPUT,INTEGER >= 1)
+!     H(J) = J'TH ELEMENT OF VECTOR OF MEANS
+!           (INPUT,REAL)
+!     X(J) = J'TH ELEMENT OF DELIVERED VECTOR
+!           (OUTPUT,REAL)
+!
+!    D(J*(J-1)/2+I) = (I,J)'TH ELEMENT OF VARIANCE MATRIX (J> = I)
+!            (INPUT,REAL)
+!    F((J-1)*(2*N-J)/2+I) = (I,J)'TH ELEMENT OF LOWER TRIANGULAR
+!           DECOMPOSITION OF VARIANCE MATRIX (J <= I)
+!            (OUTPUT,REAL)
+
+!    FIRST = .TRUE. IF THIS IS THE FIRST CALL OF THE ROUTINE
+!    OR IF THE DISTRIBUTION HAS CHANGED SINCE THE LAST CALL OF THE ROUTINE.
+!    OTHERWISE SET TO .FALSE.
+!            (INPUT,LOGICAL)
+
+!    ier = 1 if the input covariance matrix is not +ve definite
+  !        = 0 otherwise
+  integer(kind=i4),             intent(in) :: n
+  type(YMM8r4_t), dimension(:), intent(in) :: h,d
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED h:32,d:32
+#endif
+  type(YMM8r4_t), dimension(:), intent(inout) :: f
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED f:32
+#endif
+  type(YMM8r4_t), dimension(:), intent(out)  :: x
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED x:32
+#endif
+  logical(kind=i4),             intent(in)   :: first
+  integer(kind=i4),             intent(in)   :: ier
+  ! Locals
+  type(YMM8r4_t), automatic :: y,v
+  integer(kind=i4), automatic :: j,i,m,idx,jdx
+  integer(kind=i4), save :: n2
+  ! Exec code ...
+  ier = 0
+  idx = 0
+  jdx = 0
+  if(first) then
+     n2 = n+n
+     if(all(d(1).v<v8r4_n0.v)) then
+        ier = 1
+        return
+     end if
+     f(1).v = sqrt(d(1).v)
+     y.v = v8r4_n1.v/f(1).v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:32,d:32,y:32)
+#endif
+     do j=2, n
+        f(j).v = d(1+j*(j-2)/2).v*y.v
+     end do
+
+     do i = 2, n
+        v.v = d(i*(i-1)/2+i).v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:32)
+#endif
+        do m = 1, i-1
+           idx = (m-1)*(n2-m)/2+i
+           v.v = v.v-(f(idx).v*(f(idx).v)
+        end do
+        
+        if(all(v.v<v8r4_n0.v)) then
+           ier = 1
+           return
+        end if
+
+        v.v = sqrt(v.v)
+        y.v = v8r4_n1.v/v.v
+        f((i-1)*(n2-i)/2+i).v = v.v
+        do j = i+1,n
+           v.v = d(j*(j-1)/2+i).v = v.v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:32)
+#endif
+           do m = 1, i-1
+              idx = (m-1)*(n2-m)/2+i
+              jdx = (m-1)*(n2-m)/2+j
+              v.v = v.v-f(idx).v*f(jdx).v
+           end do
+           f((i-1)*(n2-i)/2+j).v = v.v*y.v
+        end do
+     end do
+  end if
+
+  x(1:n).v = h(1:n).v
+  do j = 1, n
+     y.v = random_normal_ymm8r4()
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:32,x:32)
+#endif     
+     do i = j, n
+        x(i).v = x(i).v+f((j-1)*(n2-j)/2+i).v*y.v
+     end do
+  end do
+end subroutine random_mvnorm_ymm8r4
+
+#if defined __GFORTAN__ && !defined __INTEL_COMPILER
+subroutine random_mvnorm_zmm16r4(n,h,d,f,first,x,ier) !GCC$ ATTRIBUTES aligned(32) :: random_mvnorm_zmm16r4 !GCC$ ATTRIBUTES hot :: random_mvnorm_zmm16r4 !GCC$ ATTRIBUTES inline :: random_mvnorm_zmm16r4
+#elif defined __ICC || defined __INTEL_COMPILER
+subroutine random_mvnorm_zmm16r4(n,h,d,f,first,x,ier)
+  !DIR$ ATTRIBUTES INLINE :: random_mvnorm_zmm16r4
+  !DIR$ ATTRIBUTES ALIGN : 32 :: random_mvnorm_zmm16r4
+  !DIR$ ATTRIBUTES VECTOR :: random_mvnorm_zmm16r4
+#endif
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+  use omp_lib
+#endif
+! Adapted from Fortran 77 code from the book:
+!     Dagpunar, J. 'Principles of random variate generation'
+!     Clarendon Press, Oxford, 1988.   ISBN 0-19-852202-9
+
+! N.B. An extra argument, ier, has been added to Dagpunar's routine
+
+!     SUBROUTINE GENERATES AN N VARIATE RANDOM NORMAL
+!     VECTOR USING A CHOLESKY DECOMPOSITION.
+
+! ARGUMENTS:
+!        N = NUMBER OF VARIATES IN VECTOR
+!           (INPUT,INTEGER >= 1)
+!     H(J) = J'TH ELEMENT OF VECTOR OF MEANS
+!           (INPUT,REAL)
+!     X(J) = J'TH ELEMENT OF DELIVERED VECTOR
+!           (OUTPUT,REAL)
+!
+!    D(J*(J-1)/2+I) = (I,J)'TH ELEMENT OF VARIANCE MATRIX (J> = I)
+!            (INPUT,REAL)
+!    F((J-1)*(2*N-J)/2+I) = (I,J)'TH ELEMENT OF LOWER TRIANGULAR
+!           DECOMPOSITION OF VARIANCE MATRIX (J <= I)
+!            (OUTPUT,REAL)
+
+!    FIRST = .TRUE. IF THIS IS THE FIRST CALL OF THE ROUTINE
+!    OR IF THE DISTRIBUTION HAS CHANGED SINCE THE LAST CALL OF THE ROUTINE.
+!    OTHERWISE SET TO .FALSE.
+!            (INPUT,LOGICAL)
+
+!    ier = 1 if the input covariance matrix is not +ve definite
+  !        = 0 otherwise
+  integer(kind=i4),             intent(in) :: n
+  type(ZMM16r4_t), dimension(:), intent(in) :: h,d
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED h:64,d:64
+#endif
+  type(ZMM16r4_t), dimension(:), intent(inout) :: f
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED f:64
+#endif
+  type(ZMM16r4_t), dimension(:), intent(out)  :: x
+#if defined __INTEL_COMPILER
+  !DIR$ ASSUME_ALIGNED x:64
+#endif
+  logical(kind=i4),             intent(in)   :: first
+  integer(kind=i4),             intent(in)   :: ier
+  ! Locals
+  type(ZMM16r4_t), automatic :: y,v
+  integer(kind=i4), automatic :: j,i,m,idx,jdx
+  integer(kind=i4), save :: n2
+  ! Exec code ...
+  ier = 0
+  idx = 0
+  jdx = 0
+  if(first) then
+     n2 = n+n
+     if(all(d(1).v<v16r4_n0.v)) then
+        ier = 1
+        return
+     end if
+     f(1).v = sqrt(d(1).v)
+     y.v = v16r4_n1.v/f(1).v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:64,d:64,y:64)
+#endif
+     do j=2, n
+        f(j).v = d(1+j*(j-2)/2).v*y.v
+     end do
+
+     do i = 2, n
+        v.v = d(i*(i-1)/2+i).v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:64)
+#endif
+        do m = 1, i-1
+           idx = (m-1)*(n2-m)/2+i
+           v.v = v.v-(f(idx).v*(f(idx).v)
+        end do
+        
+        if(all(v.v<v16r4_n0.v)) then
+           ier = 1
+           return
+        end if
+
+        v.v = sqrt(v.v)
+        y.v = v16r4_n1.v/v.v
+        f((i-1)*(n2-i)/2+i).v = v.v
+        do j = i+1,n
+           v.v = d(j*(j-1)/2+i).v = v.v
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:64)
+#endif
+           do m = 1, i-1
+              idx = (m-1)*(n2-m)/2+i
+              jdx = (m-1)*(n2-m)/2+j
+              v.v = v.v-f(idx).v*f(jdx).v
+           end do
+           f((i-1)*(n2-i)/2+j).v = v.v*y.v
+        end do
+     end do
+  end if
+
+  x(1:n).v = h(1:n).v
+  do j = 1, n
+     y.v = random_normal_zmm16r4()
+#if defined __ICC || defined __INTEL_COMPILER
+     !DIR$ VECTOR ALWAYS
+     !DIR$ VECTOR ALIGNED
+#elif defined __GFORTRAN__ && !defined __INTEL_COMPILER
+     !$OMP SIMD ALIGNED(f:64,x:64)
+#endif     
+     do i = j, n
+        x(i).v = x(i).v+f((j-1)*(n2-j)/2+i).v*y.v
+     end do
+  end do
+end subroutine random_mvnorm_zmm16r4
+
+#if defined __GFORTRAN__ && !defined __INTEL_COMPILER
+function rand_inv_gauss_ymm8r4(h,b,first) result(v) !GCC$ ATTRIBUTES hot :: rand_inv_gauss_ymm8r4 !GCC$ ATTRIBUTES aligned(32) :: rand_inv_gauss_ymm8r4 !GCC$ ATTRIBUTES inline :: rand_inv_gauss_ymm8r4
+#elif defined __ICC || defined __INTEL_COMPILER
+function rand_inv_gauss_ymm8r4(h,b,first) result(v)
+  !DIR$ ATTRIBUTES INLINE :: rand_inv_gauss_ymm8r4
+  !DIR$ ATTRIBUTES ALIGN : 32 :: rand_inv_gauss_ymm8r4
+  !DIR$ ATTRIBUTES VECTOR :: rand_inv_gauss_ymm8r4
+#endif
+  type(YMM8r4_t),    intent(in) :: h,b
+  logical(kind=i4),  intent(in) :: first
+  type(YMM8r4_t) :: v
+  ! Locals
+  type(YMM8r4_t), automatic :: ym,xm,r,w,r1,r2,x
+  type(YMM8r4_t), save :: a,c,d,e
+  ! Locals
+  ym.v = v8r4_n0.v
+  xm.v = v8r4_n0.v
+  r.v  = v8r4_n0.v
+  w.v  = v8r4_n0.v
+  r1.v = v8r4_n0.v
+  r2.v = v8r4_n0.v
+  x.v  = v8r4_n0.v
+  if(first) then
+     if(all(h.v>v8r4_quart.v*b.v*sqrt(v8r4_huge.v))) then
+        v.v = v8r4_huge.v
+        return
+     end if
+     e.v = b.v*b.v
+     d.v = h.v+v8r4_n1.v
+     ym.v = (-d.v+sqrt(d.v*d.v+e.v))/b.v
+     if(all(ym.v<v8r4_tiny.v)) then
+        v.v = v8r4_tiny.v
+        return
+     end if
+     d.v = h.v-v8r4_n0.v
+     xm.v = (d.v+sqrt(d.v*d.v+e.v))/b.v
+     d.v = v8r4_half.v*d.v
+     e.v = -v8r4_half.v*b.v
+     r.v = xm.v+v8r4_n1.v/xm.v
+     w.v = xm.v*ym.v
+     a.v = w.v**(-v8r4_half.v*h.v)*sqrt(xm.v/ym.v)*exp(-e.v*(r.v-ym.v-v8r4_n1.v/ym.v))
+     if(all(a.v<v8r4_tiny.v)) then
+        v.v = v8r4_tiny.v
+        return
+     end if
+     c.v = -d.v*log(xm.v)-e.v*r.v
+  end if
+
+  do
+     call random_number(r1.v)
+     if(all(r1<=v8r4_n0.v)) cycle
+     call random_number(r2.v)
+     x.v = a.v*r2.v/r1.v
+     if(all(x.v<=v8r4_n0.v)) cycle
+     if(all(log(r1.v)<d.v*log(x.v)+e.v*(x.v+v8r4_n1.v/x.v)+c.v)) exit
+  end do
+  v.v = x.v
+end function rand_inv_gauss_ymm8r4
+  
 
 END MODULE mod_vec_rand_distr
