d0fc7a6dfff8ee2fa3b323ce8f8ae8fc4a7ff586
Bernard Gingold <beniekg@gmail.com>
2021-09-22 15:38:38 +0200

    Added non-looped  SIMD (avx) input argument function.

--- dsp/GMS_32f_atan_32f.c
+++ dsp/GMS_32f_atan_32f.c
@@ -1,6 +1,6 @@
 
 
-#include <immintrin.h>
+
 #include "GMS_cephes.h"
 
 
@@ -8,7 +8,7 @@
     #define DSP_32F_ATAN_32F_BLOCK                               \
        int32_t idx = 0;                                          \
        const int32_t len = npoints/8;                            \
-       register __m256 aVal, d, pio2, x, y, z, arctangent;       \
+       register __m256 aVal, pio2, x, y, z, arctangent;          \
        register __m256 n_third,p_third,condition,fzeroes,fones;	 \
        register __m256 ftwos,ffours;                             \ 
        pio2 = _mm256_set1_ps(1.5707963267948966192f);            \
@@ -147,4 +147,57 @@ atan_a_ymm8r4_ymm8r4_looped(float * __restrict __attribute__((aligned(32))) b,
               for(; idx != npoints; ++idx) {
                   b[i] = ceph_atanf(a[i]);
 	      }
+}
+
+
+__m256
+atan_ymm8r4_ymm8r4(const __m256 v) {
+
+      
+       register __m256 aVal, x, y, z, arctangent;       
+       register __m256 n_third,p_third,condition;          
+       const __m256 pio2 = _mm256_set1_ps(1.5707963267948966192f);            
+       const __m256 fzeroes = _mm256_setzero_ps();                            
+       const __m256 fones = _mm256_set1_ps(1.0f);                             
+       const __m256 ftwos = _mm256_set1_ps(2.0f);                             
+       const __m256 ffours  = _mm256_set1_ps(4.0f);                           
+       const __m256 n_third = _mm256_set1_ps(-0.3333333333333333333333333f);  
+       const __m256 p_third = _mm256_set1_ps(0.3333333333333333333333333f);
+       arctangent = _mm256_setzero_ps();
+       aVal = v;
+       z = aVal;
+       condition = _mm256_cmp_ps(z, fzeroes, _CMP_LT_OQ);
+       z = _mm256_sub_ps(z, _mm256_and_ps(_mm256_mul_ps(z, ftwos), condition));
+       condition = _mm256_cmp_ps(z, fones, _CMP_LT_OQ);
+       x = _mm256_add_ps(
+       z, _mm256_and_ps(_mm256_sub_ps(_mm256_div_ps(fones, z), z), condition));
+		      // Original loop of 2-cycles removed
+		      /*
+                             for (i = 0; i < 2; i++) {
+                                 x = _mm256_add_ps(x, _mm256_sqrt_ps(_mm256_fmadd_ps(x, x, fones)));
+                             }
+                       */
+       x = _mm256_add_ps(x, _mm256_sqrt_ps(_mm256_fmadd_ps(x, x, fones)));
+       x = _mm256_add_ps(x, _mm256_sqrt_ps(_mm256_fmadd_ps(x, x, fones)));
+       x = _mm256_div_ps(fones, x);
+       y = fzeroes;
+		      // Original loop of 2-cycles removed
+		      /*
+                            for (j = TERMS - 1; j >= 0; j--) {
+                                 y = _mm256_fmadd_ps(
+                                 y, _mm256_mul_ps(x, x), _mm256_set1_ps(pow(-1, j) / (2 * j + 1)));
+                            }
+                       */
+	y = _mm256_fmadd_ps(
+        y, _mm256_mul_ps(x, x),n_third); // removing call to pow
+        y = _mm256_fmadd_ps(
+        y, _mm256_mul_ps(x, x), p_third);  // removed call to pow
+	y = _mm256_mul_ps(y, _mm256_mul_ps(x, ffours));
+        condition = _mm256_cmp_ps(z, fones, _CMP_GT_OQ);
+        y = _mm256_add_ps(y, _mm256_and_ps(_mm256_fnmadd_ps(y, ftwos, pio2), condition));
+        arctangent = y;
+        condition = _mm256_cmp_ps(aVal, fzeroes, _CMP_LT_OQ);
+        arctangent = _mm256_sub_ps(
+        arctangent, _mm256_and_ps(_mm256_mul_ps(arctangent, ftwos), condition));
+        return (arctangent);
 }
