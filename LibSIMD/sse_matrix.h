d59942ccc76385ed3ea413e72f011b1d32b9487c
Bernard Gingold <beniekg@gmail.com>
2021-10-11 15:41:19 +0200

    Update sse_matrix.h

--- LibSIMD/sse_matrix.h
+++ LibSIMD/sse_matrix.h
@@ -1,65 +1,35 @@
 
 
 #ifndef __SSE_MATRIX_H__
-#define __SSE_MATRIX_H__
+#define __SSE_MATRIX_H__ 301220181140
 
 
-#if !defined (SSE_MATRIX_MAJOR)
-#define SSE_MATRIX_MAJOR 1
-#endif
 
-#if !defined (SSE_MATRIX_MINOR)
-#define SSE_MATRIX_MINOR 0
-#endif
-
-#if !defined (SSE_MATRIX_MICRO)
-#define SSE_MATRIX_MICRO 0
-#endif
-
-#if !defined (SSE_MATRIX_FULLVER) 
-#define SSE_MATRIX_FULLVER 1000*(SSE_MATRIX_MAJOR)+100*(SSE_MATRIX_MINOR)+10*(SSE_MATRIX_MICRO)
-#endif
-
-#if !defined (SSE_MATRIX_CREATE_DATE)
-#define SSE_MATRIX_CREATE_DATE  "30-12-2018 11:40 + 00200 (SUN 30 DEC 2018 GMT+2)"
-#endif
-
-#if !defined (SSE_MATRIX_BUILD_DATE)
-#define SSE_MATRIX_BUILD_DATE "00-00-0000 00:00"
-#endif
-
-#if !defined (SSE_MATRIX_AUTHOR)
-#define SSE_MATRIX_AUTHOR "Programmer: Bernard Gingold, contact: beniekg@gmail.com"
-#endif
-
-#if !defined (SSE_MATRIX_SYNOPSIS)
-#define SSE_MATRIX_SYNOPSIS "SSE accelerated Matrix-Matrix, Matrix-Vector operations -- Fortran callable."
-#endif
 
 // Interoparable with coresponding Fortran derived type.
    
-typedef struct __declspec(align(16))  M4x4f32 {
+typedef struct __attribute__((aligned(16)))  M4x4f32 {
 	    float row0[4];
 	    float row1[4];
 	    float row2[4];
 	    float row3[4];
  }M4x4f32;
 
-typedef struct __declspec(align(16))  V1x4f32 {
+typedef struct __attribute__((aligned(16))) V1x4f32 {
 		float v[4];
  }V1x4f32;
 
-typedef struct __declspec(align(16))  V1x3f32 {
+typedef struct __attribute__((aligned(16)))  V1x3f32 {
 	    float v[4]; // Last element should be set either to 0 or to 1
  }V1x3f32;
 
 
  // void functions callable from Fortran.
 
-void warmup_fp128unit(volatile float * __restrict ,
+/*void warmup_fp128unit(volatile float * __restrict ,
 					  const float * __restrict ,
 					  const float * __restrict ,
-					  const int n );
+					  const int n );*/
 
 void M4x4f32_setzero(struct M4x4f32 * __restrict);
 
