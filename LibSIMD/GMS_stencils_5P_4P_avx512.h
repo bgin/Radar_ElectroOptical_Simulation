3d8cb0629c6e870871c4494c14cce0965ff41966
Bernard Gingold <beniekg@gmail.com>
2021-10-11 15:25:53 +0200

    Update GMS_stencils_5P_4P_avx512.h

--- LibSIMD/GMS_stencils_5P_4P_avx512.h
+++ LibSIMD/GMS_stencils_5P_4P_avx512.h
@@ -24,7 +24,7 @@
 
 
 __m512d
-stencil_5P_zmm8r8(__m512d (*) (__m512d),
+stencil_5P_central_zmm8r8(__m512d (*) (__m512d),
                   const __m512d,
 		  const __m512d,
                   __m512d * __restrict,
@@ -34,42 +34,43 @@ stencil_5P_zmm8r8(__m512d (*) (__m512d),
 
 
 
-__m512d
+/*__m512d
 stencil_5P_central_zmm8r8_optim(__m512d (*) (__m512d),
                                 const __m512d,
 		                const __m512d,
                                 __m512d * __restrict)
 		                                     __attribute__((noinline))
 			                             __attribute__((hot))
-				                     __attribute__((aligned(32)));
+				                     __attribute__((aligned(32)));*/
 
 
 
 __m512d
-stencil_4P_zmm8r8(__m512d (*) (__m512d),
-                  const __m512d,
-		  const __m512d,
-                  __m512d * __restrict,
-		  __m512d * __restrict) __attribute__((noinline))
-			                __attribute__((hot))
-				        __attribute__((aligned(32)));
+stencil_4P_forward_zmm8r8(__m512d (*) (__m512d),
+                          const __m512d,
+		          const __m512d,
+                          __m512d * __restrict,
+		          __m512d * __restrict) __attribute__((noinline))
+			                        __attribute__((hot))
+				                __attribute__((aligned(32)));
 
 
-__m512d
+/*__m512d
 stencil_4P_forward_zmm8r8_optim(__m512d (*) (__m512d),
                                 const __m512d,
 		                const __m512d,
                                 __m512d * __restrict)
 		                                     __attribute__((noinline))
 			                             __attribute__((hot))
-				                     __attribute__((aligned(32)));
+				                     __attribute__((aligned(32)));*/
 
 
 __m512d
-stencil_4P_backward_zmm8r8_optim(__m512d (*) (__m512d),
+stencil_4P_backward_zmm8r8(__m512d (*) (__m512d),
                                 const __m512d,
 		                const __m512d,
-                                __m512d * __restrict)
+                                __m512d * __restrict,
+			        __m512d * __restrict)
 		                                     __attribute__((noinline))
 			                             __attribute__((hot))
 				                     __attribute__((aligned(32)));
