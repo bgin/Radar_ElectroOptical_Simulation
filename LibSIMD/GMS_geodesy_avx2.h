297a64fa27a28edf4eae69f300bee2aadb077b18
Bernard Gingold <beniekg@gmail.com>
2021-10-31 14:08:19 +0100

    Added new functions.

--- LibSIMD/GMS_geodesy_avx2.h
+++ LibSIMD/GMS_geodesy_avx2.h
@@ -64,6 +64,172 @@ cart_to_geodetic_a_ymm4r8_looped(const double * __restrict,
 					         __attribute__((aligned(32)));
 
 
+void
+geodetic_to_cart_ymm4r8(const __m256d,
+		        const __m256d,
+			const __m256d,
+			const __m256d,
+			const __m256d,
+			__m256d * __restrict,
+			__m256d * __restrict,
+			__m256d * __restrict)  __attribute__((noinline))
+			                       __attribute__((hot))
+					       __attribute__((regcall))
+					       __attribute__((aligned(32)));
+
+
+void
+geodetic_to_cart_u_ymm4r8_looped(const double,
+			         const double,
+				 const double * __restrict,
+				 const double * __restrict,
+				 const double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 const int32_t)  __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+void
+geodetic_to_cart_a_ymm4r8_looped(const double,
+			         const double,
+				 double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 double * __restrict,
+				 const int32_t)  __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+void
+forward_method_ymm4r8(const __m256d,      //ellipsoid semi-maxjor axis
+		      const __m256d,      //elipsoid flattening [dimensionless]
+		      const __m256d,    //vector of 4 starting-points latitude [rad]
+		      const __m256d,    //vector of 4 starting-points longtitude [rad]
+		      const __m256d,      //vector of 4 forward azimutes [rad]
+		      const __m256d,      //vector of 4 distances vp1-to-vp2 [m]
+		      __m256d * __restrict,         //vector of 4 endpoints latitude [rad]
+		      __m256d * __restrict,         //vector of 4 endpoints longtitude [rad]
+		      __m256d * __restrict)        //backward facing vector of 4 azimutes vp2-to-vp1 [rad]
+			                       __attribute__((noinline))
+			                       __attribute__((hot))
+					       __attribute__((regcall))
+					       __attribute__((aligned(32)));
+
+
+void 
+forward_method_u_ymm4r8_looped(const double,
+			       const double,
+			       const double * __restrict,
+			       const double * __restrict,
+			       const double * __restrict,
+			       const double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       const int32_t)    __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+
+void 
+forward_method_a_ymm4r8_looped(const double,
+			       const double,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       double * __restrict,
+			       const int32_t)    __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+	/*
+                           @Reference:
+                                          http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf
+       */
+__m256d
+spheroid_distance_ymm4r8(const __m256d,
+			 const __m256d,
+			 const __m256d,
+			 const __m256d,
+			 const __m256d)        __attribute__((noinline))
+			                       __attribute__((hot))
+					       __attribute__((regcall))
+					       __attribute__((aligned(32)));
+
+
+
+void
+spheroid_distance_u_ymm4r8_looped(const double,
+			          double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  const int32_t)   __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+void
+spheroid_distance_a_ymm4r8_looped(const double,
+			          double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  const int32_t)   __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+__m256d
+geocentric_radius_ymm4r8(const __m256d,
+			 const __m256d,
+			 const __m256d)        __attribute__((noinline))
+			                       __attribute__((hot))
+					       __attribute__((regcall))
+					       __attribute__((aligned(32)));
+
+
+void
+geocentric_radius_u_ymm4r8_looped(const double,
+			          const double * __restrict,
+				  const double * __restrict,
+				  double * __restrict,
+				  const int32_t)  __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+void
+geocentric_radius_a_ymm4r8_looped(const double,
+			          double * __restrict,
+				  double * __restrict,
+				  double * __restrict,
+				  const int32_t)  __attribute__((noinline))
+			                         __attribute__((hot))
+					         __attribute__((aligned(32)));
+
+
+
+
+
+
+						 
+
+						 
+
+
 
 
 
