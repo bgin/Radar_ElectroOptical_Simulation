339ffaf34ee776948f57100d9f5d21f8e048fd32
Bernard Gingold <beniekg@gmail.com>
2021-11-28 10:28:27 +0100

    SIMD (AVX/AVX512) memmove functions.

---
+++ LibSIMD/GMS_simd_mem_move.h
@@ -0,0 +1,52 @@
+
+
+#ifndef __GMS_SIMD_MEM_MOVE_H__
+#define __GMS_SIMD_MEM_MOVE_H__ 26092020447
+
+
+   const unsigned int GMS_SIMD_MEM_MOVE_MAJOR = 1U;
+   const unsigned int GMS_SIMD_MEM_MOVE_MINOR = 0U;
+   const unsigned int GMS_SIMD_MEM_MOVE_MICRO = 0U;
+   const unsigned int GMS_SIMD_MEM_MOVE_FULLVER =
+         1000U*GMS_SIMD_MEM_MOVE_MAJOR+100U*GMS_SIMD_MEM_MOVE_MINOR+10U*GMS_SIMD_MEM_MOVE_MICRO;
+   const char * const GMS_SIMD_MEM_MOVE_CREATE_DATE = "26-09-2020 4:47PM +00200 (SAT 26 SEP 2020 GMT+2)";
+   const char * const GMS_SIMD_MEM_MOVE_BUILD_DATE  = __DATE__ ":" __TIME__;
+   const char * const GMS_SIMD_MEM_MOVE_AUTHOR      = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+
+
+#include <stdint.h>
+
+
+            void ymm8r4_cached_u_memmove(float * __restrict,
+	                                 const float * __restrict,
+					 const int32_t)   __attribute__((noinline))
+					                  __attribute__((hot))
+							  __attribute__((aligned(32)));
+
+
+	    void ymm8r4_uncached_memove(float * __restrict,
+	                                const float * __restrict,
+					const int32_t)    __attribute__((noinline))
+					                  __attribute__((hot))
+							  __attribute__((aligned(32)));
+	    
+
+
+            void zmm16r4_cached_u_memmove(float * __restrict,
+	                                  const float * __restrict,
+					  const int32_t)   __attribute__((noinline))
+					                   __attribute__((hot))
+							   __attribute__((aligned(32)));
+
+
+	    void zmm16r4_uncached_memmove(float * __restrict,
+	                                  const float * __restrict,
+					  const int32_t)   __attribute__((noinline))
+					                   __attribute__((hot))
+							   __attribute__((aligned(32)));
+            
+
+
+
+
+#endif /*__GMS_SIMD_MEM_MOVE_H__*/
