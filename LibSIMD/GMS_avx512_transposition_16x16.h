6fd654608c6db81fb5b063c11786207fa61957a9
Bernard Gingold <beniekg@gmail.com>
2021-12-16 13:31:59 +0100

    AVX512 transposition 16x16 (single-precision).

---
+++ LibSIMD/GMS_avx512_transposition_16x16.h
@@ -0,0 +1,124 @@
+
+#ifndef __GMS_AVX512_TRANSPOSITION_16X16_H__
+#define __GMS_AVX512_TRANSPOSITION_16X16_H__
+
+
+
+#include <immintrin.h>
+
+
+         //In-place transformation.
+         void
+	 transpose_zmm16r4_16x16_ip(__m512 * __restrict,
+		                    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict,
+				    __m512 * __restrict) __attribute__((noinline))
+							 __attribute__((hot))
+							 __attribute__((regcall))
+							 __attribute__((aligned(32)));
+
+
+
+	 void
+	 transpose_u_zmm16r4_16x16_ip(float * __restrict,
+		                      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict)  __attribute__((noinline))
+							   __attribute__((hot))
+							   __attribute__((regcall))
+							   __attribute__((aligned(32)));
+
+
+	 void
+	 transpose_a_zmm16r4_16x16_ip(float * __restrict,
+		                      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict,
+				      float * __restrict)  __attribute__((noinline))
+							   __attribute__((hot))
+							   __attribute__((regcall))
+							   __attribute__((aligned(32)));
+
+
+	 void
+	 transpose_u_zmm16r4_16x16_ip_v2(float * __restrict)   __attribute__((noinline))
+							       __attribute__((hot))
+							       __attribute__((aligned(32)));
+
+
+	 void
+	 transpose_a_zmm16r4_16x16_ip_v2(float * __restrict)   __attribute__((noinline))
+							       __attribute__((hot))
+							       __attribute__((aligned(32)));
+
+
+	 void
+	 transpose_u_zmm16r4_16x16_ip_v3(float * __restrict,
+	                                 float * __restrict)   __attribute__((noinline))
+							       __attribute__((hot))
+							       __attribute__((aligned(32)));
+
+
+	 void
+	 transpose_a_zmm16r4_16x16_ip_v3(float * __restrict,
+	                                 float * __restrict)   __attribute__((noinline))
+							       __attribute__((hot))
+							       __attribute__((aligned(32)));
+
+
+
+
+
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__GMS_AVX512_TRANSPOSITION_16X16_H__*/
