fe46f9c00daa2ff4f8bbcc6d147783906c3d21fe
Bernard Gingold <beniekg@gmail.com>
2022-01-21 17:37:07 +0100

    Adaptation finished.

--- ODE-Solvers/depac_src/dcfod.f90
+++ ODE-Solvers/depac_src/dcfod.f90
@@ -1,5 +1,6 @@
 !** DCFOD
 PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
+       use mod_kinds, only : i4,dp
   !> Subsidiary to DDEBDF
   !***
   ! **Library:**   SLATEC
@@ -23,11 +24,11 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
   !   891214  Prologue converted to Version 4.0 format.  (BAB)
   !   900328  Added TYPE section.  (WRB)
   !
-  INTEGER, INTENT(IN) :: Meth
-  REAL(DP), INTENT(OUT) :: Elco(13,12), Tesco(3,12)
+  INTEGER(i4), INTENT(IN) :: Meth
+  REAL(dp), INTENT(OUT) :: Elco(13,12), Tesco(3,12)
   !
-  INTEGER :: i, ib, nq, nqm1, nqp1
-  REAL(DP) :: agamq, fnq, fnqm1, pc(12), pint, ragq, rq1fac, rqfac, tsign, xpin
+  INTEGER(i4) :: i, ib, nq, nqm1, nqp1
+  REAL(dp) :: agamq, fnq, fnqm1, pc(12), pint, ragq, rq1fac, rqfac, tsign, xpin
   !     ------------------------------------------------------------------
   !      DCFOD  IS CALLED BY THE INTEGRATOR ROUTINE TO SET COEFFICIENTS
   !      NEEDED THERE.  THE COEFFICIENTS FOR THE CURRENT METHOD, AS
@@ -58,8 +59,8 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
   !* FIRST EXECUTABLE STATEMENT  DCFOD
   IF( Meth==2 ) THEN
     !
-    pc(1) = 1._DP
-    rq1fac = 1._DP
+    pc(1) = 1._dp
+    rq1fac = 1._dp
     DO nq = 1, 5
       !           ------------------------------------------------------------
       !            THE PC ARRAY WILL CONTAIN THE COEFFICIENTS OF THE
@@ -70,7 +71,7 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
       nqp1 = nq + 1
       !           FORM COEFFICIENTS OF P(X)*(X+NQ).
       !           ------------------------------------
-      pc(nqp1) = 0._DP
+      pc(nqp1) = 0._dp
       DO ib = 1, nq
         i = nq + 2 - ib
         pc(i) = pc(i-1) + fnq*pc(i)
@@ -81,7 +82,7 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
       DO i = 1, nqp1
         Elco(i,nq) = pc(i)/pc(2)
       END DO
-      Elco(2,nq) = 1._DP
+      Elco(2,nq) = 1._dp
       Tesco(1,nq) = rq1fac
       Tesco(2,nq) = nqp1/Elco(1,nq)
       Tesco(3,nq) = (nq+2)/Elco(1,nq)
@@ -89,14 +90,14 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
     END DO
   ELSE
     !
-    Elco(1,1) = 1._DP
-    Elco(2,1) = 1._DP
-    Tesco(1,1) = 0._DP
-    Tesco(2,1) = 2._DP
-    Tesco(1,2) = 1._DP
-    Tesco(3,12) = 0._DP
-    pc(1) = 1._DP
-    rqfac = 1._DP
+    Elco(1,1) = 1._dp
+    Elco(2,1) = 1._dp
+    Tesco(1,1) = 0._dp
+    Tesco(2,1) = 2._dp
+    Tesco(1,2) = 1._dp
+    Tesco(3,12) = 0._dp
+    pc(1) = 1._dp
+    rqfac = 1._dp
     DO nq = 2, 12
       !           ------------------------------------------------------------
       !            THE PC ARRAY WILL CONTAIN THE COEFFICIENTS OF THE
@@ -110,7 +111,7 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
       nqp1 = nq + 1
       !           FORM COEFFICIENTS OF P(X)*(X+NQ-1).
       !           ----------------------------------
-      pc(nq) = 0._DP
+      pc(nq) = 0._dp
       DO ib = 1, nqm1
         i = nqp1 - ib
         pc(i) = pc(i-1) + fnqm1*pc(i)
@@ -119,8 +120,8 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
       !           COMPUTE INTEGRAL, -1 TO 0, OF P(X) AND X*P(X).
       !           -----------------------
       pint = pc(1)
-      xpin = pc(1)/2._DP
-      tsign = 1._DP
+      xpin = pc(1)/2._dp
+      tsign = 1._dp
       DO i = 2, nq
         tsign = -tsign
         pint = pint + tsign*pc(i)/i
@@ -129,12 +130,12 @@ PURE SUBROUTINE DCFOD(Meth,Elco,Tesco)
       !           STORE COEFFICIENTS IN ELCO AND TESCO.
       !           --------------------------------
       Elco(1,nq) = pint*rq1fac
-      Elco(2,nq) = 1._DP
+      Elco(2,nq) = 1._dp
       DO i = 2, nq
         Elco(i+1,nq) = rq1fac*pc(i)/i
       END DO
       agamq = rqfac*xpin
-      ragq = 1._DP/agamq
+      ragq = 1._dp/agamq
       Tesco(2,nq) = ragq
       IF( nq<12 ) Tesco(1,nqp1) = ragq*rqfac/nqp1
       Tesco(3,nqm1) = ragq
