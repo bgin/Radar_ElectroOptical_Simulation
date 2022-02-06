96b93a1fe937d671c031be8e7e43f29c542985bf
Bernard Gingold <beniekg@gmail.com>
2022-02-06 13:14:28 +0100

    BLAS flops counting routines.

--- Mathematics/GMS_blas_flops.f90
+++ Mathematics/GMS_blas_flops.f90
@@ -21,8 +21,12 @@ module blas_flops
      implicit none
      public
 
-     real(kind=dp), parameter, private :: nano = 1.0e-9_dp
-
+     real(kind=dp),    parameter, private :: nano = 1.0e-9_dp
+     integer(kind=i4), parameter, private :: mul_ops = 1
+     integer(kind=i4), parameter, private :: add_ops = 1
+     integer(kind=i4), parameter, private :: cmul_ops= 6 ! complex
+     integer(kind=i4), parameter, private :: cadd_ops= 2 ! complex
+     
      contains
 
      pure elemental function fmuls_asum(n) result(ops)
@@ -568,7 +572,392 @@ module blas_flops
        real(kind=dp),  intent(in) :: n
        real(kind=dp)  :: bytes
        bytes=bytes_her2(n)
-     end function bytes_syrr2
+     end function bytes_syr2
+
+     ! BLAS Level 3
+     ! // read A, B, C; write C
+     pure elemental function bytes_gemm(m,n,k) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_gemm
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: m,n,k
+       real(kind=dp)  :: bytes
+       bytes=nano*((m*k+k*n+2.0_dp*m*m)*sizeof(n))
+     end function bytes_gemm
+
+     pure elemental function bytes_hemm(side,m,n) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_hemm
+       !dir$ optimize : 3
+#end
+        logical(kind=i4),       intent(in) :: side
+        real(kind=dp),          intent(in) :: m,n
+        real(kind=dp) :: bytes
+        real(kind=dp) :: tmp
+        if(side) then
+           tmp=0.5_dp*m*(m+1)
+        else
+           tmp=0.5_dp*n*(n+1)
+        end if
+        bytes=nano*(tmp+3.0_dp*m*n)*sizeof(n)
+     end function bytes_hemm
+
+     pure elemental function bytes_symm(side,m,n) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_symm
+       !dir$ optimize : 3
+#end
+         logical(kind=i4),       intent(in) :: side
+         real(kind=dp),          intent(in) :: m,n
+         real(kind=dp) :: bytes
+         bytes=bytes_hemm(side,m,n)
+     end function bytes_symm
+
+       
+     pure elemental function bytes_herk(n,k) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_herk
+       !dir$ optimize : 3
+#end
+        real(kind=dp),  intent(in) :: n,k
+        real(kind=dp) :: bytes
+        real(kind=dp) :: tmp
+        tmp=0.5_dp*n*(n+1.0_dp)
+        bytes=nano*(n*k+2.0_dp*tmp)*sizeof(n)
+     end function bytes_herk
+
+
+     pure elemental function bytes_syrk(n,k) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_syrk
+       !dir$ optimize : 3
+#end
+        real(kind=dp),  intent(in) :: n,k
+        real(kind=dp) :: bytes
+        bytes=bytes_herk(n,k)
+     end function bytes_syrk
+
+
+     pure elemental function bytes_herk2(n,k) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_herk2
+       !dir$ optimize : 3
+#end
+        real(kind=dp),  intent(in) :: n,k
+        real(kind=dp) :: bytes
+        real(kind=dp) :: tmp
+        tmp=0.5_dp*n*(n+1.0_dp)
+        bytes=nano*((2.0_dp*n*k+2.0_dp*tmp)*sizeof(n)
+     end function bytes_herk2
+
+      
+     pure elemental function bytes_syrk2(n,k) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_syrk2
+       !dir$ optimize : 3
+#end
+        real(kind=dp),  intent(in) :: n,k
+        real(kind=dp) :: bytes
+        bytes=bytes_herk2(n,k)
+     end function bytes_syrk2
+
+     pure elemental function bytes_trmm(side,m,n) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_trmm
+       !dir$ optimize : 3
+#end
+       logical(kind=i4),  intent(in) :: side
+       real(kind=dp),     intent(in) :: m,n
+       real(kind=dp) :: bytes
+       if(side) then
+          bytes=nano*(0.5_dp*(m+1)*m+2.0_dp*m*n)*sizeof(n)
+       else
+          bytes=nano*(0.5_dp*(n+1)*m+2.0_dp*m*n)*sizeof(n)
+       end if
+     end function bytes_trmm
+
+     pure elemental function bytes_trsm(side,m,n) result(bytes)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: bytes_trmm
+       !dir$ optimize : 3
+#end
+       logical(kind=i4),  intent(in) :: side
+       real(kind=dp),     intent(in) :: m,n
+       real(kind=dp) :: bytes
+       bytes=bytes_trmm(side,m,n)
+     end function bytes_trsm
+     
+     
+     ! Gflops calculation functions
+     ! BLAS Level 1
+     pure function flops_asum(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_asum
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_asum(n)+ &
+                   add_ops*fadds_asum(n))
+     end function flops_asum
+
+     pure function flops_axpy(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_asum
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_axpy(n)+ &
+                   add_ops*fadds_axpy(n))
+     end function flops_axpy
+
+     pure function flops_copy(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_copy
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+        flops=0.0_dp
+     end function flops_copy
+
+     pure function flops_iamax(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_iamax
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_iamax(n)+ &
+                   add_ops*fadds_iamax(n))
+     end function flops_iamax
+
+     pure function flops_nrm2(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_nrm2
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_nrm2(n)+ &
+                   add_ops*fadds_nrm2(n))
+     end function flops_nrm2
+
+     pure function flops_dot(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_dot
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_dot(n)+ &
+                   add_ops*fadds_dot(n))
+     end function flops_dot
+
+     pure function flops_scal(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_scal
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_scal(n)+ &
+                   add_ops*fadds_scal(n))
+     end function flops_scal
+
+     pure function flops_swap(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_swap
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+        flops=0.0_dp
+     end function flops_swap
+
+     ! BLAS Level 2
+     
+      pure function flops_gemv(m,n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_gemv
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: m,n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_gemv(m,n)+ &
+                   add_ops*fadds_gemv(m,n))
+     end function flops_gemv
+
+     pure function flops_symv(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_symv
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_gemv(n)
+     end function flops_symv
+
+     pure function flops_hemv(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_hemv
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_symv(n)
+     end function flops_hemv
+
+     pure function flops_trmv(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_trmv
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_trmv(n)+ &
+                   add_ops*fadds_trmv(n))
+     end function flops_trmv
+     
+     pure function flops_trsv(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_trsv
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_trmv(n)
+     end function flops_trsv
+
+     pure function flops_her(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_her
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_ger(n)
+     end function flops_her
+
+     pure function flops_syr(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_syr
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_her(n)
+     end function flops_syr
+
+     pure function flops_ger(m,n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_ger
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: m,n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_ger(m,n)+ &
+                   add_ops*fadds_ger(m,n))
+     end function flops_ger
+
+     pure function flops_her2(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_her2
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=2.0_dp*flops_her(n)
+     end function flops_her2
+
+     pure function flops_syr2(n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_syr
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       flops=flops_her2(n)
+     end function flops_syr2
+
+     ! BLAS Level 3
+       pure function flops_gemm(m,n,k) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_gemm
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: m,n,k
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_gemm(m,n,k)+ &
+                   add_ops*fadds_gemm(m,n,k))
+     end function flops_gemm
+
+     pure function flops_gbmm(m,n,k,kl,ku) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_gemm
+       !dir$ optimize : 3
+#end
+       real(kind=dp),  intent(in) :: n
+       real(kind=dp) :: flops
+       if(m==k) then
+          flops=nano*(mul_ops*fmuls_gbmm(m,n,k,kl,ku)+ &
+                      add_ops*fadds_gemm(m,n,k,kl,ku))
+       else
+          flops=-1.0_dp
+       end if
+     end function flops_gbmm
+
+     pure function flops_hemm(side,m,n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_hemm
+       !dir$ optimize : 3
+#end
+       logical(kind=i4), intent(in) :: side
+       real(kind=dp),  intent(in) :: m,n
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_gemm(side,m,n)+ &
+                   add_ops*fadds_gemm(side,m,n))
+     end function flops_hemm
+
+     pure function flops_hbmm(m,n,kd) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_hemm
+       !dir$ optimize : 3
+#end
+       
+       real(kind=dp),  intent(in) :: m,n,kd
+       real(kind=dp) :: flops
+       flops=nano*(mul_ops*fmuls_gbmm(m,n,m,kd,kd)+ &
+                   add_ops*fadds_gbmm(m,n,m,kd,kd))
+     end function flops_hbmm
+
+     pure function flops_symm(side,m,n) result(flops)
+#if defined(__INTEL_COMPILER) || defined(__ICC)
+       !dir$ attributes inline :: flops_symm
+       !dir$ optimize : 3
+#end
+       logical(kind=i4), intent(in) :: side
+       real(kind=dp),  intent(in) :: m,n
+       real(kind=dp) :: flops
+       flops=flops_hemm(sid,m,n)
+     end function flops_symm
+
+     
+
+     
+     
+       
+     
+     
+     
+     
+     
+     
+     
+     
+     
      
      
      
