9eee656eed6873d3efedfe47cf82b1cd347fb65e
Bernard Gingold <beniekg@gmail.com>
2022-02-19 17:55:43 +0100

    More wrappers added.
    WIP.

--- GPU/GMS_cuBLAS_L3_wrappers.cuh
+++ GPU/GMS_cuBLAS_L3_wrappers.cuh
@@ -138,6 +138,89 @@ cuBLAS_Ssymm_iface(cublasSideMode_t,
                                        __attribute__((aligned(32)));
 
 
+extern "C"
+void
+cuBLAS_Ssyrk_iface(cublasFillMode_t,
+                   cublasOperation_t,
+                   const int32_t,
+                   const int32_t,
+                   const float,
+                   const float * __restrict,
+                   const int32_t,
+                   const float,
+                   float * __restrict,
+                   const int32_t,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                       __attribute__((hot))
+                                       __attribute__((aligned(32)));
+
+
+extern "C"
+void
+cuBLAS_Csyrk_iface(cublasFillMode_t,
+                   cublasOperation_t,
+                   const int32_t,
+                   const int32_t,
+                   const cuComplex,
+                   const cuComplex * __restrict,
+                   const int32_t,
+                   const cuComplex,
+                   cuComplex * __restrict,
+                   const int32_t,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                       __attribute__((hot))
+                                       __attribute__((aligned(32)));
+
+
+extern "C"
+void
+cuBLAS_Strsm_iface(cublasSideMode_t,
+                   cublasFillMode_t,
+                   cublasOperation_t,
+                   cublasDiagType_t,
+                   const int32_t,
+                   const int32_t,
+                   const float,
+                   const float * __restrict,
+                   const int32_t,
+                   float * __restrict,
+                   const int32_t,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                       __attribute__((hot))
+                                       __attribute__((aligned(32)));
+
+
+
+extern "C"
+void
+cuBLAS_Ctrsm_iface(cublasSideMode_t,
+                   cublasFillMode_t,
+                   cublasOperation_t,
+                   cublasDiagType_t,
+                   const int32_t,
+                   const int32_t,
+                   const cuComplex,
+                   const cuComplex * __restrict,
+                   const int32_t,
+                   cuComplex * __restrict,
+                   const int32_t,
+                   cudaError_t * __restrict,
+                   int32_t * __restrict,
+                   uint64_t * __restrict) 
+                                       __attribute__((hot))
+                                       __attribute__((aligned(32)));
+
+
+
+                   
+
+
 
 
 
