2bc0d35d48fcb316423ae9b0c4a55bededc5a498
Bernard Gingold <beniekg@gmail.com>
2022-03-11 17:03:35 +0100

    Radar RCS kernels

---
+++ GPU/GMS_rcs.cuh
@@ -0,0 +1,46 @@
+
+
+#ifndef __GMS_RCS_CUH__
+#define __GMS_RCS_CUH__
+
+#include <cstdint>
+
+extern "C"
+void empirical_K_cuda(const float * __restrict__,
+                      const float * __restrict__,
+                      const float,
+                      float * __restrict__,
+                      const uint32_t,
+                      const uint32_t,
+                      const uint32_t);
+
+extern "C"
+void effective_rcs_cuda(const float 
+                        const float * __restrict__, 
+                        const float, 
+                        const float, 
+                        float * __restrict__,
+                        float * __restrict__,
+                        const uint32_t,
+                        const uint32_t,
+                        const uint32_t);
+
+
+extern "C"
+void bistatic_target_rcs_cuda(const float, 
+		              const float, 
+	                      const float * __restrict__,
+		              const float * __restrict__,
+			      const float * __restrict__,
+			      const float,   
+			      const int32_t, 
+			      float * __restrict,               
+                              float * __restrict,
+                              const uint32_t,
+                              const uint32_t,
+                              const uint32_t);
+
+
+
+
+#endif /*__GMS_RCS_CUH__*/
