6887de387b835e0e44c432dc34a8b17af331a14e
Bernard Gingold <beniekg@gmail.com>
2022-04-01 16:11:30 +0200

    Add files via upload

--- GPU/GMS_platform_errors_kernels_iface.f90
+++ GPU/GMS_platform_errors_kernels_iface.f90
@@ -84,4 +84,91 @@ interface
    
 end interface
 
+#if 0
+void elev_mpath_err_cuda(   const float * __restrict__ R,
+                            const float * __restrict__ ha,
+                            const float * __restrict__ ht,
+                            const float psi0,
+                            const float psiv,
+                            const float psis,
+                            const float * __restrict__ th3,
+                            const float * __restrict__ thmax,
+                            const float * __restrict__ kme,
+                            float * __restrict__ Sigem, // deg, elevation multipath error
+                            const uint32_t n_threads,
+                            const uint32_t type,
+                            const uint32_t n)
+#endif
+
+interface
+
+   subroutine elev_mpath_err_cuda(R,ha,ht,psi0,psiv,psis, &
+                                  th3,thmax,kme,Sigem,    &
+                                  n_threads,type,n) &
+               bind(c,name='elev_mpath_err_cuda')
+               use, intrinsic :: ISO_C_BINDING
+               real(c_float), dimension(*),       intent(in)        :: R
+               real(c_float), dimension(*),       intent(in)        :: ha
+               real(c_float), dimension(*),       intent(in)        :: ht
+               real(c_float),                     intent(in), value :: psi0
+               real(c_float),                     intent(in), value :: psiv
+               real(c_float),                     intent(in), value :: psis
+               real(c_float), dimension(*),       intent(in)        :: th3
+               real(c_float), dimension(*),       intent(in)        :: thmax
+               real(c_float), dimension(*),       intent(in)        :: kme
+               real(c_float), dimension(*),       intent(out)       :: Sigem
+               integer(c_int),                    intent(in), value :: n_threads
+               integer(c_int),                    intent(in), value :: n
+               integer(c_int),                    intent(in), value :: type
+   end subroutine
+
+end interface
+
+#if 0
+void elev_refract_err_cuda(     const float * __restrict__ R,
+                                const float * __restrict__ ha,
+                                const float * __restrict__ ht,
+                                const float psi0,
+                                const float psiv,
+                                const float psis,
+                                const float * __restrict__ th3,
+                                const float * __restrict__ thmax,
+                                const float * __restrict__ kme, 
+                                const float Ns,
+                                const int32_t flucts,
+                                float * __restrict__ Siger, //deg, rms error of elevation measurement
+                                const uint32_t n_threads,
+                                const uint32_t type,
+                                const uint32_t n)
+
+#endif
+
+interface
+
+   subroutine elev_refract_err_cuda(R,ha,ht,psi0,psiv,psis, &
+                                    th3,thmax,kme,Ns,flucts,&
+                                    Siger,n_threads,type,n) &
+               bind(c,name='elev_refract_err_cuda')
+               use, intrinsic :: ISO_C_BINDING
+               real(c_float), dimension(*),       intent(in)        :: R
+               real(c_float), dimension(*),       intent(in)        :: ha
+               real(c_float), dimension(*),       intent(in)        :: ht
+               real(c_float),                     intent(in), value :: psi0
+               real(c_float),                     intent(in), value :: psiv
+               real(c_float),                     intent(in), value :: psis
+               real(c_float), dimension(*),       intent(in)        :: th3
+               real(c_float), dimension(*),       intent(in)        :: thmax
+               real(c_float), dimension(*),       intent(in)        :: kme
+               real(c_float),                     intent(in), value :: Ns
+               integer(c_int),                    intent(in), value :: flucts
+               real(c_float), dimension(*),       intent(out)       :: Siger
+               integer(c_int),                    intent(in), value :: n_threads
+               integer(c_int),                    intent(in), value :: n
+               integer(c_int),                    intent(in), value :: type
+   end subroutine
+
+end interface
+
+
+
 end module platform_errors_kernels_iface
