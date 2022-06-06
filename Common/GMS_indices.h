9de454368d4effae06703cb76d3ea74290b1f50c
Bernard Gingold <beniekg@gmail.com>
2022-06-06 13:08:57 +0200

    Fixed wrong indexing formulae.

--- Common/GMS_indices.h
+++ Common/GMS_indices.h
@@ -52,9 +52,9 @@
 
 #define Idx3D(i,j,k)     ((i) * jdim + ((j) * kdim + (k)))
 
-#define Idx4D(i,j,k,l)   ((i) + idim * ((j) + jdim * ((k) + kdim * (l))))
+#define Idx4D(i,j,k,l)   ((i) * idim + ((j) * jdim + ((k) * kdim + (l))))
 
-#define Idx5D(i,j,k,l,m) ((i) + idim * ((j) + jdim * (k) + kdim * (l) + ldim * (m)))
+#define Idx5D(i,j,k,l,m) ((i) * idim + ((j) * jdim + (k) * kdim + (l) * ldim + (m)))
 
 /*
 	@Warning:
@@ -66,7 +66,7 @@
 
 #define I3D(i,j,k)  ((i) * ny + ((j) * nz + (k)))
 
-#define I4D(i,j,k,l)  ((i) + nx * ((j) + ny * ((k) + nz * ((l)))))
+#define I4D(i,j,k,l)  ((i) * nx + ((j) * ny + ((k) * nz + ((l)))))
 
 /*
 	In this case dimension parameters 
@@ -88,15 +88,100 @@
 */
 #define IS2D(i,idim,j)   i * jdim + j
 
-#define IS3D(i,idim,j,jdim,k) i + idim * (j + jdim * k)
+#define IS3D(i,idim,j,jdim,k) i * idim + (j * jdim + k)
 
-#define IS4D(i,idim,j,jdim,k,kdim,l) i + idim * (j + jdim * (k + kdim * l))
+#define IS4D(i,idim,j,jdim,k,kdim,l) i * idim + (j * jdim + (k * kdim + l))
 
-#define IS5D(i,idim,j,jdim,k,kdim,l,ldim,m) i + idim * (j + jdim * (k + kdim + (l + ldim * m)))
+#define IS5D(i,idim,j,jdim,k,kdim,l,ldim,m) i * idim + (j * jdim + (k * kdim + (l * ldim + m)))
 
 // WRF indexing scheme
 // m_jme and m_kme must be present in the calling scope.
 #define Dim3(j,k,i) ((j) + (m_jme) * ((k) + (m_kme) * (i)))
+
+/*
+ /// 1-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+      
+        T& operator()(long i) {
+
+            return data[i*_stride[0]];
+        }
+
+     
+
+        /// 2-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+        /// @param[in] j index for dimension 1
+      
+        T& operator()(long i, long j) {
+
+            return data[i*_stride[0]+j*_stride[1]];
+        }
+
+      
+        /// 3-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+        /// @param[in] j index for dimension 1
+        /// @param[in] k index for dimension 2
+       
+        T& operator()(long i, long j, long k) {
+
+            return data[i*_stride[0]+j*_stride[1]+k*_stride[2]];
+        }
+
+     
+
+        /// 4-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+        /// @param[in] j index for dimension 1
+        /// @param[in] k index for dimension 2
+        /// @param[in] l index for dimension 3
+     
+        T& operator()(long i, long j, long k, long l) {
+
+            return data[i*_stride[0]+j*_stride[1]+k*_stride[2]+
+                           l*_stride[3]];
+        }
+
+     
+
+        /// 5-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+        /// @param[in] j index for dimension 1
+        /// @param[in] k index for dimension 2
+        /// @param[in] l index for dimension 3
+        /// @param[in] m index for dimension 4
+      
+        T& operator()(long i, long j, long k, long l, long m) {
+
+            return data[i*_stride[0]+j*_stride[1]+k*_stride[2]+
+                           l*_stride[3]+m*_stride[4]];
+        }
+
+      
+
+        /// 6-d indexing operation 
+
+        /// @param[in] i index for dimension 0
+        /// @param[in] j index for dimension 1
+        /// @param[in] k index for dimension 2
+        /// @param[in] l index for dimension 3
+        /// @param[in] m index for dimension 4
+        /// @param[in] n index for dimension 5
+        
+        T& operator()(long i, long j, long k, long l, long m, long n) {
+
+            return data[i*_stride[0]+j*_stride[1]+k*_stride[2]+
+                           l*_stride[3]+m*_stride[4]+n*_stride[5]];
+        }
+
+       
+*/
 
 
 
