e7c1cb4a46dde2162b51da6e55baf709252ecd0e
Bernard Gingold <beniekg@gmail.com>
2022-08-26 12:36:44 +0200

    More unrolled kernels added.
    WIP.

--- Infrared EOS/GMS_eos_sensor_simd.f90
+++ Infrared EOS/GMS_eos_sensor_simd.f90
@@ -2413,9 +2413,9 @@ module eos_sensor_simd
            real(kind=dp), dimension(1:n), intent(in)  :: freq
            integer(kind=i4),              intent(in)  :: n
            type(ZMM8r8_t),                intent(in)  :: T      !vector enhanced i.e. broadcast
-           type(ZMM8r8_t), parameter :: twopi = YMM8r4_t(6.283185307179586476925286766559_sp)
-           type(ZMM8r8_r), parameter :: half  = YMM8r4_t(0.5_sp)
-           type(ZMM8r8_t), parameter :: one   = YMM8r4_t(1.0_sp)
+           type(ZMM8r8_t), parameter :: twopi = YMM8r4_t(6.283185307179586476925286766559_dp)
+           type(ZMM8r8_r), parameter :: half  = YMM8r4_t(0.5_dp)
+           type(ZMM8r8_t), parameter :: one   = YMM8r4_t(1.0_dp)
            type(ZMM8r8_t) :: arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7
            !dir$ attributes align : 64 :: arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7
            type(ZMM8r8_t) :: arg8,arg9,arg10,arg11,arg12,arg13,arg14,arg15
@@ -2581,6 +2581,255 @@ module eos_sensor_simd
               return
             end if
        end subroutine const_flux_spectr_unroll_16x_zmm8r8
+
+
+       subroutine const_flux_spectr_unroll_8x_zmm8r8(Phi0f,Phi0,freq,n,T)
+           !dir$ optimize:3
+           !dir$ attributes code_align : 32 ::  const_flux_spectr_unroll_8x_zmm8r8
+           !dir$ attributes forceinline ::   const_flux_spectr_unroll_8x_zmm8r8
+           !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" ::  const_flux_spectr_unroll_8x_zmm8r8
+           real(kind=dp), dimension(1:n), intent(out) :: Phi0f
+           type(ZMM8r8_t),                intent(in)  :: Phi0   !vector enhanced i.e. broadcast
+           real(kind=dp), dimension(1:n), intent(in)  :: freq
+           integer(kind=i4),              intent(in)  :: n
+           type(ZMM8r8_t),                intent(in)  :: T      !vector enhanced i.e. broadcast
+           type(ZMM8r8_t), parameter :: twopi = YMM8r4_t(6.283185307179586476925286766559_dp)
+           type(ZMM8r8_r), parameter :: half  = YMM8r4_t(0.5_dp)
+           type(ZMM8r8_t), parameter :: one   = YMM8r4_t(1.0_dp)
+           type(ZMM8r8_t) :: arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7
+           !dir$ attributes align : 64 :: arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7
+           type(ZMM8r8_t) :: f0,f1,f2,f3,f4,f5,f6,f7
+           !dir$ attributes align : 64 :: f0,f1,f2,f3,f4,f5,f6,f7
+           type(ZMM8r8_t) :: c0,c1,c2,c3,c4,c5,c6,c7
+           !dir$ attributes align : 64 :: c0,c1,c2,c3,c4,c5,c6,c7
+           type(ZMM8r8_t) :: sa0,sa1,sa2,sa3,sa4,sa5,sa6,sa7
+           !dir$ attributes align : 64 :: sa0,sa1,sa2,sa3,sa4,sa,sa6,sa7
+           type(ZMM8r8_t) :: hT,Phi0T
+           !dir$ attributes align : 64 :: hT,Phi0t
+           real(kind=dp) :: f,c,sa,arg
+           integer(kind=i4) :: i,ii,j
+           hT.v    = half.v*T.v
+           Phi0T.v = Phi0.v*T.v
+           if(n<8) then
+              return
+           else if(n==8) then
+              do i,n
+                 f           = freq(i)
+                 c           = twopi.v(0)*f*hT.v(0)
+                 sa          = sin(c)/c
+                 arg         = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(i)    = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+           else if(n>8 .and. n<=64) then
+              ! rolled version
+              !dir$ assume_aligned Phi0f:64
+              !dir$ assume_aligned freq:64
+              do i=1,iand(n,not(7)),8
+                 !dir$ vector aligned
+                 !dir$ ivdep
+                 !dir$ vector vectorlength(8)
+                 !dir$ vector always
+                 do ii=0,7
+                    f0.v(ii)   = freq(i+ii)
+                    c0.v(ii)   = twopi.v*f0.v*hT.v
+                    sa0.v(ii)  = sin(c0.v(ii))/c0.v(ii)
+                    arg0.v(ii) = one.v-(f0.v*hT.v*f0.v*hT.v)
+                    Phi0f(i+ii)= Phi0T.v(ii)*(sa0.v(ii)/arg0.v(ii))
+                 end do
+              end do
+              ! Remainder loop
+              !dir$ loop_count max=8,min=1,avg=4
+              do j=i,n
+                 f           = freq(j)
+                 c           = twopi.v(0)*f*hT.v(0)
+                 sa          = sin(c)/c
+                 arg         = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(j)    = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+            else if(n>64) then
+              ! 16x-unrolled version
+              !dir$ assume_aligned Phi0f:64
+              !dir$ assume_aligned freq:64
+              do i=1,iand(n,not(7)),64
+                  call mm_prefetch(freq(i*64),FOR_K_PREFETCH_T1)
+                 !dir$ vector aligned
+                 !dir$ ivdep
+                 !dir$ vector vectorlength(8)
+                 !dir$ vector always
+                 do ii=0,7
+                    f0.v(ii)       = freq(i+ii)
+                    c0.v(ii)       = twopi.v*f0.v*hT.v
+                    sa0.v(ii)      = sin(c0.v(ii))/c0.v(ii)
+                    arg0.v(ii)     = one.v-(f0.v*hT.v*f0.v*hT.v)
+                    Phi0f(i+ii)    = Phi0T.v(ii)*(sa0.v(ii)/arg0.v(ii))
+                    f1.v(ii)       = freq(i+8+ii)
+                    c1.v(ii)       = twopi.v*f1.v*hT.v
+                    sa1.v(ii)      = sin(c1.v(ii))/c1.v(ii)
+                    arg1.v(ii)     = one.v-(f1.v*hT.v*f1.v*hT.v)
+                    Phi0f(i+8+ii) = Phi0T.v(ii)*(sa1.v(ii)/arg1.v(ii))
+                    f2.v(ii)       = freq(i+16+ii)
+                    c2.v(ii)       = twopi.v*f2.v*hT.v
+                    sa2.v(ii)      = sin(c2.v(ii))/c2.v(ii)
+                    arg2.v(ii)     = one.v-(f2.v*hT.v*f2.v*hT.v)
+                    Phi0f(i+16+ii) = Phi0T.v(ii)*(sa2.v(ii)/arg2.v(ii))
+                    f3.v(ii)       = freq(i+24+ii)
+                    c3.v(ii)       = twopi.v*f3.v*hT.v
+                    sa3.v(ii)      = sin(c3.v(ii))/c3.v(ii)
+                    arg3.v(ii)     = one.v-(f3.v*hT.v*f3.v*hT.v)
+                    Phi0f(i+24+ii) = Phi0T.v(ii)*(sa3.v(ii)/arg3.v(ii))
+                    f4.v(ii)       = freq(i+32+ii)
+                    c4.v(ii)       = twopi.v*f4.v*hT.v
+                    sa4.v(ii)      = sin(c4.v(ii))/c4.v(ii)
+                    arg4.v(ii)     = one.v-(f4.v*hT.v*f4.v*hT.v)
+                    Phi0f(i+32+ii) = Phi0T.v(ii)*(sa4.v(ii)/arg4.v(ii))
+                    f5.v(ii)       = freq(i+40+ii)
+                    c5.v(ii)       = twopi.v*f5.v*hT.v
+                    sa5.v(ii)      = sin(c5.v(ii))/c5.v(ii)
+                    arg5.v(ii)     = one.v-(f5.v*hT.v*f5.v*hT.v)
+                    Phi0f(i+40+ii) = Phi0T.v(ii)*(sa5.v(ii)/arg5.v(ii))
+                    f6.v(ii)       = freq(i+48+ii)
+                    c6.v(ii)       = twopi.v*f6.v*hT.v
+                    sa6.v(ii)      = sin(c6.v(ii))/c6.v(ii)
+                    arg6.v(ii)     = one.v-(f6.v*hT.v*f6.v*hT.v)
+                    Phi0f(i+48+ii) = Phi0T.v(ii)*(sa6.v(ii)/arg6.v(ii))
+                    f7.v(ii)       = freq(i+56+ii)
+                    c7.v(ii)       = twopi.v*f7.v*hT.v
+                    sa7.v(ii)      = sin(c7.v(ii))/c7.v(ii)
+                    arg7.v(ii)     = one.v-(f7.v*hT.v*f7.v*hT.v)
+                    Phi0f(i+56+ii)= Phi0T.v(ii)*(sa7.v(ii)/arg7.v(ii))
+                  end do
+               end do
+              ! Remainder loop
+              !dir$ loop_count max=8,min=1,avg=4
+              do j=i,n
+                 f        = freq(j)
+                 c        = twopi.v(0)*f*hT.v(0)
+                 sa       = sin(c)/c
+                 arg      = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(j) = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+            end if
+       end subroutine const_flux_spectr_unroll_8x_zmm8r8
+
+
+       subroutine const_flux_spectr_unroll_4x_zmm8r8(Phi0f,Phi0,freq,n,T)
+           !dir$ optimize:3
+           !dir$ attributes code_align : 32 ::  const_flux_spectr_unroll_4x_zmm8r8
+           !dir$ attributes forceinline ::   const_flux_spectr_unroll_4x_zmm8r8
+           !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" ::  const_flux_spectr_unroll_4x_zmm8r8
+           real(kind=dp), dimension(1:n), intent(out) :: Phi0f
+           type(ZMM8r8_t),                intent(in)  :: Phi0   !vector enhanced i.e. broadcast
+           real(kind=dp), dimension(1:n), intent(in)  :: freq
+           integer(kind=i4),              intent(in)  :: n
+           type(ZMM8r8_t),                intent(in)  :: T      !vector enhanced i.e. broadcast
+           type(ZMM8r8_t), parameter :: twopi = YMM8r4_t(6.283185307179586476925286766559_dp)
+           type(ZMM8r8_r), parameter :: half  = YMM8r4_t(0.5_dp)
+           type(ZMM8r8_t), parameter :: one   = YMM8r4_t(1.0_dp)
+           type(ZMM8r8_t) :: arg0,arg1,arg2,arg3
+           !dir$ attributes align : 64 :: arg0,arg1,arg2,arg3
+           type(ZMM8r8_t) :: f0,f1,f2,f3
+           !dir$ attributes align : 64 :: f0,f1,f2,f3
+           type(ZMM8r8_t) :: c0,c1,c2,c3
+           !dir$ attributes align : 64 :: c0,c1,c2,c3
+           type(ZMM8r8_t) :: sa0,sa1,sa2,sa3
+           !dir$ attributes align : 64 :: sa0,sa1,sa2,sa3
+           type(ZMM8r8_t) :: hT,Phi0T
+           !dir$ attributes align : 64 :: hT,Phi0t
+           real(kind=dp) :: f,c,sa,arg
+           integer(kind=i4) :: i,ii,j
+           hT.v    = half.v*T.v
+           Phi0T.v = Phi0.v*T.v
+           if(n<8) then
+              return
+           else if(n==8) then
+              do i,n
+                 f           = freq(i)
+                 c           = twopi.v(0)*f*hT.v(0)
+                 sa          = sin(c)/c
+                 arg         = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(i)    = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+           else if(n>8 .and. n<=32) then
+              ! rolled version
+              !dir$ assume_aligned Phi0f:64
+              !dir$ assume_aligned freq:64
+              do i=1,iand(n,not(7)),8
+                 !dir$ vector aligned
+                 !dir$ ivdep
+                 !dir$ vector vectorlength(8)
+                 !dir$ vector always
+                 do ii=0,7
+                    f0.v(ii)   = freq(i+ii)
+                    c0.v(ii)   = twopi.v*f0.v*hT.v
+                    sa0.v(ii)  = sin(c0.v(ii))/c0.v(ii)
+                    arg0.v(ii) = one.v-(f0.v*hT.v*f0.v*hT.v)
+                    Phi0f(i+ii)= Phi0T.v(ii)*(sa0.v(ii)/arg0.v(ii))
+                 end do
+              end do
+              ! Remainder loop
+              !dir$ loop_count max=8,min=1,avg=4
+              do j=i,n
+                 f           = freq(j)
+                 c           = twopi.v(0)*f*hT.v(0)
+                 sa          = sin(c)/c
+                 arg         = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(j)    = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+            else if(n>64) then
+              ! 16x-unrolled version
+              !dir$ assume_aligned Phi0f:64
+              !dir$ assume_aligned freq:64
+              do i=1,iand(n,not(7)),32
+                  call mm_prefetch(freq(i*32),FOR_K_PREFETCH_T1)
+                 !dir$ vector aligned
+                 !dir$ ivdep
+                 !dir$ vector vectorlength(8)
+                 !dir$ vector always
+                 do ii=0,7
+                    f0.v(ii)       = freq(i+ii)
+                    c0.v(ii)       = twopi.v*f0.v*hT.v
+                    sa0.v(ii)      = sin(c0.v(ii))/c0.v(ii)
+                    arg0.v(ii)     = one.v-(f0.v*hT.v*f0.v*hT.v)
+                    Phi0f(i+ii)    = Phi0T.v(ii)*(sa0.v(ii)/arg0.v(ii))
+                    f1.v(ii)       = freq(i+8+ii)
+                    c1.v(ii)       = twopi.v*f1.v*hT.v
+                    sa1.v(ii)      = sin(c1.v(ii))/c1.v(ii)
+                    arg1.v(ii)     = one.v-(f1.v*hT.v*f1.v*hT.v)
+                    Phi0f(i+8+ii) = Phi0T.v(ii)*(sa1.v(ii)/arg1.v(ii))
+                    f2.v(ii)       = freq(i+16+ii)
+                    c2.v(ii)       = twopi.v*f2.v*hT.v
+                    sa2.v(ii)      = sin(c2.v(ii))/c2.v(ii)
+                    arg2.v(ii)     = one.v-(f2.v*hT.v*f2.v*hT.v)
+                    Phi0f(i+16+ii) = Phi0T.v(ii)*(sa2.v(ii)/arg2.v(ii))
+                    f3.v(ii)       = freq(i+24+ii)
+                    c3.v(ii)       = twopi.v*f3.v*hT.v
+                    sa3.v(ii)      = sin(c3.v(ii))/c3.v(ii)
+                    arg3.v(ii)     = one.v-(f3.v*hT.v*f3.v*hT.v)
+                    Phi0f(i+24+ii) = Phi0T.v(ii)*(sa3.v(ii)/arg3.v(ii))
+                  end do
+               end do
+              ! Remainder loop
+              !dir$ loop_count max=8,min=1,avg=4
+              do j=i,n
+                 f        = freq(j)
+                 c        = twopi.v(0)*f*hT.v(0)
+                 sa       = sin(c)/c
+                 arg      = one.v(0)-(f*hT.v(0)*f*hT.v(0)
+                 Phi0f(j) = Phi0T.v(0)*(sa/arg)
+              end do
+              return
+            end if
+       end subroutine const_flux_spectr_unroll_4x_zmm8r8
+
+
+       
+
+
 
 
        
