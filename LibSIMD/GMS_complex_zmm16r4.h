a6aa28c12424a0d4ce075629d51abdcc1d44b4a3
Bernard Gingold <beniekg@gmail.com>
2023-03-30 17:23:54 +0200

    Complex exponent kernels.
    WIP.

--- LibSIMD/GMS_complex_zmm16r4.h
+++ LibSIMD/GMS_complex_zmm16r4.h
@@ -653,6 +653,42 @@ SOFTWARE.
                                                          __attribute__((noinline))
 							 __attribute__((hot))
                                                          __attribute__((aligned(32))); 
-            
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+
+
+            void cexp_zmm16r4_u(       const float * __restrict xre,
+                                       const float * __restrict xim,
+                                       float * __restrict cexpr,
+                                       float * __restrict cexpi )
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32))); 
+
+
+            void cexp_zmm16r4_a(       const float * __restrict __attribute__((aligned(64))) xre,
+                                       const float * __restrict __attribute__((aligned(64))) xim,
+                                       float * __restrict __attribute__((aligned(64))) cexpr,
+                                       float * __restrict __attribute__((aligned(64))) cexpi )
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32))); 
+
+
+            void cexp_zmm16r4(       const __m512 xre,
+                                     const __m512 xim,
+                                     __m512 * __restrict cexpr,
+                                     __m512 * __restrict cexpi)
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));    
+
+
+
+
 
 #endif /*__GMS_COMPLEX_ZMM16R4_H__*/
