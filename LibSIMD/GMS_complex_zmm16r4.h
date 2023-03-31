273ea447784d45c01d9a1dcb24dbe1c29be0b3a5
Bernard Gingold <beniekg@gmail.com>
2023-03-31 10:20:25 +0200

    Complex magnitude kernels.

--- LibSIMD/GMS_complex_zmm16r4.h
+++ LibSIMD/GMS_complex_zmm16r4.h
@@ -1023,7 +1023,40 @@ SOFTWARE.
                                                              __attribute__((aligned(32))); 
 
 
+            //////////////////////////////////////////////////////////////////////////////////
 
+
+             void cmagnitude_zmm16r4_u(       const float * __restrict xre,
+                                              const float * __restrict xim,
+                                              const float * __restrict yre,
+                                              const float * __restrict yim,
+                                              float * __restrict mre)
+                                                             __attribute__((vectorcall))
+                                                             __attribute__((noinline))
+							     __attribute__((hot))
+                                                             __attribute__((aligned(32))); 
+
+
+              void cmagnitude_zmm16r4_a(      const float * __restrict __attribute__((aligned(64)))  xre,
+                                              const float * __restrict __attribute__((aligned(64)))  xim,
+                                              const float * __restrict __attribute__((aligned(64)))  yre,
+                                              const float * __restrict __attribute__((aligned(64)))  yim,
+                                              float * __restrict __attribute__((aligned(64)))  mre) 
+                                                             __attribute__((vectorcall))
+                                                             __attribute__((noinline))
+							     __attribute__((hot))
+                                                             __attribute__((aligned(32))); 
+
+
+              void cmagnitude_zmm16r4(        const __m512 xre,
+                                              const __m512 xim,
+                                              const __m512 yre,
+                                              const __m512 yim,
+                                              __m512 * __restrict  mre)
+                                                              __attribute__((vectorcall))
+                                                             __attribute__((noinline))
+							     __attribute__((hot))
+                                                             __attribute__((aligned(32))); 
 
 
 
