74409586a382e8be5d970efcfcea4610ae9caba5
Bernard Gingold <beniekg@gmail.com>
2023-04-01 13:24:20 +0200

    Complex tanh kernels.

--- LibSIMD/GMS_complex_zmm16r4.h
+++ LibSIMD/GMS_complex_zmm16r4.h
@@ -677,6 +677,39 @@ SOFTWARE.
             ////////////////////////////////////////////////////////////////////////////
 
 
+              void ctanh_zmm16r4_u( const float * __restrict xre,
+                                    const float * __restrict xim,
+                                    float * __restrict tanr,
+                                    float * __restrict tani) 
+                                                        __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));  
+
+
+              void ctanh_zmm16r4_a( const float * __restrict __attribute__((aligned(64))) xre,
+                                    const float * __restrict __attribute__((aligned(64))) xim,
+                                    float * __restrict tanr,
+                                    float * __restrict tani) 
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));   
+
+
+              void ctanh_zmm16r4(   const __m512 xre,
+                                    const __m512 xim,
+                                    __m512 * __restrict tanr,
+                                    __m512 * __restrict tani)  
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));    
+
+
+           /////////////////////////////////////////////////////////////////////////////
+
+
 
              void ceq_zmm16r4_u(      const float * __restrict xre,
                                       const float * __restrict xim,
