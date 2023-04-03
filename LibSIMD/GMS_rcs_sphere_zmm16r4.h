c5db2a4a4da9660386703ccc85536afdca9e8120
Bernard Gingold <beniekg@gmail.com>
2023-04-03 10:14:58 +0200

    S1_f3216_zmm16r4 kernels.

--- LibSIMD/GMS_rcs_sphere_zmm16r4.h
+++ LibSIMD/GMS_rcs_sphere_zmm16r4.h
@@ -233,6 +233,42 @@ SOFTWARE.
                                                          __attribute__((aligned(32)));
 
 
+                 /*
+                       Formula 3.2-16, optics contribution at upper end of resonance region
+                   */
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+                    void S1_f3216_zmm16r4(const __m512 k0a,
+                                          const __m512 tht,
+                                          __m512 * __restrict S1r,
+                                          __m512 * __restrict S1i)    
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
+
+
+                    void S1_f3216_zmm16r4_a(const float * __restrict __attribute__((aligned(64))) pk0a,
+                                            const float * __restrict __attribute__((aligned(64))) ptht,
+                                            float * __restrict __attribute__((aligned(64))) S1r,
+                                            float * __restrict __attribute__((aligned(64))) S1i)
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
+
+
+                    void S1_f3216_zmm16r4_u(const float * __restrict  pk0a,
+                                            const float * __restrict  ptht,
+                                            float * __restrict  S1r,
+                                            float * __restrict  S1i)
+                                                          __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
 
 
 
