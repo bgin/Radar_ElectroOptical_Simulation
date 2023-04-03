6d55adb1ea5b47af448755c501f3773f153a1943
Bernard Gingold <beniekg@gmail.com>
2023-04-03 09:27:49 +0200

    rcs_f324_zmm16r4 kernels.

---
+++ LibSIMD/GMS_rcs_sphere_zmm16r4.h
@@ -0,0 +1,101 @@
+
+
+
+#ifndef __GMS_RCS_SPHERE_ZMM16R4_H__
+#define __GMS_RCS_SPHERE_ZMM16R4_H__ 040120231245
+
+
+
+/*MIT License
+Copyright (c) 2020 Bernard Gingold
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+    const unsigned int GMS_RCS_SPHERE_ZMM16R4_MAJOR = 1U;
+    const unsigned int GMS_RCS_SPHERE_ZMM16R4_MINOR = 0U;
+    const unsigned int GMS_RCS_SPHERE_ZMM16R4_MICRO = 0U;
+    const unsigned int GMS_RCS_SPHERE_ZMM16R4_FULLVER =
+      1000U*GMS_RCS_SPHERE_ZMM16R4_MAJOR+
+      100U*GMS_RCS_SPHERE_ZMM16R4_MINOR+
+      10U*GMS_RCS_SPHERE_ZMM16R4_MICRO;
+    const char * const GMS_RCS_SPHERE_ZMM16R4_CREATION_DATE = "04-01-2023 12:45 AM +00200 (WED 04 JAN 2023 GMT+2)";
+    const char * const GMS_RCS_SPHERE_ZMM16R4_BUILD_DATE    = __DATE__ ":" __TIME__;
+    const char * const GMS_RCS_SPHERE_ZMM16R4_AUTHOR        = "Programmer: Bernard Gingold, contact: beniekg@gmail.com";
+    const char * const GMS_RCS_SPHERE_ZMM16R4_DESCRIPTION   = "AVX512 optimized Sphere Radar Cross Section (analytic) functionality."
+
+
+#include <immintrin.h>
+
+
+
+
+              /*
+                       Radar Cross Section Handbook 1, page 147, formula 3.2-4
+                       Backscattering function ,resonance region 0.4 .le. k0a .le. 20.0
+                       Theta = 0, far-field
+                       Valid for k0a < 1 only!!
+                   */
+
+
+                     void rcs_f324_zmm16r4(   const __m512 k0a, // size of sphere expressed in wavenumber units
+                                               __m512 * __restrict F0r, // the results
+                                               __m512 * __restrict F0i)
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
+
+
+                     void rcs_f324_zmm16r4_a(  const float * __restrict __attribute__((aligned(64))) pk0a, // size of sphere expressed in wavenumber units
+                                               __m512 * __restrict F0r, // the results
+                                               __m512 * __restrict F0i)
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
+
+
+                     void rcs_f324_zmm16r4_u(  const float * __restrict  pk0a, // size of sphere expressed in wavenumber units
+                                               __m512 * __restrict F0r, // the results
+                                               __m512 * __restrict F0i) 
+                                                         __attribute__((vectorcall))
+                                                         __attribute__((noinline))
+							 __attribute__((hot))
+                                                         __attribute__((aligned(32)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__GMS_RCS_SPHERE_ZMM16R4_H__*/
