ff6627e07532ee875827ad2265b38d87684ef413
Bernard Gingold <beniekg@gmail.com>
2023-07-18 14:33:39 +0200

    Added a kernel description.

---
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.f90
@@ -0,0 +1,125 @@
+
+
+
+
+!/*MIT License
+!Copyright (c) 2020 Bernard Gingold
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+!*/
+
+module rcs_cylinder_zmm16r4
+
+
+!===================================================================================85
+ !---------------------------- DESCRIPTION ------------------------------------------85
+ !
+ !
+ !
+ !          Module  name:
+ !                         rcs_cylinder_zmm16r4
+ !          
+ !          Purpose:
+ !                        Various characteristics of analytically derived Radar
+ !                        Cross Section of cylindrical objects  
+ !                        Based  on George T. Ruck, Donald E. Barrick , William D. Stuart , 
+ !                        - "Radar Cross Section Handbook 1 and 2" (1970, Kluwer Academic Plenum Publishers) 
+ !                        This module contains only explicitly vectorized (SIMD)
+ !                        
+ !          History:
+ !                        Date: 08-07-2023
+ !                        Time: 14:21 GMT+2
+ !                        
+ !          Version:
+ !
+ !                      Major: 1
+ !                      Minor: 0
+ !                      Micro: 0
+ !
+ !          Author:  
+ !                      Bernard Gingold
+ !          
+ !                 
+ !          References:
+ !         
+ !                      George T. Ruck, Donald E. Barrick , William D. Stuart
+ !                      Radar Cross Section Handbook 1 and 2" (1970, Kluwer Academic Plenum Publishers)     
+ !         
+ !          E-mail:
+ !                  
+ !                      beniekg@gmail.com
+!==================================================================================85
+    ! Tab:5 col - Type and etc.. definitions
+    ! Tab:10,11 col - Type , function and subroutine code blocks.
+
+    use mod_kinds,    only : i4,sp
+    use mod_vectypes, only : ZMM16r4_t
+    use avx512_cvec16
+    
+    public
+    implicit none
+    
+     ! Major version
+     integer(kind=i4),  parameter :: RCS_CYLINDER_ZMM16R4_MAJOR = 1
+     ! Minor version
+     integer(kind=i4),  parameter :: RCS_CYLINDER_ZMM16R4_MINOR = 0
+     ! Micro version
+     integer(kind=i4),  parameter :: RCS_CYLINDER_ZMM16R4_MICRO = 0
+     ! Full version
+     integer(kind=i4),  parameter :: RCS_CYLINDER_ZMM16R4_FULLVER =   &
+            1000*RCS_CYLINDER_ZMM16R4_MAJOR+100*RCS_CYLINDER_ZMM16R4_MINOR+10*RCS_CYLINDER_ZMM16R4_MICRO
+     ! Module creation date
+     character(*),        parameter :: RCS_CYLINDER_ZMM16R4_CREATE_DATE = "18-07-2022 14:30 +00200 (TUE 18 JUL 2023 GMT+2)"
+     ! Module build date
+     character(*),        parameter :: RCS_CYLINDER_ZMM16R4_BUILD_DATE  = __DATE__ " " __TIME__
+     ! Module author info
+     character(*),        parameter :: RCS_CYLINDER_ZMM16R4_AUTHOR      = "Programmer: Bernard Gingold, contact: beniekg@gmail.com"
+     ! Short description
+     character(*),        parameter :: RCS_CYLINDER_ZMM16R4_SYNOPSIS    = "Analytical Cylindrical objects RCS characteristics and models explicitly vectorized (SIMD)."
+    
+
+    
+     contains
+     
+     
+                   !/* 
+                   !      Low frequency scattering widths (k0a << 1).
+                   !      Backscatter scattering width for E-field 
+                   !      cylinder-parallel,formula 4.1-19
+                   ! */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end module rcs_cylinder_zmm16r4
