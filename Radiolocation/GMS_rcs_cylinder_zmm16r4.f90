3ed755d203d2726d8369f5b1fdf5bb0c48406a65
Bernard Gingold <beniekg@gmail.com>
2023-07-20 08:11:23 +0200

    rcs_f4123_zmm16r4 kernel.

--- Radiolocation/GMS_rcs_cylinder_zmm16r4.f90
+++ Radiolocation/GMS_rcs_cylinder_zmm16r4.f90
@@ -1546,6 +1546,24 @@ module rcs_cylinder_zmm16r4
                      end do
               end subroutine rcs_f4122_zmm16r4_rolled
               
+              
+               ! /*
+               !        Forward scattering widths, E-field.
+               !        Formula 4.1-23
+               !    */
+               
+               pure function rcs_f4123_zmm16r4(a,k0a) result(rcs)
+               
+                   !dir$ optimize:3
+                   !dir$ attributes code_align : 32 :: rcs_f4123_zmm16r4
+                   !dir$ attributes forceinline :: rcs_f4123_zmm16r4
+                   !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" :: rcs_f4123_zmm16r4
+                   type(ZMM16r4_t), intent(in) :: a
+                   type(ZMM16r4_t), intent(in) :: k0a
+                   type(ZMM16r4_t) :: rcs
+                   rcs = rcs_f4120_zmm16r4(a,k0a)
+               end function rcs_f4123_zmm16r4
+              
 
 
 
