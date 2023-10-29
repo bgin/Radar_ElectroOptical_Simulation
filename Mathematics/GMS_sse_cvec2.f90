e62c9f4d17ce8dc06a5e21d919abc4b6c6f0c0e7
Bernard Gingold <beniekg@gmail.com>
2023-10-29 19:28:54 +0100

    Added:  sse complex (double).

--- Mathematics/GMS_sse_cvec2.f90
+++ Mathematics/GMS_sse_cvec2.f90
@@ -1,5 +1,25 @@
 
 
+
+!/*MIT License
+!Copyright (c) 2020 Bernard Gingold
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+!*/
+
 module sse_cvec2
 
  !===================================================================================85
@@ -1527,8 +1547,7 @@ module sse_cvec2
 !        integer(c_char), parameter :: all_ones = Z'FF'
 !#endif
         ! EXec code ....
-        ratio = 0.0_dp
-        denom = 0.0_dp
+       
 !#if   (USE_INTRINSIC_VECTOR_COMPARE) == 1
 !        tre.xmm = abs(rhs.re)
 !        tim.xmm = abs(rhs.im)
@@ -1536,8 +1555,7 @@ module sse_cvec2
 !#else
         bres = abs(rhs.re) >= abs(rhs.im)
 !#endif
-        re_part = v8_n0
-        im_part = v8_n0
+        
 !#if   (USE_INTRINSIC_VECTOR_COMPARE) == 1
 !        if(mask_gte == all_ones) then
 !#elif
