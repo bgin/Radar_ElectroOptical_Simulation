f32f2ecd9a87b83539c9e0380c4fa4739cafda75
Bernard Gingold <beniekg@gmail.com>
2024-02-06 17:54:45 +0100

    Added: random exponential subroutine.
    unrolled 16x.

--- Radiolocation/GMS_um_diffracted_waves.f90
+++ Radiolocation/GMS_um_diffracted_waves.f90
@@ -3686,6 +3686,433 @@ module um_diffracted_waves
                end do
             end if 
        end subroutine rand_exp_init_idxrw_unroll8x
+       
+       
+       subroutine rand_exp_init_idxrw_unroll16x()
+            !dir$ optimize:3
+            !dir$ attributes code_align : 32 ::  rand_exp_init_idxrw_unroll16x
+            !dir$ attributes forceinline ::  rand_exp_init_idxrw_unroll16x
+            !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" :: rand_exp_init_idxrw_unroll16x
+            use rand_scalar_distributions, only : random_exponential_clamped
+            use urban_model
+            ! Locals
+            real(sp), automatic :: r00,r10,r20,r30
+            real(sp), automatic :: r01,r11,r21,r31
+            real(sp), automatic :: r02,r12,r22,r32
+            real(sp), automatic :: r03,r13,r23,r33
+            integer(i4), automatic :: j,i,m,m1
+            if(nidxrw_diff()) then
+               m = mod(nidsrw,16)
+               if(m/=0) then
+                   !dir$ assume_aligned idsrw:32
+                   !dir$ assume_aligned iderw:32
+                   !dir$ assume_aligned idwrw:32
+                   !dir$ assume_aligned idnrw:32
+                   !dir$ vector aligned
+                   !dir$ ivdep
+                   !dir$ vector vectorlength(4)
+                   !dir$ vector always
+                   do i=1, nbpc
+                      do j=1,m
+                          r00 = random_exponential_clamped()
+                          idsrw(j+0,i) = r00
+                          r10 = random_exponential_clamped()
+                          iderw(j+0,i) = r10
+                          r20 = random_exponential_clamped()
+                          idwrw(j+0,i) = r20
+                          r30 = random_exponential_clamped()
+                          idnrw(j+0,i) = r30
+                      end do
+                   end do
+                   if(nidsrw<16) return
+               end if
+               m1 = m+1
+               do i=1, nbpc
+               !dir$ assume_aligned idsrw:32
+               !dir$ assume_aligned iderw:32
+               !dir$ assume_aligned idwrw:32
+               !dir$ assume_aligned idnrw:32
+               !dir$ vector aligned
+               !dir$ ivdep
+               !dir$ vector vectorlength(4)
+               !dir$ vector always
+                  do j=m1, nidsrw,16
+                     r00 = random_exponential_clamped()
+                     idsrw(j+0,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+0,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+0,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+0,i) = r30
+                     r01 = random_exponential_clamped()
+                     idsrw(j+1,i) = r01
+                     r11 = random_exponential_clamped()
+                     iderw(j+1,i) = r11
+                     r21 = random_exponential_clamped()
+                     idwrw(j+1,i) = r21
+                     r31 = random_exponential_clamped()
+                     idnrw(j+1,i) = r31
+                     r02 = random_exponential_clamped()
+                     idsrw(j+2,i) = r02
+                     r12 = random_exponential_clamped()
+                     iderw(j+2,i) = r12
+                     r22 = random_exponential_clamped()
+                     idwrw(j+2,i) = r22
+                     r32 = random_exponential_clamped()
+                     idnrw(j+2,i) = r32
+                     r03 = random_exponential_clamped()
+                     idsrw(j+3,i) = r03
+                     r13 = random_exponential_clamped()
+                     iderw(j+3,i) = r13
+                     r23 = random_exponential_clamped()
+                     idwrw(j+3,i) = r23
+                     r33 = random_exponential_clamped()
+                     idnrw(j+3,i) = r33
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+4,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+4,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+4,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+4,i) = r30
+                     r01 = random_exponential_clamped()
+                     idsrw(j+5,i) = r01
+                     r11 = random_exponential_clamped()
+                     iderw(j+5,i) = r11
+                     r21 = random_exponential_clamped()
+                     idwrw(j+5,i) = r21
+                     r31 = random_exponential_clamped()
+                     idnrw(j+5,i) = r31
+                     r02 = random_exponential_clamped()
+                     idsrw(j+6,i) = r02
+                     r12 = random_exponential_clamped()
+                     iderw(j+6,i) = r12
+                     r22 = random_exponential_clamped()
+                     idwrw(j+6,i) = r22
+                     r32 = random_exponential_clamped()
+                     idnrw(j+6,i) = r32
+                     r03 = random_exponential_clamped()
+                     idsrw(j+7,i) = r03
+                     r13 = random_exponential_clamped()
+                     iderw(j+7,i) = r13
+                     r23 = random_exponential_clamped()
+                     idwrw(j+7,i) = r23
+                     r33 = random_exponential_clamped()
+                     idnrw(j+7,i) = r33
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+8,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+8,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+8,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+8,i) = r30
+                     r01 = random_exponential_clamped()
+                     idsrw(j+9,i) = r01
+                     r11 = random_exponential_clamped()
+                     iderw(j+9,i) = r11
+                     r21 = random_exponential_clamped()
+                     idwrw(j+9,i) = r21
+                     r31 = random_exponential_clamped()
+                     idnrw(j+9,i) = r31
+                     r02 = random_exponential_clamped()
+                     idsrw(j+10,i) = r02
+                     r12 = random_exponential_clamped()
+                     iderw(j+10,i) = r12
+                     r22 = random_exponential_clamped()
+                     idwrw(j+10,i) = r22
+                     r32 = random_exponential_clamped()
+                     idnrw(j+10,i) = r32
+                     r03 = random_exponential_clamped()
+                     idsrw(j+11,i) = r03
+                     r13 = random_exponential_clamped()
+                     iderw(j+11,i) = r13
+                     r23 = random_exponential_clamped()
+                     idwrw(j+11,i) = r23
+                     r33 = random_exponential_clamped()
+                     idnrw(j+11,i) = r33
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+12,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+12,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+12,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+12,i) = r30
+                     r01 = random_exponential_clamped()
+                     idsrw(j+13,i) = r01
+                     r11 = random_exponential_clamped()
+                     iderw(j+13,i) = r11
+                     r21 = random_exponential_clamped()
+                     idwrw(j+13,i) = r21
+                     r31 = random_exponential_clamped()
+                     idnrw(j+13,i) = r31
+                     r02 = random_exponential_clamped()
+                     idsrw(j+14,i) = r02
+                     r12 = random_exponential_clamped()
+                     iderw(j+14,i) = r12
+                     r22 = random_exponential_clamped()
+                     idwrw(j+14,i) = r22
+                     r32 = random_exponential_clamped()
+                     idnrw(j+14,i) = r32
+                     r03 = random_exponential_clamped()
+                     idsrw(j+15,i) = r03
+                     r13 = random_exponential_clamped()
+                     iderw(j+15,i) = r13
+                     r23 = random_exponential_clamped()
+                     idwrw(j+15,i) = r23
+                     r33 = random_exponential_clamped()
+                     idnrw(j+15,i) = r33
+                   end do
+               end do
+            else
+               m = mod(nidsrw,16)
+               if(m/=0) then
+                   !dir$ assume_aligned idsrw:32
+                   !dir$ vector aligned
+                   !dir$ ivdep
+                   !dir$ vector vectorlength(4)
+                   !dir$ vector always
+                   do i=1, nbpc
+                      do j=1,m
+                          r00 = random_exponential_clamped()
+                          idsrw(j+0,i) = r00
+                      end do
+                   end do
+                   if(nidsrw<16) return
+               end if
+               m1 = m+1
+               do i=1, nbpc
+               !dir$ assume_aligned idsrw:32
+               !dir$ vector aligned
+               !dir$ ivdep
+               !dir$ vector vectorlength(4)
+               !dir$ vector always
+                  do j=m1, nidsrw,16
+                     r00 = random_exponential_clamped()
+                     idsrw(j+0,i) = r00
+                     r10 = random_exponential_clamped()
+                     idsrw(j+1,i) = r10
+                     r20 = random_exponential_clamped()
+                     idsrw(j+2,i) = r20
+                     r30 = random_exponential_clamped()
+                     idsrw(j+3,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+4,i) = r00
+                     r10 = random_exponential_clamped()
+                     idsrw(j+5,i) = r10
+                     r20 = random_exponential_clamped()
+                     idsrw(j+6,i) = r20
+                     r30 = random_exponential_clamped()
+                     idsrw(j+7,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+8,i) = r00
+                     r10 = random_exponential_clamped()
+                     idsrw(j+9,i) = r10
+                     r20 = random_exponential_clamped()
+                     idsrw(j+10,i) = r20
+                     r30 = random_exponential_clamped()
+                     idsrw(j+11,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idsrw(j+12,i) = r00
+                     r10 = random_exponential_clamped()
+                     idsrw(j+13,i) = r10
+                     r20 = random_exponential_clamped()
+                     idsrw(j+14,i) = r20
+                     r30 = random_exponential_clamped()
+                     idsrw(j+15,i) = r30
+                  end do
+               end do
+               m = mod(niderw,16)
+               if(m/=0) then
+                   !dir$ assume_aligned iderw:32
+                   !dir$ vector aligned
+                   !dir$ ivdep
+                   !dir$ vector vectorlength(4)
+                   !dir$ vector always
+                   do i=1, nbpc
+                      do j=1,m
+                          r00 = random_exponential_clamped()
+                          iderw(j+0,i) = r00
+                      end do
+                   end do
+                   if(niderw<16) return
+               end if
+               m1 = m+1
+               do i=1, nbpc
+               !dir$ assume_aligned iderw:32
+               !dir$ vector aligned
+               !dir$ ivdep
+               !dir$ vector vectorlength(4)
+               !dir$ vector always
+                  do j=m1, niderw,16
+                     r00 = random_exponential_clamped()
+                     iderw(j+0,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+1,i) = r10
+                     r20 = random_exponential_clamped()
+                     iderw(j+2,i) = r20
+                     r30 = random_exponential_clamped()
+                     iderw(j+3,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     iderw(j+4,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+5,i) = r10
+                     r20 = random_exponential_clamped()
+                     iderw(j+6,i) = r20
+                     r30 = random_exponential_clamped()
+                     iderw(j+7,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     iderw(j+8,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+9,i) = r10
+                     r20 = random_exponential_clamped()
+                     iderw(j+10,i) = r20
+                     r30 = random_exponential_clamped()
+                     iderw(j+11,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     iderw(j+12,i) = r00
+                     r10 = random_exponential_clamped()
+                     iderw(j+13,i) = r10
+                     r20 = random_exponential_clamped()
+                     iderw(j+14,i) = r20
+                     r30 = random_exponential_clamped()
+                     iderw(j+15,i) = r30
+                  end do
+               end do
+               m = mod(nidwrw,16)
+               if(m/=0) then
+                   !dir$ assume_aligned idwrw:32
+                   !dir$ vector aligned
+                   !dir$ ivdep
+                   !dir$ vector vectorlength(4)
+                   !dir$ vector always
+                   do i=1, nbpc
+                      do j=1,m
+                          r00 = random_exponential_clamped()
+                          idwrw(j+0,i) = r00
+                      end do
+                   end do
+                   if(nidwrw<16) return
+               end if
+               m1 = m+1
+               do i=1, nbpc
+               !dir$ assume_aligned idwrw:32
+               !dir$ vector aligned
+               !dir$ ivdep
+               !dir$ vector vectorlength(4)
+               !dir$ vector always
+                  do j=m1, nidwrw,16
+                     r00 = random_exponential_clamped()
+                     idwrw(j+0,i) = r00
+                     r10 = random_exponential_clamped()
+                     idwrw(j+1,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+2,i) = r20
+                     r30 = random_exponential_clamped()
+                     idwrw(j+3,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idwrw(j+4,i) = r00
+                     r10 = random_exponential_clamped()
+                     idwrw(j+5,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+6,i) = r20
+                     r30 = random_exponential_clamped()
+                     idwrw(j+7,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idwrw(j+8,i) = r00
+                     r10 = random_exponential_clamped()
+                     idwrw(j+9,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+10,i) = r20
+                     r30 = random_exponential_clamped()
+                     idwrw(j+11,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idwrw(j+12,i) = r00
+                     r10 = random_exponential_clamped()
+                     idwrw(j+13,i) = r10
+                     r20 = random_exponential_clamped()
+                     idwrw(j+14,i) = r20
+                     r30 = random_exponential_clamped()
+                     idwrw(j+15,i) = r30
+                  end do
+               end do
+               m = mod(nidnrw,16)
+               if(m/=0) then
+                   !dir$ assume_aligned idnrw:32
+                   !dir$ vector aligned
+                   !dir$ ivdep
+                   !dir$ vector vectorlength(4)
+                   !dir$ vector always
+                   do i=1, nbpc
+                      do j=1,m
+                          r00 = random_exponential_clamped()
+                          idnrw(j+0,i) = r00
+                      end do
+                   end do
+                   if(nidnrw<16) return
+               end if
+               m1 = m+1
+               do i=1, nbpc
+               !dir$ assume_aligned idnrw:32
+               !dir$ vector aligned
+               !dir$ ivdep
+               !dir$ vector vectorlength(4)
+               !dir$ vector always
+                  do j=m1, nidnrw,16
+                     r00 = random_exponential_clamped()
+                     idnrw(j+0,i) = r00
+                     r10 = random_exponential_clamped()
+                     idnrw(j+1,i) = r10
+                     r20 = random_exponential_clamped()
+                     idnrw(j+2,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+3,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idnrw(j+4,i) = r00
+                     r10 = random_exponential_clamped()
+                     idnrw(j+5,i) = r10
+                     r20 = random_exponential_clamped()
+                     idnrw(j+6,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+7,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idnrw(j+8,i) = r00
+                     r10 = random_exponential_clamped()
+                     idnrw(j+9,i) = r10
+                     r20 = random_exponential_clamped()
+                     idnrw(j+10,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+11,i) = r30
+                     ! 
+                     r00 = random_exponential_clamped()
+                     idnrw(j+12,i) = r00
+                     r10 = random_exponential_clamped()
+                     idnrw(j+13,i) = r10
+                     r20 = random_exponential_clamped()
+                     idnrw(j+14,i) = r20
+                     r30 = random_exponential_clamped()
+                     idnrw(j+15,i) = r30
+                  end do
+               end do
+            end if 
+       end subroutine rand_exp_init_idxrw_unroll16x
 
 
 
