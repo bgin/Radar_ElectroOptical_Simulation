9d04f894ee94e34768a1cf8a816568eec3a6c7b9
Bernard Gingold <beniekg@gmail.com>
2024-03-30 10:29:27 +0100

    Added: minor modification.
    WIP.

--- Guided Missile/Aerodynamics/helpers.f90
+++ Guided Missile/Aerodynamics/helpers.f90
@@ -1,5 +1,27 @@
-module helpers_mod
 
+!Copyright (c) 2021 USU Aero Lab
+
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+
+module helpers_mod
+     ! ************Slightly modified by Bernard Gingold, 30/03/2024, 10:20AM**************
+    use mod_kinds, only : i4,sp
     implicit none
 
     logical :: run_checks, verbose
@@ -7,7 +29,7 @@ module helpers_mod
     type progress_bar
         ! NOT WORKING
 
-        integer :: N_total, N_complete
+        integer(kind=i4):: N_total, N_complete
 
         contains
 
@@ -22,7 +44,7 @@ contains
 
         implicit none
 
-        integer,intent(in) :: stat
+        integer(kind=i4),intent(in) :: stat
         character(len=*),intent(in),optional :: what
 
         ! Check if stat is nonzero
@@ -44,9 +66,9 @@ contains
 
         implicit none
 
-        real,dimension(3),intent(in) :: vec
-        integer,intent(in) :: plane
-        real,dimension(3) :: mirrored_vec
+        real(kind=sp),dimension(3),intent(in) :: vec
+        integer(kind=i4),intent(in) :: plane
+        real(kind=sp),dimension(3) :: mirrored_vec
 
         mirrored_vec = vec
         mirrored_vec(plane) = -vec(plane)
@@ -59,11 +81,11 @@ contains
 
         implicit none
         
-        integer,intent(in) :: N
-        integer,dimension(N),intent(in) :: P
-        integer,dimension(:),allocatable,intent(out) :: P_inv
+        integer(kind=i4),intent(in) :: N
+        integer(kind=i4),dimension(N),intent(in) :: P
+        integer(kind=i4),dimension(:),allocatable,intent(out) :: P_inv
 
-        integer :: i
+        integer(kind=i4):: i
 
         ! Invert
         allocate(P_inv(N))
@@ -80,7 +102,7 @@ contains
         implicit none
 
         class(progress_bar),intent(inout) :: this
-        integer,intent(in) :: N_total
+        integer(kind=i4),intent(in) :: N_total
 
         ! Initialize
         this%N_total = N_total
@@ -103,7 +125,7 @@ contains
         this%N_complete = this%N_complete + 1
 
         ! Print progress
-        write(*,'(tl13, i3, a)',advance='no') 100.0*real(this%N_complete)/real(this%N_total), "% complete."
+        write(*,'(tl13, i3, a)',advance='no') 100.0*real(kind=sp)(this%N_complete)/real(kind=sp)(this%N_total), "% complete."
 
         ! Write newline
         if (this%N_complete == this%N_total) then
@@ -120,7 +142,7 @@ contains
 
         character(len=:),allocatable,intent(in) :: filename
 
-        integer :: stat
+        integer(kind=i4):: stat
 
         open(unit=13, iostat=stat, file=filename)
         if (stat == 0) close(13, status='delete')
