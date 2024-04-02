b064d5549e6142a25b028fb83cc498cff26cdfe4
Bernard Gingold <beniekg@gmail.com>
2024-04-02 19:09:53 +0200

    Added: a minor modification.

--- Guided Missile/Aerodynamics/wake_strip.f90
+++ Guided Missile/Aerodynamics/wake_strip.f90
@@ -1,5 +1,31 @@
-module wake_strip_mod
 
+! Types for the most basic geometric objects
+
+!MIT License
+
+!Copyright (c) 2021 USU Aero Lab
+
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+
+module wake_strip_mod
+      ! ************Slightly modified by Bernard Gingold, 02/04/2024, 06:52PM**************
+    use mod_kinds, only : i4,sp
     use panel_mod
     use base_geom_mod
     use helpers_mod
@@ -9,8 +35,8 @@ module wake_strip_mod
 
     type, extends(mesh) :: wake_strip
 
-        integer :: i_top_parent_1, i_top_parent_2, i_bot_parent_1, i_bot_parent_2
-        integer :: i_top_parent, i_bot_parent
+        integer(kind=i4) :: i_top_parent_1, i_top_parent_2, i_bot_parent_1, i_bot_parent_2
+        integer(kind=i4) :: i_top_parent, i_bot_parent
         logical :: on_mirror_plane
 
         contains
@@ -35,68 +61,68 @@ contains
         type(flow),intent(in) :: freestream
         type(edge),intent(in) :: starting_edge
         logical,intent(in) :: mirror_start
-        integer,intent(in) :: mirror_plane, N_panels_streamwise, initial_panel_order, N_body_panels
-        real,intent(in) :: trefftz_dist
+        integer(kind=i4),intent(in) :: mirror_plane, N_panels_streamwise, initial_panel_order, N_body_panels
+        real(kind=sp) :: ,intent(in) :: trefftz_dist
         type(vertex),dimension(:),allocatable,intent(in) :: body_verts
         logical,intent(in) :: wake_mirrored
 
-        real,dimension(3) :: start_1, start_2
-        integer :: N_body_verts, i
+        real(kind=sp) :: ,dimension(3) :: start_1, start_2
+        integer(kind=i4) :: N_body_verts, i
 
         ! Get number of vertices on the body
         N_body_verts = size(body_verts)
 
         ! Set mirroring
-        this%on_mirror_plane = starting_edge%on_mirror_plane
-        this%mirror_plane = mirror_plane
-        this%mirrored = wake_mirrored .and. .not. this%on_mirror_plane
+        this.on_mirror_plane = starting_edge.on_mirror_plane
+        this.mirror_plane = mirror_plane
+        this.mirrored = wake_mirrored .and. .not. this.on_mirror_plane
 
         ! Get starting locations and parent vertices
         if (mirror_start) then
 
             ! Get starting location
             ! Note order is flipped here to maintain convention
-            start_1 = mirror_across_plane(body_verts(starting_edge%top_verts(2))%loc, mirror_plane)
-            start_2 = mirror_across_plane(body_verts(starting_edge%top_verts(1))%loc, mirror_plane)
+            start_1 = mirror_across_plane(body_verts(starting_edge.top_verts(2)).loc, mirror_plane)
+            start_2 = mirror_across_plane(body_verts(starting_edge.top_verts(1)).loc, mirror_plane)
 
             ! Get parent vertices
-            this%i_top_parent_1 = starting_edge%top_verts(2) + N_body_verts
-            this%i_top_parent_2 = starting_edge%top_verts(1) + N_body_verts
-            this%i_bot_parent_1 = starting_edge%bot_verts(2) + N_body_verts
-            this%i_bot_parent_2 = starting_edge%bot_verts(1) + N_body_verts
+            this.i_top_parent_1 = starting_edge.top_verts(2) + N_body_verts
+            this.i_top_parent_2 = starting_edge.top_verts(1) + N_body_verts
+            this.i_bot_parent_1 = starting_edge.bot_verts(2) + N_body_verts
+            this.i_bot_parent_2 = starting_edge.bot_verts(1) + N_body_verts
 
             ! Get parent panels
-            this%i_top_parent = starting_edge%panels(1) + N_body_panels
-            this%i_bot_parent = starting_edge%panels(2) + N_body_panels
+            this.i_top_parent = starting_edge.panels(1) + N_body_panels
+            this.i_bot_parent = starting_edge.panels(2) + N_body_panels
 
         else
 
             ! Get starting location
-            start_1 = body_verts(starting_edge%top_verts(1))%loc
-            start_2 = body_verts(starting_edge%top_verts(2))%loc
+            start_1 = body_verts(starting_edge.top_verts(1)).loc
+            start_2 = body_verts(starting_edge.top_verts(2)).loc
 
             ! Get parent vertices
-            this%i_top_parent_1 = starting_edge%top_verts(1)
-            this%i_top_parent_2 = starting_edge%top_verts(2)
-            this%i_bot_parent_1 = starting_edge%bot_verts(1)
-            this%i_bot_parent_2 = starting_edge%bot_verts(2)
+            this.i_top_parent_1 = starting_edge.top_verts(1)
+            this.i_top_parent_2 = starting_edge.top_verts(2)
+            this.i_bot_parent_1 = starting_edge.bot_verts(1)
+            this.i_bot_parent_2 = starting_edge.bot_verts(2)
 
             ! Get parent panels
-            this%i_top_parent = starting_edge%panels(1)
-            this%i_bot_parent = starting_edge%panels(2)
+            this.i_top_parent = starting_edge.panels(1)
+            this.i_bot_parent = starting_edge.panels(2)
 
         end if
 
         ! Initialize vertices
-        call this%init_vertices(freestream, N_panels_streamwise, trefftz_dist, start_1, start_2, body_verts)
+        call this.init_vertices(freestream, N_panels_streamwise, trefftz_dist, start_1, start_2, body_verts)
 
         ! Intialize panels
-        call this%init_panels(N_panels_streamwise)
+        call this.init_panels(N_panels_streamwise)
 
         ! Initialize other panel properties
-        do i=1,this%N_panels
-            call this%panels(i)%init_with_flow(freestream, this%mirrored, mirror_plane)
-            call this%panels(i)%set_distribution(1, this%panels, this%vertices, this%mirrored, mirror_plane) ! With the current formulation, wake panels are always linear
+        do i=1,this.N_panels
+            call this.panels(i).init_with_flow(freestream, this.mirrored, mirror_plane)
+            call this.panels(i).set_distribution(1, this.panels, this.vertices, this.mirrored, mirror_plane) ! With the current formulation, wake panels are always linear
         end do
 
     end subroutine wake_strip_init
@@ -110,58 +136,58 @@ contains
 
         class(wake_strip),intent(inout) :: this
         type(flow),intent(in) :: freestream
-        integer,intent(in) :: N_panels_streamwise
-        real,intent(in) :: trefftz_dist
-        real,dimension(3),intent(in) :: start_1, start_2
+        integer(kind=i4),intent(in) :: N_panels_streamwise
+        real(kind=sp) :: ,intent(in) :: trefftz_dist
+        real(kind=sp) :: ,dimension(3),intent(in) :: start_1, start_2
         type(vertex),dimension(:),allocatable,intent(in) :: body_verts
 
-        real,dimension(3) :: loc
-        real :: d1, d2, sep_1, sep_2
-        integer :: i, N_body_verts
+        real(kind=sp) :: ,dimension(3) :: loc
+        real(kind=sp) ::  :: d1, d2, sep_1, sep_2
+        integer(kind=i4) :: i, N_body_verts
 
         ! Allocate memory
-        this%N_verts = N_panels_streamwise*2 + 2
+        this.N_verts = N_panels_streamwise*2 + 2
         N_body_verts = size(body_verts)
-        allocate(this%vertices(this%N_verts))
+        allocate(this.vertices(this.N_verts))
 
         ! Initialize starting vertices in wake strip
-        call this%vertices(1)%init(start_1, 1)
-        call this%vertices(2)%init(start_2, 2)
+        call this.vertices(1).init(start_1, 1)
+        call this.vertices(2).init(start_2, 2)
 
         ! Set parents
-        this%vertices(1)%top_parent = this%i_top_parent_1
-        this%vertices(1)%bot_parent = this%i_bot_parent_1
-        this%vertices(2)%top_parent = this%i_top_parent_2
-        this%vertices(2)%bot_parent = this%i_bot_parent_2
+        this.vertices(1).top_parent = this.i_top_parent_1
+        this.vertices(1).bot_parent = this.i_bot_parent_1
+        this.vertices(2).top_parent = this.i_top_parent_2
+        this.vertices(2).bot_parent = this.i_bot_parent_2
 
         ! Calculate distances to Trefftz plane
-        d1 = trefftz_dist - inner(start_1, freestream%c_hat_g)
-        d2 = trefftz_dist - inner(start_2, freestream%c_hat_g)
+        d1 = trefftz_dist - inner(start_1, freestream.c_hat_g)
+        d2 = trefftz_dist - inner(start_2, freestream.c_hat_g)
 
         ! Calculate spacing between vertices
         sep_1 = d1 / N_panels_streamwise
         sep_2 = d2 / N_panels_streamwise
 
         ! Loop through following vertices
-        do i=3,this%N_verts
+        do i=3,this.N_verts
 
             ! Calculate location of vertices
             if (modulo(i, 2) == 0) then
-                loc = start_2 + sep_2*(i-2)/2*freestream%c_hat_g
+                loc = start_2 + sep_2*(i-2)/2*freestream.c_hat_g
             else
-                loc = start_1 + sep_1*(i-1)/2*freestream%c_hat_g
+                loc = start_1 + sep_1*(i-1)/2*freestream.c_hat_g
             end if
 
             ! Initialize vertices
-            call this%vertices(i)%init(loc, i)
+            call this.vertices(i).init(loc, i)
 
             ! Set parents
             if (modulo(i, 2) == 0) then
-                this%vertices(i)%top_parent = this%i_top_parent_2
-                this%vertices(i)%bot_parent = this%i_bot_parent_2
+                this.vertices(i).top_parent = this.i_top_parent_2
+                this.vertices(i).bot_parent = this.i_bot_parent_2
             else
-                this%vertices(i)%top_parent = this%i_top_parent_1
-                this%vertices(i)%bot_parent = this%i_bot_parent_1
+                this.vertices(i).top_parent = this.i_top_parent_1
+                this.vertices(i).bot_parent = this.i_bot_parent_1
             end if
 
         end do
@@ -175,36 +201,36 @@ contains
         implicit none
 
         class(wake_strip),intent(inout) :: this
-        integer,intent(in) :: N_panels_streamwise
+        integer(kind=i4),intent(in) :: N_panels_streamwise
 
-        real :: d1, d2
-        integer :: i, j, i1, i2, advance, N_skipped
+        real(kind=sp) ::  :: d1, d2
+        integer(kind=i4) :: i, j, i1, i2, advance, N_skipped
         logical,dimension(:),allocatable :: skipped_panels
         type(panel),dimension(:),allocatable :: temp_panels
 
         ! Determine number of panels
-        this%N_panels = N_panels_streamwise*2
-        allocate(this%panels(this%N_panels))
-        allocate(skipped_panels(this%N_panels), source=.false.)
+        this.N_panels = N_panels_streamwise*2
+        allocate(this.panels(this.N_panels))
+        allocate(skipped_panels(this.N_panels), source=.false.)
 
         ! Create panels
         i1 = 1
         i2 = 2
-        do i=1,this%N_panels
+        do i=1,this.N_panels
 
             ! Initialize
             advance = 0
 
             ! See if one is at the end
-            if (i1 == this%N_verts - 1) then
+            if (i1 == this.N_verts - 1) then
                 advance = 2
-            else if (i2 == this%N_verts) then
+            else if (i2 == this.N_verts) then
                 advance = 1
             else
 
                 ! Check lengths of hypotenuses
-                d1 = dist(this%vertices(i1+2)%loc, this%vertices(i2)%loc)
-                d2 = dist(this%vertices(i1)%loc, this%vertices(i2+2)%loc)
+                d1 = dist(this.vertices(i1+2).loc, this.vertices(i2).loc)
+                d2 = dist(this.vertices(i1).loc, this.vertices(i2+2).loc)
 
                 ! Pick which one
                 if (d1 < d2) then
@@ -218,14 +244,14 @@ contains
             if (advance == 1) then
 
                 ! Initialize
-                call this%init_panel(i, i1, i1+2, i2, skipped_panels)
+                call this.init_panel(i, i1, i1+2, i2, skipped_panels)
                 
                 ! Increment index
                 i1 = i1 + 2
             else
                 
                 ! Initialize
-                call this%init_panel(i, i1, i2+2, i2, skipped_panels)
+                call this.init_panel(i, i1, i2+2, i2, skipped_panels)
 
                 ! Increment index
                 i2 = i2 + 2
@@ -235,11 +261,11 @@ contains
 
         ! Get rid of skipped panels
         N_skipped = count(skipped_panels)
-        allocate(temp_panels(this%N_panels - N_skipped))
+        allocate(temp_panels(this.N_panels - N_skipped))
 
         ! Move non-skipped panels over
         j = 0
-        do i=1,this%N_panels
+        do i=1,this.N_panels
 
             ! Check that this panel was not skipped
             if (.not. skipped_panels(i)) then
@@ -248,17 +274,17 @@ contains
                 j = j + 1
 
                 ! Copy over
-                temp_panels(j) = this%panels(i)
+                temp_panels(j) = this.panels(i)
 
             end if
 
         end do
 
         ! Move allocation
-        call move_alloc(temp_panels, this%panels)
+        call move_alloc(temp_panels, this.panels)
 
         ! Update number of panels
-        this%N_panels = this%N_panels - N_skipped
+        this.N_panels = this.N_panels - N_skipped
 
     end subroutine wake_strip_init_panels
 
@@ -269,14 +295,14 @@ contains
         implicit none
         
         class(wake_strip),intent(inout) :: this
-        integer,intent(in) :: i_panel, i1, i2, i3
+        integer(kind=i4),intent(in) :: i_panel, i1, i2, i3
         logical,dimension(:),allocatable,intent(inout) :: skipped_panels
 
         ! Check for zero area
-        if (this%has_zero_area(i1, i2, i3)) then
+        if (this.has_zero_area(i1, i2, i3)) then
             skipped_panels(i_panel) = .true.
         else
-            call this%panels(i_panel)%init(this%vertices(i1), this%vertices(i2), this%vertices(i3), i_panel, in_wake=.true.)
+            call this.panels(i_panel).init(this.vertices(i1), this.vertices(i2), this.vertices(i3), i_panel, in_wake=.true.)
         end if
 
     end subroutine wake_strip_init_panel
