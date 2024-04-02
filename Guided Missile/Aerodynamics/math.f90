ec226ac4fc7487e943778d2dd46889a3b8ba786e
Bernard Gingold <beniekg@gmail.com>
2024-04-02 19:09:08 +0200

    Add files via upload

--- Guided Missile/Aerodynamics/math.f90
+++ Guided Missile/Aerodynamics/math.f90
@@ -1,10 +1,36 @@
-! Generic math subroutines
-module math_mod
 
+!MIT License
+
+!Copyright (c) 2021 USU Aero Lab
+
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+
+! Generic math subroutines
+
+
+module math_mod
+    ! ************Slightly modified by Bernard Gingold, 30/03/2024, 10:13AM**************
+    use mod_kinds, only : i4,sp
     implicit none
-    real,parameter :: pi = 3.14159265358979323846264338327950288419716939937510 ! No, this is not at all excessive
-    real,parameter :: pi2 = pi*0.5
-    real,parameter :: inf = huge(0.) ! Reason #403929 why Fortran is the best: huge() is an intrinsic function
+    real(kind=sp),parameter :: pi = 3.14159265358979323846264338327950288419716939937510_sp ! No, this is not at all excessive
+    real(kind=sp),parameter :: pi2 = pi*0.5_sp
+    real(kind=sp),parameter :: inf = huge(0.) ! Reason #403929 why Fortran is the best: huge() is an intrinsic function
     
 contains
 
@@ -14,7 +40,7 @@ function isinf(x) result(is)
 
   implicit none
 
-  real,intent(in) :: x
+  real(kind=sp),intent(in) :: x
 
   logical :: is
 
@@ -33,8 +59,8 @@ function plane_normal(p1, p2, p3) result(n)
 
   implicit none
 
-  real,dimension(3),intent(in) :: p1, p2, p3
-  real,dimension(3) :: a, b, n
+  real(kind=sp),dimension(3),intent(in) :: p1, p2, p3
+  real(kind=sp),dimension(3) :: a, b, n
 
   a = p2 - p1
   b = p3 - p1
@@ -49,11 +75,11 @@ function reflect_point(A, B, C, D, P) result(P_refl)
 
   implicit none
 
-  real,intent(in) :: A, B, C, D
-  real,dimension(3),intent(in) :: P(3)
-  real,dimension(3) :: P_refl
+  real(kind=sp),intent(in) :: A, B, C, D
+  real(kind=sp),dimension(3),intent(in) :: P(3)
+  real(kind=sp),dimension(3) :: P_refl
 
-  real :: mult
+  real(kind=sp) :: mult
 
   mult = 2.0*(A*P(1) + B*P(2) + C*P(3) + D)/(A**2 + B**2 + C**2)
 
@@ -69,8 +95,8 @@ function dist(a, b) result(c)
 
     implicit none
 
-    real,dimension(3),intent(in) :: a, b
-    real :: c
+    real(kind=sp),dimension(3),intent(in) :: a, b
+    real(kind=sp) :: c
 
     c = norm2(a-b)
 
@@ -82,9 +108,9 @@ function cross(a, b) result(c)
 
     implicit none
 
-    real,dimension(3),intent(in) :: a, b
+    real(kind=sp),dimension(3),intent(in) :: a, b
 
-    real,dimension(3) :: c
+    real(kind=sp),dimension(3) :: c
 
     c(1) = a(2)*b(3) - a(3)*b(2)
     c(2) = a(3)*b(1) - a(1)*b(3)
@@ -97,8 +123,8 @@ function inner(a, b) result(c)
   ! Calculates the 3D Euclidean inner product
 
   implicit none
-  real,dimension(3),intent(in) :: a, b
-  real :: c
+  real(kind=sp),dimension(3),intent(in) :: a, b
+  real(kind=sp) :: c
 
   c = a(1)*b(1)+a(2)*b(2)+a(3)*b(3)
 
@@ -109,8 +135,8 @@ function inner2(a, b) result(c)
   ! Calculates the 2D Euclidean inner product
 
   implicit none
-  real,dimension(2),intent(in) :: a, b
-  real :: c
+  real(kind=sp),dimension(2),intent(in) :: a, b
+  real(kind=sp) :: c
 
   c = a(1)*b(1)+a(2)*b(2)
 
@@ -122,10 +148,10 @@ function outer(a, b) result(c)
 
   implicit none
 
-  real,dimension(3),intent(in) :: a, b
-  real,dimension(3,3) :: c
+  real(kind=sp),dimension(3),intent(in) :: a, b
+  real(kind=sp),dimension(3,3) :: c
 
-  integer :: i
+  integer(kind=i4):: i
 
   do i=1,3
     c(:,i) = a(:)*b(i)
@@ -139,8 +165,8 @@ function det3(a) result(c)
 
   implicit none
 
-  real,dimension(3,3) :: a
-  real :: c
+  real(kind=sp),dimension(3,3) :: a
+  real(kind=sp) :: c
 
   c = a(1,1)*(a(2,2)*a(3,3)-a(2,3)*a(3,2))
   c = c - a(1,2)*(a(2,1)*a(3,3)-a(2,3)*a(3,1))
@@ -154,11 +180,11 @@ function rot_x(v, theta) result(v_rot)
 
     implicit none
 
-    real,dimension(3),intent(in) :: v
-    real,intent(in) :: theta
-    real,dimension(3) :: v_rot
+    real(kind=sp),dimension(3),intent(in) :: v
+    real(kind=sp),intent(in) :: theta
+    real(kind=sp),dimension(3) :: v_rot
 
-    real,dimension(3,3) :: rm(3,3) = 0.
+    real(kind=sp),dimension(3,3) :: rm(3,3) = 0.
 
     rm(1,1) = 1.
     rm(2,2) = cos(theta)
@@ -176,11 +202,11 @@ function rot_y(v, theta) result(v_rot)
 
     implicit none
 
-    real,dimension(3),intent(in) :: v
-    real,intent(in) :: theta
-    real,dimension(3) :: v_rot
+    real(kind=sp),dimension(3),intent(in) :: v
+    real(kind=sp),intent(in) :: theta
+    real(kind=sp),dimension(3) :: v_rot
 
-    real,dimension(3,3) :: rm(3,3) = 0.
+    real(kind=sp),dimension(3,3) :: rm(3,3) = 0.
 
     rm(1,1) = cos(theta)
     rm(1,3) = sin(theta)
@@ -198,11 +224,11 @@ function rot_z(v, theta) result(v_rot)
 
     implicit none
 
-    real,dimension(3),intent(in) :: v
-    real,intent(in) :: theta
-    real,dimension(3) :: v_rot
+    real(kind=sp),dimension(3),intent(in) :: v
+    real(kind=sp),intent(in) :: theta
+    real(kind=sp),dimension(3) :: v_rot
 
-    real,dimension(3,3) :: rm(3,3) = 0.
+    real(kind=sp),dimension(3,3) :: rm(3,3) = 0.
 
     rm(1,1) = cos(theta)
     rm(1,2) = -sin(theta)
@@ -220,12 +246,12 @@ function parallel_matmul(A, B) result(C)
 
   implicit none
   
-  real,dimension(:,:),allocatable,intent(in) :: A, B
+  real(kind=sp),dimension(:,:),allocatable,intent(in) :: A, B
   
-  real,dimension(:,:),allocatable :: C
+  real(kind=sp),dimension(:,:),allocatable :: C
 
-  integer,dimension(:),allocatable :: A_shape, B_shape
-  integer :: i, j
+  integer(kind=i4),dimension(:),allocatable :: A_shape, B_shape
+  integer(kind=i4):: i, j
 
   ! Check shape
   A_shape = shape(A)
