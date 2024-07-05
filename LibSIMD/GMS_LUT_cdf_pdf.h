1d3d22e3b2bd036e3e4d1608fc16ab4ef15de7e9
Bernard Gingold <beniekg@gmail.com>
2024-07-05 20:52:41 +0200

    [Added]: LUTs for CDF,PDF kernels.
    WIP.

---
+++ LibSIMD/GMS_LUT_cdf_pdf.h
@@ -0,0 +1,97 @@
+
+
+#ifndef __GMS_LUT_CDF_PDF_H__
+#define __GMS_LUT_CDF_PDF_H__
+
+
+/*MIT License
+Copyright (c) 2020 Bernard Gingold
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+ /* Used by the function: gamma_log_zmm8r8*/               
+__attribute__((section(".rodata")))
+__attribute__((aligned(64))) const static double gamma_log_zmm8r8_c[58] = { 
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+       -1.910444077728E-03,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+        8.4171387781295E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+       -5.952379913043012E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+        7.93650793500350248E-04,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+       -2.777777777777681622553E-03,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        8.333333333333333331554247E-02,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03,
+        5.7083835261E-03};
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__GMS_LUT_CDF_PDF_H__*/
