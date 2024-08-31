e1f1d4a2689e61145c78d79a679bb11b038e7377
Bernard Gingold <beniekg@gmail.com>
2024-08-31 16:19:44 +0200

    Minor error fixes.

--- Mathematics/GMS_sse_cvec4.f90
+++ Mathematics/GMS_sse_cvec4.f90
@@ -1,5 +1,26 @@
 
 
+
+
+!/*MIT License
+!Copyright (c) 2020 Bernard Gingold
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+!*/
+
 module sse_cvec4
 
  !===================================================================================85
@@ -1375,6 +1396,17 @@ module sse_cvec4
        iq.re = log(t0.v)
        iq.im = carg_xmm4c4(c)
       end function clog_xmm4c4 
+      
+     pure function cnorm_xmm4c4(x) result(cn) 
+#elif defined __ICC || defined __INTEL_COMPILER
+           !DIR$ ATTRIBUTES INLINE :: cnorm_xmm4c4
+           !DIR$ ATTRIBUTES VECTOR:PROCESSOR(skylake_avx512) :: cnorm_xmm4c4
+           !DIR$ ATTRIBUTES CODE_ALIGN : 32 :: cnorm_xmm4c4
+#endif
+            type(XMM4c4_t),  intent(in) :: x
+            type(XMM4r4_t) :: cn
+            cn.v = sqrt(x.re*x.re+x.im*x.im)
+       end function cnorm_xmm4c4
     
 !DIR$ ATTRIBUTES INLINE :: clog_2xxmm4r4
       pure function clog_2xmm4r4(re,im) result(iq)
