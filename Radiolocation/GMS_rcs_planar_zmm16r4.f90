81208e937e2100137fb5edcc466f77e8e694105a
Bernard Gingold <beniekg@gmail.com>
2024-09-01 09:32:33 +0200

    RCS planar surface (AVX512 sp) version.
    WIP.

---
+++ Radiolocation/GMS_rcs_planar_zmm16r4.f90
@@ -0,0 +1,175 @@
+
+#include "GMS_config.fpp"
+
+!/*MIT License
+!Copyright (c) 2020 Bernard Gingold
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+!*/
+
+module rcs_planar_zmm16r4
+
+
+
+!===================================================================================85
+ !---------------------------- DESCRIPTION ------------------------------------------85
+ !
+ !
+ !
+ !          Module  name:
+ !                         rcs_planar_zmm16r4
+ !          
+ !          Purpose:
+ !                        Various characteristics of analytically derived Radar
+ !                        Cross Section of planar objects  
+ !                        Based  on George T. Ruck, Donald E. Barrick , William D. Stuart , 
+ !                        - "Radar Cross Section Handbook 1 and 2" (1970, Kluwer Academic Plenum Publishers) 
+ !                        This module contains only explicitly vectorized (SIMD)
+ !                        
+ !          History:
+ !                        Date: 01-09-2024
+ !                        Time: 07:07 GMT+2
+ !                        
+ !          Version:
+ !
+ !                      Major: 1
+ !                      Minor: 0
+ !                      Micro: 0
+ !
+ !          Author:  
+ !                      Bernard Gingold
+ !          
+ !                 
+ !          References:
+ !         
+ !                      George T. Ruck, Donald E. Barrick , William D. Stuart
+ !                      Radar Cross Section Handbook 1 and 2" (1970, Kluwer Academic Plenum Publishers)     
+ !         
+ !          E-mail:
+ !                  
+ !                      beniekg@gmail.com
+!==================================================================================85
+    ! Tab:5 col - Type and etc.. definitions
+    ! Tab:10,11 col - Type , function and subroutine code blocks.
+
+    use mod_kinds,    only : i4,sp
+    use mod_vectypes, only : ZMM16r4_t
+    use avx512_cvec16_v2
+
+    public
+    implicit none
+    
+     ! Major version
+     integer(kind=i4),  parameter :: RCS_PLANAR_ZMM16R4_MAJOR = 1
+     ! Minor version
+     integer(kind=i4),  parameter :: RCS_PLANAR_ZMM16R4_MINOR = 0
+     ! Micro version
+     integer(kind=i4),  parameter :: RCS_PLANAR_ZMM16R4_MICRO = 0
+     ! Full version
+     integer(kind=i4),  parameter :: RCS_PLANAR_ZMM16R4_FULLVER =   &
+            1000*RCS_PLANAR_ZMM16R4_MAJOR+100*RCS_PLANAR_ZMM16R4_MINOR+10*RCS_PLANAR_ZMM16R4_MICRO
+     ! Module creation date
+     character(*),        parameter :: RCS_PLANAR_ZMM16R4_CREATE_DATE = "01-09-2024 07:10 +00200 (SUN 01 SEP 2024 GMT+2)"
+     ! Module build date
+     character(*),        parameter :: RCS_PLANAR_ZMM16R4_BUILD_DATE  = __DATE__ " " __TIME__
+     ! Module author info
+     character(*),        parameter :: RCS_PLANAR_ZMM16R4_AUTHOR      = "Programmer: Bernard Gingold, contact: beniekg@gmail.com"
+     ! Short description
+     character(*),        parameter :: RCS_PLANAR_ZMM16R4_SYNOPSIS    = "Analytical Cylindrical objects RCS characteristics and models explicitly vectorized (SIMD)."
+    
+#ifndef __RCS_PLANAR_PF_CACHE_HINT__
+#define __RCS_PLANAR_PF_CACHE_HINT__ 1
+#endif 
+    
+
+        !/*
+        !               Complex impedances.
+        !               Formula 7.1-6
+        !           */
+
+
+      pure function zi_f716_zmm16r4(tht,mu,eps) result(z)
+            !dir$ optimize:3
+            !dir$ attributes code_align : 32 :: zi_f716_zmm16r4
+            !dir$ attributes forceinline :: zi_f716_zmm16r4
+            !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" :: zi_f716_zmm16r4
+            use mod_vecconsts, only : v16_0,v16_1
+            type(ZMM16r4_t),   intent(in) :: tht
+            type(ZMM16c4),     intent(in) :: mu
+            type(ZMM16c4),     intent(in) :: eps
+            type(ZMM16c4) :: z
+            ! Locals
+            type(ZMM16r4_t), automatic :: cost
+            type(ZMM16r4_t), automatic :: invc
+            type(ZMM16r4_t), automatic :: wrkc
+            type(ZMM16c4),   automatic :: div
+            type(ZMM16c4),   automatic :: csq
+            !dir$ attributes align : 64 :: cost
+            !dir$ attributes align : 64 :: invc
+            !dir$ attributes align : 64 :: wrkc
+            !dir$ attributes align : 64 :: div
+            !dir$ attributes align : 64 :: csq
+            cost.v = cos(tht.v)
+            wrkc.v = v16_0.v
+            div    = mu/eps       
+            invc.v = v16_1.v/cost.v
+            csq    = csqrt_c16(div)
+            z.re   = invc.v*csq.re
+            z.im   = invc.v*csq.im
+      end function zi_f716_zmm16r4
+      
+      
+       !/*
+       !                   Equivalent complex impedances.
+       !                   Formula 7.1-4
+       !             */
+      
+      pure function R_f714_zmm16r4(tht1,mu1,eps1,tht2,mu2,eps2) result(R)
+            !dir$ optimize:3
+            !dir$ attributes code_align : 32 :: R_f714_zmm16r4
+            !dir$ attributes forceinline :: R_f714_zmm16r4
+            !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" :: R_f714_zmm16r4
+            type(ZMM16r4_t),    intent(in) :: tht1
+            type(ZMM16c4),      intent(in) :: mu1
+            type(ZMM16c4),      intent(in) :: eps1
+            type(ZMM16r4_t),    intent(in) :: tht2
+            type(ZMM16c4),      intent(in) :: mu2
+            type(ZMM16c4),      intent(in) :: eps2
+            type(ZMM16c4)  :: R
+            ! Locals
+            type(ZMM16c4), automatic :: z1
+            type(ZMM16c4), automatic :: z2
+            type(ZMM16c4), automatic :: t0
+            type(ZMM16c4), automatic :: t1 
+            !dir$ attributes align : 64 :: z1
+            !dir$ attributes align : 64 :: z2
+            !dir$ attributes align : 64 :: t0
+            !dir$ attributes align : 64 :: t1
+            z1    = zi_f716_zmm16r4(tht1,mu1,eps1)
+            z2    = zi_f716_zmm16r4(tht2,mu2,eps2)
+            t0    = z1-z2
+            t1    = z1+z2
+            t0.re = -t0.re
+            t0.im = -t0.im
+            R     = t0/t1 
+      end function R_f714_zmm16r4
+
+
+
+
+
+
+end module rcs_planar_zmm16r4
