38b532397c57475f82039674f96aec010e6cdb56
Bernard Gingold <beniekg@gmail.com>
2025-04-15 11:07:04 +0200

    Added `analytic_sol_L2_refractive_error_f911_r4`

--- Radiolocation/GMS_atmos_refraction.f90
+++ Radiolocation/GMS_atmos_refraction.f90
@@ -9664,4 +9664,40 @@ module atmos_refraction
              delLf  = rat1*rat2 
         end function analytic_sol_phase_to_geo_path_case_1_f910_r8
 
+        !For: beta*ctg^2(z0) << 1 and z0~90(deg) and 2*beta*Hc >> 1
+        !Formula: 9.11, page: 154
+        elemental function analytic_sol_L2_refractive_error_f911_r4(deln0,z0,beta,Hc) result(L2)
+#if defined(__INTEL_COMPILER) && !defined(__GNUC__)           
+            !dir$ optimize:3
+            !dir$ attributes code_align : 32 :: analytic_sol_L2_refractive_error_f911_r4
+            !dir$ attributes forceinline :: analytic_sol_L2_refractive_error_f911_r4
+#endif 
+!$omp declare simd(analytic_sol_L2_refractive_error_f911_r4)
+             real(kind=sp),        intent(in) :: deln0 
+             real(kind=sp),        intent(in) :: z0 
+             real(kind=sp),        intent(in) :: beta 
+             real(kind=sp),        intent(in) :: Hc 
+             real(kind=sp)                    :: L2 
+             real(kind=sp),        parameter  :: C157079632679489661923132169164 = &
+                                                   1.57079632679489661923132169164_sp
+             real(kind=sp),        parameter  :: C063661977236758134307553505349 = &
+                                                    0.63661977236758134307553505349_sp
+             real(kind=sp),        automatic  :: bta,  sctgz0,
+             real(kind=sp),        automatic  :: basctg, exp1 
+             real(kind=sp),        automatic  :: sqr1,   t0 
+             real(kind=sp),        automatic  :: trm1,   trm2 
+             real(kind=sp),        automatic  :: ctgz0 
+             bta    = beta*6378.0_sp 
+             ctgz0  = 1.0_sp/tan(z0)
+             sctgz0 = ctgz0*ctgz0  
+             sqr    = sqrt(bta)
+             basctg = bta*sctgz0 
+             exp1   = exp(0.5_sp*basctg)
+             trm2   = 1.0_sp-C063661977236758134307553505349 * &
+                      sqr*ctgz0
+             t0     = deln0*sqr 
+             trm1   = t0*exp1*C157079632679489661923132169164 
+             L2     = trm1*trm2 
+        end function analytic_sol_L2_refractive_error_f911_r4
+
 end module atmos_refraction
