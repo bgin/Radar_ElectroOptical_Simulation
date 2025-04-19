ff2f6a4ebf5ac7803ab42de1b8aca62993a14742
Bernard Gingold <beniekg@gmail.com>
2025-04-19 13:51:25 +0200

    Added: `analytic_sol_L2_f341_xmm4r4`

--- Radiolocation/GMS_atmos_refraction_xmm4r4.f90
+++ Radiolocation/GMS_atmos_refraction_xmm4r4.f90
@@ -537,13 +537,13 @@ module atmos_refraction_xmm4r4
                  ctgz0.v(j) = v4r4_1.v(j)/tan(z0.v(j))
                  sdn0.v(j)  = dn0.v(j)*dn0.v(j) 
                  exp1.v(j)  = exp(ea1.v(j))
-                 num1.v(j)  = beta.v(j)*a.v(j)*sdn0.v(j)*ctgz0.v(j)
+                 num1.v(j)  = beta.v(j)*C6378.v(j)*sdn0.v(j)*ctgz0.v(j)
                  cosz0.v(j) = cos(z0.v(j))
                  den1.v(j)  = cosz0.v(j)*cosz0.v(j) 
                  exp2.v(j)  = exp(ea2.v(j))
                  rat1.v(j)  = num1.v(j)/den1.v(j) 
                  stgz0.v(j) = C20.v(j)*(tgz0.v(j)*tgz0.v(j)) 
-                 den2.v(j)  = sqrt(v4r4_1.v(j)+stgz0.v(j)*(H.v(j)/a.v(j)))
+                 den2.v(j)  = sqrt(v4r4_1.v(j)+stgz0.v(j)*(H.v(j)/C6378.v(j)))
                  num2.v(j)  = exp1.v(j)-exp2.v(j) 
                  rat2.v(j)  = num2.v(j)/den2.v(j) 
                  L1.v(j)    = rat1.v(j)*rat2.v(j) 
@@ -554,18 +554,93 @@ module atmos_refraction_xmm4r4
                  ctgz0.v = v4r4_1.v/tan(z0.v)
                  sdn0.v  = dn0.v*dn0.v
                  exp1.v  = exp(ea1.v)
-                 num1.v = beta.v*a.v*sdn0.v*ctgz0.v
+                 num1.v = beta.v*C6378.v*sdn0.v*ctgz0.v
                  cosz0.v = cos(z0.v)
                  den1.v  = cosz0.v*cosz0.v 
                  exp2.v  = exp(ea2.v)
                  rat1.v  = num1.v/den1.v 
                  stgz0.v = C20.v*(tgz0.v*tgz0.v) 
-                 den2.v  = sqrt(v4r4_1.v+stgz0.v*(H.v/a.v))
+                 den2.v  = sqrt(v4r4_1.v+stgz0.v*(H.v/C6378.v))
                  num2.v  = exp1.v-exp2.v 
                  rat2.v  = num2.v/den2.v 
                  L1.v    = rat1.v*rat2.v 
 #endif
       end function analytic_sol_L1_f337_xmm4r4
+
+       !формулa (3.35) для расчета регулярной
+       !рефракции оптических волн в земной атмосфере.
+       ! formula 3.41, page: 68
+      pure function analytic_sol_L2_f341_xmm4r4(dn0,beta,z0,H) result(L2)
+#if defined(__INTEL_COMPILER) && !defined(__GNUC__)            
+            !dir$ optimize:3
+            !dir$ attributes code_align : 32 :: analytic_sol_L2_f341_xmm4r4
+            !dir$ attributes forceinline :: analytic_sol_L2_f341_xmm4r4
+            !dir$ attributes optimization_parameter:"target_arch=skylake-avx512" :: analytic_sol_L2_f341_xmm4r4
+#endif  
+            use mod_vecconsts, only : v4r4_1
+            type(XMM4r4_t),      intent(in) :: beta 
+            type(XMM4r4_t),      intent(in) :: dn0 
+            type(XMM4r4_t),      intent(in) :: z0 
+            type(XMM4r4_t),      intent(in) :: H 
+            type(XMM4r4_t)                  :: L2 
+            type(XMM4r4_t),      parameter  :: C6378 = XMM4r4_t(6378.0_sp)
+            type(XMM4r4_t),      parameter  :: C20   = XMM4r4_t(2.0_sp)
+            type(XMM4r4_t),      parameter  :: C1253314137315500251207882642406 = &
+                                                       XMM4r4_t(1.253314137315500251207882642406_sp)
+            type(XMM4r4_t),      parameter  :: C05   = XMM4r4_t(0.5_sp)                                           
+            type(XMM4r4_t), automatic :: sba, ctgz0, ba 
+            type(XMM4r4_t), automatic :: sctgz0, tbh, phi1, phi2 
+            type(XMM4r4_t), automatic :: exp1, bactgz0, t0, t1  
+             !dir$ attributes align : 16 :: C6378 
+             !dir$ attributes align : 16 :: C20 
+             !dir$ attributes align : 16 :: sba 
+             !dir$ attributes align : 16 :: ctgz0 
+             !dir$ attributes align : 16 :: ba 
+             !dir$ attributes align : 16 :: sctgz0 
+             !dir$ attributes align : 16 :: tbh 
+             !dir$ attributes align : 16 :: phi1 
+             !dir$ attributes align : 16 :: phi2 
+             !dir$ attributes align : 16 :: exp1 
+             !dir$ attributes align : 16 :: bactgz0 
+             !dir$ attributes align : 16 :: t0 
+             !dir$ attributes align : 16 :: t1 
+#if (GMS_EXPLICIT_VECTORIZE) == 1
+             integer(kind=i4) :: j
+#if defined(__INTEL_COMPILER) && !defined(__GNUC__)                  
+             !dir$ loop_count(4)
+             !dir$ vector aligned
+             !dir$ vector vectorlength(4)
+             !dir$ vector always
+#endif             
+             do j=0,3  
+                 sba.v(j)    = sqrt(beta.v(j)*C6378.v(j))
+                 ctgz0.v(j)  = v4r4_1.v(j)/tan(z0.v(j))
+                 sctgz0.v(j) = ctgz0.v(j)*ctgz0.v(j)  
+                 bactgz0.v(j)= beta.v(j)*C6378.v(j)*sctgz0.v(j)  
+                 tbH.v(j)    = c20.v(j)*beta.v(j)*H.v(j)  
+                 t0.v(j)     = dn0.v(j)*sqrt(beta.v(j)*C6378.v(j)*ctgz0.v(j))
+                 exp1.v(j)   = exp(sctgz0.v(j)*C05.v(j))* &
+                                 C1253314137315500251207882642406.v(j)
+                 phi1.v(j)   = prob_integral_xmm4r4(sqrt(bactgz0.v(j)*tbH.v(j)))
+                 phi2.v(j)   = prob_integral_xmm4r4(sqrt(bactgz0.v(j)))
+                 t1.v(j)     = phi1.v(j)-phi2.v(j) 
+                 L2.v(j)     = t0.v(j)*exp1.v(j)*t1.v(j) 
+             end do 
+#else 
+                 sba.v    = sqrt(beta.v*C6378.v)
+                 ctgz0.v  = v4r4_1.v/tan(z0.v)
+                 sctgz0.v = ctgz0.v*ctgz0.v 
+                 bactgz0.v= beta.v*C6378.v*sctgz0.v 
+                 tbH.v    = c20.v*beta.v*H.v 
+                 t0.v     = dn0.v*sqrt(beta.v*C6378.v*ctgz0.v)
+                 exp1.v   = exp(sctgz0.v*C05.v)* &
+                                 C1253314137315500251207882642406.v
+                 phi1.v   = prob_integral_xmm4r4(sqrt(bactgz0.v*tbH.v))
+                 phi2.v   = prob_integral_xmm4r4(sqrt(bactgz0.v))
+                 t1.v     = phi1.v-phi2.v
+                 L2.v     = t0.v*exp1.v*t1.v
+#endif
+      end function analytic_sol_L2_f341_xmm4r4
     
 
 
