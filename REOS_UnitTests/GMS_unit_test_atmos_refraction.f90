fcc8880dff76943c543cc030884cfc915b8d1ece
Bernard Gingold <beniekg@gmail.com>
2025-05-09 08:18:52 +0200

    Added: more unit tests.

--- REOS_UnitTests/GMS_unit_test_atmos_refraction.f90
+++ REOS_UnitTests/GMS_unit_test_atmos_refraction.f90
@@ -22,6 +22,16 @@ module unit_test_atmos_refraction
 
        use mod_kinds, only : i4, sp, dp 
        use atmos_refraction 
+       
+       ! Refractive indices short data [Temp (C), Rel Humiditiy (%), Pressure (kPa), Wavelength (nm), n-Cidor,n-Edlen, diff(Cidor-Edlen)]
+       character(len=64),      parameter :: row1 =  "20,0,101.325,633,1.000271800,1.000271799,0.1"
+	   character(len=64),      parameter :: row2 =  "20,0,60,633,1.000160924,1.000160920,0.4"
+       character(len=64),      parameter :: row3 =  "20,0,120,633,1.000321916,1.000321918,-0.2"
+       character(len=64),      parameter :: row4 =  "50,0,100,633,1.000243285,1.000243270,1.5"
+       character(len=64),      parameter :: row5 =  "5,0,100,633,1.000282756,1.000282750,0.6"
+       character(len=64),      parameter :: row6 =  "-40,0,100,633,1.000337580,1.000337471,10.9"
+       character(len=64),      parameter :: row7 =  "50,100,120,633,1.000287924,1.000287864,6.0"
+
 
        character(len=1),        parameter, private :: dcol = ":"
 
@@ -30,18 +40,17 @@ module unit_test_atmos_refraction
        contains 
 
 subroutine unit_test_n_refract_tht_f243_r4()
-              character(len=256) :: emsg 
-              character(len=128) :: filename 
-              real(kind=sp),     automatic :: n 
-              real(kind=sp),     automatic :: n0 
-              real(kind=sp),     automatic :: z 
-              real(kind=sp),     automatic :: z0 
-              real(kind=sp),     automatic :: r 
-              real(kind=sp),     automatic :: R0  
-              real(kind=sp),     automatic :: phi 
-              real(kind=sp),     automatic :: phi0 
-              real(kind=sp),     automatic :: n_over_tht 
-              integer(kind=i4),  automatic :: lstart, lend 
+              character(len=128), automatic :: filename 
+              real(kind=sp),      automatic :: n 
+              real(kind=sp),      automatic :: n0 
+              real(kind=sp),      automatic :: z 
+              real(kind=sp),      automatic :: z0 
+              real(kind=sp),      automatic :: r 
+              real(kind=sp),      automatic :: R0  
+              real(kind=sp),      automatic :: phi 
+              real(kind=sp),      automatic :: phi0 
+              real(kind=sp),      automatic :: n_over_tht 
+              integer(kind=i4),   automatic :: lstart, lend 
               character(len=10)            :: t
               character(len=8)             :: d
               character(len=50), parameter :: header = "[TEST #1: n_refract_tht_f243_r4 -- START]"
@@ -51,7 +60,8 @@ subroutine unit_test_n_refract_tht_f243_r4()
               call DATE_AND_TIME(date=d,time=t)
               filename = __FILE__ 
               lstart   = __LINE__ 
-              print*, d , ":" , t , filename , lstart ,  header
+              print*, d , " : " , t , filename , lstart  
+              print*,   header
               n           = 1.000271800_sp 
               n0          = 1.000282756_sp 
               z           = 0.5_sp 
@@ -61,36 +71,37 @@ subroutine unit_test_n_refract_tht_f243_r4()
               phi         = 0.35_sp 
               phi0        = 0.15_sp 
               n_over_tht  = n_refract_tht_f243_r4(n,n0,z,z0,r,R0,phi,phi0)
-              print*,"[Input]:  n=", n, "n0=", n0, "z=", z, "z0", z0, "r", r, "R0", R0, "phi", phi, "phi0", phi0  
+              print*,"[Input]:  n=",n,"n0=",n0,"z=",z,"z0",z0,"r=",r,"R0=",R0,"phi=",phi,"phi0=",phi0  
               print*,"[Output]: n_over_tht=",n_over_tht  
               call DATE_AND_TIME(date=d,time=t)
               lend = __LINE__
-              print*, d , ":" , t , filename , lend ,  footer
+              print*, d , ":" , t , filename , lend
+              print*,  footer
 end subroutine unit_test_n_refract_tht_f243_r4
 
 subroutine unit_test_n_refract_tht_f243_r8()
-              character(len=256) :: emsg 
-              character(len=128) :: filename 
-              real(kind=dp),     automatic :: n 
-              real(kind=dp),     automatic :: n0 
-              real(kind=dp),     automatic :: z 
-              real(kind=dp),     automatic :: z0 
-              real(kind=dp),     automatic :: r 
-              real(kind=dp),     automatic :: R0  
-              real(kind=dp),     automatic :: phi 
-              real(kind=dp),     automatic :: phi0 
-              real(kind=dp),     automatic :: n_over_tht 
-              integer(kind=i4),  automatic :: lstart, lend 
+              character(len=128), automatic :: filename 
+              real(kind=dp),      automatic :: n 
+              real(kind=dp),      automatic :: n0 
+              real(kind=dp),      automatic :: z 
+              real(kind=dp),      automatic :: z0 
+              real(kind=dp),      automatic :: r 
+              real(kind=dp),      automatic :: R0  
+              real(kind=dp),      automatic :: phi 
+              real(kind=dp),      automatic :: phi0 
+              real(kind=dp),      automatic :: n_over_tht 
+              integer(kind=i4),   automatic :: lstart, lend 
               character(len=10)            :: t
               character(len=8)             :: d
-              character(len=50), parameter :: header = "[TEST #1: n_refract_tht_f243_r8 -- START]"
-              character(len=48), parameter :: footer = "[TEST #1: n_refract_tht_f243_r8 -- END]"
+              character(len=50), parameter :: header = "[TEST #2: n_refract_tht_f243_r8 -- START]"
+              character(len=48), parameter :: footer = "[TEST #2: n_refract_tht_f243_r8 -- END]"
               ! Exec code ...
                           
               call DATE_AND_TIME(date=d,time=t)
               filename = __FILE__ 
               lstart   = __LINE__ 
-              print*, d , ":" , t , filename , lstart ,  header
+              print*, d , " : " , t , filename , lstart
+              print*,  header
               n           = 1.000271800_dp 
               n0          = 1.000282756_dp 
               z           = 0.5_dp 
@@ -100,12 +111,95 @@ subroutine unit_test_n_refract_tht_f243_r8()
               phi         = 0.35_dp 
               phi0        = 0.15_dp 
               n_over_tht  = n_refract_tht_f243_r8(n,n0,z,z0,r,R0,phi,phi0)
-              print*,"[Input]:  n=", n, "n0=", n0, "z=", z, "z0", z0, "r", r, "R0", R0, "phi", phi, "phi0", phi0  
+              print*,"[Input]:  n=",n,"n0=",n0,"z=",z,"z0=",z0,"r=",r,"R0=",R0,"phi=",phi,"phi0=",phi0  
               print*,"[Output]: n_over_tht=",n_over_tht  
               call DATE_AND_TIME(date=d,time=t)
               lend = __LINE__
-              print*, d , ":" , t , filename , lend ,  footer
+              print*, d , " : " , t , filename , lend 
+              print*,  footer
 end subroutine unit_test_n_refract_tht_f243_r8
+
+subroutine unit_test_n_refract_phi_f243_r4()
+              character(len=128), automatic :: filename 
+              real(kind=sp),      automatic :: n 
+              real(kind=sp),      automatic :: n0 
+              real(kind=sp),      automatic :: z 
+              real(kind=sp),      automatic :: z0 
+              real(kind=sp),      automatic :: r 
+              real(kind=sp),      automatic :: R0  
+              real(kind=sp),      automatic :: phi 
+              real(kind=sp),      automatic :: phi0 
+              real(kind=sp),      automatic :: n_over_tht 
+              integer(kind=i4),   automatic :: lstart, lend 
+              character(len=10)            :: t
+              character(len=8)             :: d
+              character(len=50), parameter :: header = "[TEST #3: n_refract_phi_f243_r4 -- START]"
+              character(len=48), parameter :: footer = "[TEST #3: n_refract_phi_f243_r4 -- END]"
+              ! Exec code ...
+                          
+              call DATE_AND_TIME(date=d,time=t)
+              filename = __FILE__ 
+              lstart   = __LINE__ 
+              print*, d , ":" , t , filename , lstart
+              print*,  header
+              n           = 1.000271800_sp 
+              n0          = 1.000282756_sp 
+              z           = 0.5_sp 
+              z0          = 0.42_sp 
+              r           = 25000.0_sp
+              R0          = 15000.0_sp
+              phi         = 0.47_sp 
+              phi0        = 0.25_sp 
+              n_over_tht  = n_refract_phi_f243_r4(n,n0,z,z0,r,R0,phi,phi0)
+              print*,"[Input]:  n=",n,"n0=",n0,"z=",z,"z0=",z0,"r=",r,"R0=",R0,"phi=",phi,"phi0=",phi0  
+              print*,"[Output]: n_over_tht=",n_over_tht  
+              call DATE_AND_TIME(date=d,time=t)
+              lend = __LINE__
+              print*, d , " : " , t , filename , lend  
+              print*,  footer
+end subroutine unit_test_n_refract_phi_f243_r4
+
+subroutine unit_test_n_refract_phi_f243_r8
+              character(len=128), automatic :: filename 
+              real(kind=dp),      automatic :: n 
+              real(kind=dp),      automatic :: n0 
+              real(kind=dp),      automatic :: z 
+              real(kind=dp),      automatic :: z0 
+              real(kind=dp),      automatic :: r 
+              real(kind=dp),      automatic :: R0  
+              real(kind=dp),      automatic :: phi 
+              real(kind=dp),      automatic :: phi0 
+              real(kind=dp),      automatic :: n_over_tht 
+              integer(kind=i4),   automatic :: lstart, lend 
+              character(len=10)            :: t
+              character(len=8)             :: d
+              character(len=50), parameter :: header = "[TEST #4: n_refract_phi_f243_r8 -- START]"
+              character(len=48), parameter :: footer = "[TEST #4: n_refract_phi_f243_r8 -- END]"
+              ! Exec code ...
+                          
+              call DATE_AND_TIME(date=d,time=t)
+              filename = __FILE__ 
+              lstart   = __LINE__ 
+              print*, d , ":" , t , filename , lstart
+              print*,  header
+              n           = 1.000271800_dp 
+              n0          = 1.000282756_dp 
+              z           = 0.5_dp 
+              z0          = 0.42_dp 
+              r           = 25000.0_dp
+              R0          = 15000.0_dp
+              phi         = 0.47_dp 
+              phi0        = 0.25_dp 
+              n_over_tht  = n_refract_phi_f243_r8(n,n0,z,z0,r,R0,phi,phi0)
+              print*,"[Input]:  n=",n,"n0=",n0,"z=",z,"z0=",z0,"r=",r,"R0=",R0,"phi=",phi,"phi0=",phi0  
+              print*,"[Output]: n_over_tht=",n_over_tht  
+              call DATE_AND_TIME(date=d,time=t)
+              lend = __LINE__
+              print*, d , " : " , t , filename , lend  
+              print*,  footer
+end subroutine unit_test_n_refract_phi_f243_r8
+
+
 
 
 
@@ -115,8 +209,11 @@ end module unit_test_atmos_refraction
 
 program main 
 use unit_test_atmos_refraction
-
+    write(*,*), "Using precomputed n-Cidor values."
+    write(*,'(A60)'), row1 
+    write(*,'(A60)'), row5  
     call unit_test_n_refract_tht_f243_r4()
     call unit_test_n_refract_tht_f243_r8() 
-       
+    call unit_test_n_refract_phi_f243_r4()
+    call unit_test_n_refract_phi_f243_r8()
 end program main 
